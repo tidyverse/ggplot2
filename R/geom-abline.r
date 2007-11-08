da6f694803ebdfab8cda7d72d5711c3263c2dd07
hadley <h.wickham@gmail.com>
2007-11-08 15:37:49 +0000

    Fixed abline bug

--- R/geom-abline.r
+++ R/geom-abline.r
@@ -3,7 +3,7 @@ GeomAbline <- proto(Geom, {
     if (missing(data)) {
       data <- data.frame(intercept = intercept, slope=slope)
     }
-    mapping <- defaults(mapping, aes(intercept=intercept, x=NULL, y=NULL, group=1))
+    mapping <- defaults(mapping, aes(intercept=intercept, slope=slope, x=NULL, y=NULL, group=1))
     class(mapping) <- "uneval"
     
     layer(mapping=mapping, data=data, geom = ., geom_params = list(...))
