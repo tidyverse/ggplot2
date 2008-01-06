0802516c866364bd8be3820394fec4eae8242850
hadley <h.wickham@gmail.com>
2008-01-06 19:20:49 +0000

    Rename so inheritance works.  Use gray.colors instead of gray directly

---
+++ R/scale-discrete-grey.r
@@ -0,0 +1,39 @@
+ScaleGrey <- proto(ScaleColour, expr={
+  doc <- TRUE
+
+  new <- function(., name=NULL, variable, start = 0.3, end = 0.9) {
+    .$proto(name=name, .input=variable, .output=variable, start=start, end=end)
+  }
+
+  breaks <- function(.) {
+    grey.colors(length(.$domain()), start = .$start, end = .$end)
+  }
+
+  max_levels <- function(.) Inf
+
+  # Documetation -----------------------------------------------
+
+  objname <- "grey"
+  desc <- "Grey colour scale"
+  details <- "<p>Based on ?gray.colors</p>"
+  
+  desc_params <- list(
+    "start" = "starting grey colour (between 0 and 1)",
+    "end" = "ending grey colour (between 0 and 1)"
+  )
+
+  icon <- function(.) {
+    rectGrob(c(0.1, 0.3, 0.5, 0.7, 0.9), width=0.21, 
+      gp=gpar(fill=gray(seq(0, 1, length=5)), col=NA)
+    )
+  }
+  
+  examples <- function(.) {
+    p <- qplot(mpg, wt, data=mtcars, colour=factor(cyl)) 
+    p + scale_colour_grey()
+    p + scale_colour_grey(end = 0)
+    
+    # You may want to turn off the pale grey background with this scale
+    p + scale_colour_grey() + theme_bw
+  }
+})
