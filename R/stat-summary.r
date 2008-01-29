2ad7b821392e82c485f3f398effec6dc271d0232
hadley <h.wickham@gmail.com>
2008-01-29 04:10:19 +0000

    Improvements to stat_sum

--- R/stat-summary.r
+++ R/stat-summary.r
@@ -2,12 +2,18 @@ StatSummary <- proto(Stat, {
   objname <- "summary" 
   desc <- "Summarise y values at every unique x"
   
+  details <- "<p>stat_summary allows for tremendous flexibilty in the specification of summary functions.  Currently this is not well documented, but you can see some examples below, or contact me if you'd like more details.</p>"
+  
   default_geom <- function(.) GeomPointrange
   required_aes <- c("x", "y")
    
   calculate <- function(., data, scales, fun=stat_range, ...) {
-    if (is.character(fun)) fun <- match.fun(paste("stat", fun, sep="_"))
-    summaryby(data, data$x, fun, ...)
+    
+    if (is.character(fun)) {
+      sumfun <- paste("sum", fun, sep="_")
+      if (exists(sumfun)) fun <- match.fun(sumfun)
+    }
+    summaryby(data, data$x, auto_wrap(fun), ...)
   }
   seealso <- list(
     "geom_errorbar" = "error bars",
@@ -22,49 +28,62 @@ StatSummary <- proto(Stat, {
     # Basic operation on a small dataset
     c <- qplot(cyl, mpg, data=mtcars)
     c + stat_summary()
-    c + stat_summary(fun="mean_cl_normal", colour="red")
-    c + stat_summary(fun="mean_cl_normal", colour="red", geom="crossbar")
-    c + stat_summary(fun="mean_cl_normal", colour="red", geom="crossbar", width=0.2)
-    c + stat_summary(fun="mean_cl_normal", colour="red", geom="errorbar", width=0.2)
-    c + stat_summary(fun="mean_cl_normal", colour="red", geom="linerange")
-    c + stat_summary(fun="mean_cl_normal", colour="red", geom="smooth")
-
-    c + stat_summary(fun="mean", colour="red", geom="point")
-    c + stat_summary(fun="mean", colour="red", geom="path")
-    c + stat_summary(fun="median", colour="red", geom="point")
     
-    statsumbar <- function(fun, ...) {
-      stat_summary(fun=fun, colour="red", geom="crossbar", width=0.2, ...)
+    # The simplest type of summary range takes a vector of x's and returns
+    # a single value:
+    
+    stat_sum_single <- function(fun, geom="point", ...) {
+      stat_summary(fun=fun, colour="red", geom=geom, size = 3, ...)      
     }
     
-    c + statsumbar("mean_cl_boot")
-    c + statsumbar("mean_sdl")
-    c + statsumbar("mean_sdl", mult=1)
-    c + statsumbar("median_hilow")
+    c + stat_sum_single(mean)
+    c + stat_sum_single(mean, geom="line")
+    c + stat_sum_single(median)
+    c + stat_sum_single(sd)
     
-    # Use to augment boxplot
-    qplot(color, price, data=diamonds, geom="boxplot") + stat_summary(geom="point",fun="mean")
-    qplot(color, price, data=diamonds, geom="boxplot") + stat_summary(aes(group=1), geom="line",fun="mean")
+    # More complex summary functions operate on a data.frame, summarising
+    # the values of y for a given x (the split into separate data.frames with
+    # uniques values of x is performed automatically by stat_summary)
+    #
+    # A set of useful summary functions is provided from the Hmisc package:
     
-    # A bigger dataset, where these summaries are actually useful
+    stat_sum_df <- function(fun, geom="crossbar", ...) {
+      stat_summary(fun=fun, colour="red", geom=geom, width=0.2, ...)
+    }
+    
+    c + stat_sum_df("mean_cl_boot")
+    c + stat_sum_df("mean_sdl")
+    c + stat_sum_df("mean_sdl", mult=1)
+    c + stat_sum_df("median_hilow")
+    c + stat_sum_df("range", geom="linerange")
+
+    # There are lots of different geoms you can use to display the summaries
+        
+    c + stat_sum_df(fun="mean_cl_normal")
+    c + stat_sum_df(fun="mean_cl_normal", geom = "errorbar")
+    c + stat_sum_df(fun="mean_cl_normal", geom = "pointrange")
+    c + stat_sum_df(fun="mean_cl_normal", geom = "smooth")
+        
+    # Summaries are much more useful with a bigger data set:
     m <- ggplot(movies, aes(x=round(rating), y=votes)) + geom_point()
-    
     (m2 <- m + stat_summary(fun="mean_cl_boot", geom="crossbar", colour="red", width=0.3))
     # Notice how the overplotting skews out visual perception of the mean
     # supplementing the raw data with summary statisitcs is _very_ important
   
-    # Next, we'll put votes on a log scale.
+    # Next, we'll look at votes on a log scale.
+
     # Transforming the scale performs the transforming before the statistic.
     # This means we're calculating the summary on the logged data
     m2 + scale_y_log10()
-    # Transforming the coordinate system performs the transforming after the statistic
-    # This means we're calculating the summary on the raw data, and stretching
-    # the geoms onto the log scale
+    # Transforming the coordinate system performs the transforming after the
+    # statistic. This means we're calculating the summary on the raw data, 
+    # and stretching the geoms onto the log scale.  Compare the widths of the
+    # standard errors.
     m2 + coord_trans(y="log10")
   }
 })
 
-summaryby <- function(data, split, summary=yrange, ...) {
+summaryby <- function(data, split, summary, ...) {
   parts <- split(data, factor(split))
   unique <- lapply(parts, function(df) uniquecols(df[setdiff(names(df), c("y"))]))
   
@@ -76,27 +95,27 @@ summaryby <- function(data, split, summary=yrange, ...) {
   do.call("rbind.fill", parts)
 }
 
+auto_wrap <- function(f) {
+  if (is.character(f)) f <- match.fun(f)
+  args <- names(formals(f))
+  if ("data" %in% args) {
+    function(data, ...) f(data, ...)
+  } else if ("x" %in% args ) {
+    function(df, ...) data.frame(y = safe.call(f, list(x = df$y, ...)))
+  } else {
+    error("Functions provided to stat_summary require a parameter named data or x")
+  }
+}
+
 wrap_hmisc <- function(x, fun, ...) {
   try_require("Hmisc")
 
-  params <- list(...)
-  fun.params <- params[intersect(names(formals(fun)), names(params))]
-  
-  result <- do.call(fun, c(x=list(x), fun.params))
+  result <- safe.call(fun, list(x=x, ...))
   rename(data.frame(t(result)), c(Median="y", Mean="y", Lower="min", Upper="max"))
 }
 
-stat_mean_cl_boot <- function(df, ...) wrap_hmisc(df$y, fun=smean.cl.boot, ...)
-stat_mean_cl_normal <- function(df, ...) wrap_hmisc(df$y, fun=smean.cl.normal, ...)
-stat_mean_sdl <- function(df, ...) wrap_hmisc(df$y, fun=smean.sdl, ...)
-stat_median_hilow <- function(df, ...) wrap_hmisc(df$y, fun=smedian.hilow, ...)
-
-stat_range <- function(data, ...) summaryby(data, data$x, yrange)
-stat_mean <- function(data, ...) summaryby(data, data$x, ymean)
-stat_median <- function(data, ...) summaryby(data, data$x, ymedian)
-stat_max <- function(data, ...) summaryby(data, data$x, ymax)
-
-yrange <- function(df, ...) data.frame(min=min(df$y, na.rm=TRUE), max=max(df$y, na.rm=TRUE))
-ymean <- function(df, ...) data.frame(y=mean(df$y, na.rm=TRUE))
-ymedian <- function(df, ...) data.frame(y=median(df$y, na.rm=TRUE))
-ymax <- function(df, ...) data.frame(y=max(df$y, na.rm=TRUE))
+sum_mean_cl_boot <- function(data, ...) wrap_hmisc(data$y, fun=smean.cl.boot, ...)
+sum_mean_cl_normal <- function(data, ...) wrap_hmisc(data$y, fun=smean.cl.normal, ...)
+sum_mean_sdl <- function(data, ...) wrap_hmisc(data$y, fun=smean.sdl, ...)
+sum_median_hilow <- function(data, ...) wrap_hmisc(data$y, fun=smedian.hilow, ...)
+sum_range <- function(data, ...) data.frame(min=min(data$y, na.rm=TRUE), max=max(data$y, na.rm=TRUE))
