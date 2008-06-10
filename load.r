e5e3a776c03505af290042b4bdff710069a2775f
hadley <h.wickham@gmail.com>
2008-06-10 14:14:48 -0500

    Automated test to see if digest is changing

--- load.r
+++ load.r
@@ -8,3 +8,11 @@ lapply(dir("~/documents/ggplot/ggplot/R", full.name=T), source.with.err)
 
 accessors_print("~/documents/ggplot/ggplot/R/xxx.r")
 source("~/documents/ggplot/ggplot/R/xxx.r")
+
+if (!exists("curr")) curr <- NULL
+prev <- curr
+curr <- digest.ggplot(qplot(mpg, wt, data=mtcars))
+
+if (!is.null(prev) & !identical(prev, curr)) {
+  stop("Digest has changed from ", prev, " to ", curr, call. = FALSE)
+}
