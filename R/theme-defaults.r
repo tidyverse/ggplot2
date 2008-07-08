be62bb6f08f084f806a2ca2dd69a55419ab3bce7
hadley <h.wickham@gmail.com>
2008-07-08 10:01:30 -0500

    Add black and white theme

--- R/theme-defaults.r
+++ R/theme-defaults.r
@@ -1,8 +1,3 @@
-
-# Should be function with params:
-#   fontsize
-#   background colour
-#   foreground colour  (and interpolate between them? prob won't work)
 theme_gray <- function(base_size = 12) {
   structure(list(
     axis.line =         theme_blank(),
@@ -35,4 +30,36 @@ theme_gray <- function(base_size = 12) {
   ), class = "options")
 }
 
+
+theme_bw <- function(base_size = 12) {
+  structure(list(
+    axis.line =         theme_blank(),
+    axis.text.x =       theme_text(size = base_size * 0.8 , lineheight = 0.9, vjust = 1),
+    axis.text.y =       theme_text(size = base_size * 0.8, lineheight = 0.9, hjust = 1),
+    axis.ticks =      theme_segment(),
+    axis.title.x =      theme_text(size = base_size),
+    axis.title.y =      theme_text(size = base_size, angle = 90),
+    axis.ticks.length = unit(0.3, "lines"),
+    axis.ticks.margin = unit(0.5, "lines"),
+
+    legend.background = theme_rect(colour=NA), 
+    legend.key =        theme_rect(colour = "grey80"),
+    legend.text =       theme_text(size = base_size),
+    legend.title =      theme_text(size = base_size, face = "bold", hjust = 0),
+
+    panel.background =  theme_rect(fill = "white", colour = NA), 
+    panel.border =      theme_rect(fill = NA, colour="black", size=1.5), 
+    panel.grid.major =  theme_line(colour = "grey80"),
+    panel.grid.minor =  theme_line(colour = "grey95", size = 0.25),
+    panel.empty =       theme_rect(fill = "white", colour = NA),
+
+    strip.background =  theme_rect(fill = "grey80"), 
+    strip.label =       function(variable, value) value, 
+    strip.text.x =     theme_text(size = base_size * 0.8),
+    strip.text.y =     theme_text(size = base_size * 0.8, angle = -90),
+
+    plot.box =          theme_rect(colour = NA),
+    plot.title =        theme_text(size = base_size * 1.2)    
+  ), class = "options")
+}
 
