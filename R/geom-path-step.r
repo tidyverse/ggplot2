d2dcd5adf9007669c2caba08ecd4c88f8f3f5cc3
hadley <h.wickham@gmail.com>
2008-09-11 09:17:06 -0500

    Add direction parameter to geom_step.
    Also bundle stat code into geom - it's a single use stat.

--- R/geom-path-step.r
+++ R/geom-path-step.r
@@ -10,13 +10,47 @@ GeomStep <- proto(GeomPath, {
   }
   details <- "Equivalent to plot(type='s')."
   
-  default_stat <- function(.) StatStep
+  draw <- function(., data, scales, coordinates, direction = "hv", ...) {
+    data <- stairstep(data, direction)
+    GeomPath$draw(data, scales, coordinates, ...)
+  }
+
+  desc_params <- list(
+    direction = "direction of stairs: 'vh' for vertical then horizontal, or 'hv' for horizontal then vertical"
+  )
+  default_stat <- function(.) StatIdentity
   
   examples <- function(.) {
     # Simple quantiles/ECDF from examples(plot)
     x <- sort(rnorm(47))
-    qplot(x, 1:47, geom="step")
-    plot(x, type="s")
+    qplot(seq_along(x), x, geom="step")
     
+    # Steps go vertically, then horizontally
+    qplot(seq_along(x), x, geom="step", direction = "hv")
+    plot(x, type = "s")
+    # Steps go horizontally, then vertically (default)
+    qplot(seq_along(x), x, geom="step", direction = "vh")
+    plot(x, type = "S")
   }
 })
+
+
+stairstep <- function(data, direction="hv") {
+  direction <- match.arg(direction, c("hv", "vh"))
+  data <- as.data.frame(data)[order(data$x), ]
+  n <- nrow(data)
+  
+  if (direction == "vh") {
+    xs <- rep(1:n, each = 2)[-2*n]
+    ys <- c(1, rep(2:n, each=2))
+  } else {
+      ys <- rep(1:n, each = 2)[-2*n]
+      xs <- c(1, rep(2:n, each=2))
+  }
+  
+  data.frame(
+    x = data$x[xs],
+    y = data$y[ys],
+    data[xs, setdiff(names(data), c("x", "y"))]
+  ) 
+}
