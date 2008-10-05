11279f6ee8f7238d5ec0a89d2e872da2cfb468ab
hadley <h.wickham@gmail.com>
2008-10-05 12:20:17 -0500

    Only draw outliers when present

--- R/geom-boxplot.r
+++ R/geom-boxplot.r
@@ -17,14 +17,26 @@ GeomBoxplot <- proto(Geom, {
       stringsAsFactors = FALSE
     ))
     defaults2 <- defaults[c(1,1), ]
-
-    with(data, ggname(.$my_name(), gTree(children = gList(
-      if(length(outliers[[1]]) >= 1) GeomPoint$draw(data.frame(y = outliers[[1]], x = x[rep(1, length(outliers[[1]]))], colour=I(outlier.colour), shape=outlier.shape, size=outlier.size, fill = NA), ...),
+    
+    if (!is.null(data$outliers) && length(data$outliers[[1]] >= 1)) {
+      outliers_grob <- with(data,
+        GeomPoint$draw(data.frame(
+          y = outliers[[1]], x = x[rep(1, length(outliers[[1]]))],
+          colour=I(outlier.colour), shape = outlier.shape, 
+          size = outlier.size, fill = NA), ...
+        )
+      )
+    } else {
+      outliers_grob <- NULL
+    }
+    
+    with(data, ggname(.$my_name(), grobTree(
+      outliers_grob,
       GeomPath$draw(data.frame(y=c(upper, ymax), defaults2), ...),
       GeomPath$draw(data.frame(y=c(lower, ymin), defaults2), ...),
       GeomBar$draw(data.frame(ymax = upper, ymin = lower, defaults), ...),
       GeomBar$draw(data.frame(ymax = middle, ymin = middle, defaults), ...)
-    ))))
+    )))
   }
 
   objname <- "boxplot"
