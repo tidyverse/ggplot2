89437837c35ba9997c86c7b29a432ebabde16566
hadley <h.wickham@gmail.com>
2008-10-08 10:28:04 -0500

    Add a frequency polygon primitive

---
+++ R/geom-line-freqpoly.r
@@ -0,0 +1,17 @@
+GeomFreqpoly <- proto(GeomPath, {
+  objname <- "freqpoly"
+  desc <- "Frequency polygon"
+  icon <- function(.) {
+    y <- c(0.2, 0.3, 0.5, 0.6,0.2, 0.8, 0.5, 0.3)
+    linesGrob(seq(0.1, 0.9, by=0.1), y, gp=gpar(col="grey60"))
+  }
+  
+  default_stat <- function(.) StatBin
+  
+  seealso <- list(
+    geom_histogram = GeomHistogram$desc
+  )
+  
+  examples <- function(.) {
+  }
+})
