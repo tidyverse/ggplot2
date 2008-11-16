52bedbc7c8ecf74e22fe06c4dc19bf1d60320cd5
hadley <h.wickham@gmail.com>
2008-11-16 16:58:28 -0600

    Add geom_bin2d

---
+++ R/geom-bin2d.r
@@ -0,0 +1,23 @@
+GeomBin2d <- proto(Geom, {
+  draw <- function(., data, scales, coordinates, ...) {
+    GeomRect$draw(data, scales, coordinates, ...)
+  }
+
+  objname <- "bin2d"
+  desc <- "Add heatmap of 2d bin counts"
+  icon <- function(.) {
+  }
+  
+  guide_geom <- function(.) "polygon"
+  
+  default_stat <- function(.) StatBin2d
+  required_aes <- c("xmin", "xmax", "ymin", "ymax")
+  default_aes <- function(.) {
+    aes(colour = NA, fill = "grey60", size = 0.5, linetype = 1, weight = 1)
+  }
+
+  examples <- function(.) {
+    # See stat_bin2d
+  }
+
+})
