35eaf84b9e493325c1e99d8460d5fde23108116d
hadley <h.wickham@gmail.com>
2008-11-21 07:58:49 -0600

    Add stat defaults function

--- R/geom-defaults.r
+++ R/geom-defaults.r
@@ -4,6 +4,9 @@
 # @keywords name of geom to modify
 # @keywords named list of aesthetics
 # @keywords hplot
+#X update_geom_defaults("point", aes(colour = "darkblue"))
+#X qplot(mpg, wt, data = mtcars)
+#X update_geom_defaults("point", aes(colour = "black"))
 update_geom_defaults <- function(geom, new) {
   g <- Geom$find(geom)
   old <- g$default_aes()
@@ -16,3 +19,17 @@ update_geom_defaults <- function(geom, new) {
 # change geom defaults for other params
 # change scale defaults
 # change default scale for given aesthetic
+
+# Update geom defaults
+# Modify geom aesthetic defaults for future plots
+# 
+# @keywords name of geom to modify
+# @keywords named list of aesthetics
+# @keywords hplot
+update_stat_defaults <- function(geom, new) {
+  g <- Stat$find(geom)
+  old <- g$default_aes()
+  
+  aes <- defaults(new, old)
+  g$default_aes <- eval(substitute(function(.) aes, list(aes = aes)))
+}
