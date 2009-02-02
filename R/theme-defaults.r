5f04f8e48ccc5bbab2f1a76a01387e494dede5a0
hadley <h.wickham@gmail.com>
2009-02-02 13:30:13 -0600

    Use white background, not transparent

--- R/theme-defaults.r
+++ R/theme-defaults.r
@@ -15,7 +15,7 @@ theme_gray <- function(base_size = 12) {
     axis.ticks.length = unit(0.15, "cm"),
     axis.ticks.margin = unit(0.1, "cm"),
 
-    legend.background = theme_rect(colour=NA), 
+    legend.background = theme_rect(colour="white"), 
     legend.key =        theme_rect(fill = "grey95", colour = "white"),
     legend.key.size =   unit(1.2, "lines"),
     legend.text =       theme_text(size = base_size * 0.8),
@@ -33,7 +33,7 @@ theme_gray <- function(base_size = 12) {
     strip.text.x =      theme_text(size = base_size * 0.8),
     strip.text.y =      theme_text(size = base_size * 0.8, angle = -90),
 
-    plot.background =   theme_rect(colour = NA),
+    plot.background =   theme_rect(colour = NA, fill = "white"),
     plot.title =        theme_text(size = base_size * 1.2, vjust = 0),
     plot.margin =       unit(c(1, 1, 0.5, 0.5), "lines")
   ), class = "options")
