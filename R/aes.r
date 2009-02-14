530d0494234ae1e24a03fb0ac7bf699009b29251
hadley <h.wickham@gmail.com>
2009-02-14 10:51:12 -0600

    Don't throw away list columns - needed for boxplot outliers

--- R/aes.r
+++ R/aes.r
@@ -111,7 +111,7 @@ aesdefaults <- function(data, y., params.) {
   
   cols <- tryapply(defaults(data, updated), function(x) eval(x, data, globalenv()))
   
-  cols <- cols[unlist(llply(cols, is.atomic))]
+  cols <- cols[unlist(llply(cols, function(x) is.atomic(x) || is.list(x)))]
   df <- as.data.frame(cols)
   
   factors <- sapply(df, is.factor)
