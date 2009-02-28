c701b82ee04783896d3f1192d181a9547e543158
hadley <h.wickham@gmail.com>
2009-02-28 11:41:14 -0600

    Make xlim and ylim generic

--- R/scale-convenience.r
+++ R/scale-convenience.r
@@ -9,17 +9,7 @@
 #X xlim("a", "b", "c") 
 #X qplot(mpg, wt, data=mtcars) + xlim(15, 20)
 xlim <- function(...) {
-  limits <- c(...)
-  if (is.numeric(limits)) {
-    stopifnot(length(limits) == 2)
-    if (limits[1] > limits[2]) {
-      scale_x_reverse(limits = limits)
-    } else {
-      scale_x_continuous(limits = limits)
-    }
-  } else {
-    scale_x_discrete(limits = limits)    
-  }
+  limits(c(...), "x")
 }
 
 # Set y limits
@@ -32,15 +22,50 @@ xlim <- function(...) {
 #X ylim("a", "b", "c") 
 #X qplot(mpg, wt, data=mtcars) + ylim(15, 20)
 ylim <- function(...) {
-  limits <- c(...)
-  if (is.numeric(limits)) {
-    stopifnot(length(limits) == 2)
-    if (limits[1] > limits[2]) {
-      scale_y_reverse(limits = limits)
-    } else {
-      scale_y_continuous(limits = limits)
-    }
+  limits(c(...), "y")
+}
+
+# Scale limits
+# Generate correct scale type for specified limits
+# 
+# @arguments vector of limits
+# @arguments variable
+# @keywords internal
+# @alias limits.numeric
+# @alias limits.character
+# @alias limits.factor 
+# @alias limits.Date
+# @alias limits.POSIXct
+# @alias limtis.POSIXlt
+#X limits(c(1, 5), "x")
+#X limits(c(5, 1), "x")
+#X limits(c("A", "b", "c"), "x")
+#X limits(as.Date(c("2008-01-01", "2009-01-01")), "x")
+limits <- function(lim, var) UseMethod("limits")
+limits.numeric <- function(lims, var) {
+  stopifnot(length(lims) == 2)
+  if (lims[1] > lims[2]) {
+    trans <- "reverse"
   } else {
-    scale_y_discrete(limits = limits)    
+    trans <- "identity"
   }
+  ScaleContinuous$new(var = var, limits = lims, trans = trans)  
+}
+limits.character <- function(lims, var) {
+  ScaleDiscrete$new(var = var, limits = lims)
+}
+limits.factor <- function(lims, var) {
+  ScaleDiscrete$new(var = var, limits = as.character(lims))
+}
+limits.Date <- function(lims, var) {
+  stopifnot(length(lims) == 2)
+  ScaleDate$new(var = var, limits = lims)
+}
+limits.POSIXct <- function(lims, var) {
+  stopifnot(length(lims) == 2)
+  ScaleDateTime$new(var = var, limits = lims)
+}
+limits.POSIXlt <- function(lims, var) {
+  stopifnot(length(lims) == 2)
+  ScaleDateTime$new(var = var, limits = as.POSIXct(lims))
 }
