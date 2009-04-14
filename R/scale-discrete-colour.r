4f2fd385e6730e83e44d7a1514e425e222a173e5
hadley <h.wickham@gmail.com>
2009-04-14 16:50:41 -0500

    Make default categorical two colour case less problematical for the colour blind

--- R/scale-discrete-colour.r
+++ R/scale-discrete-colour.r
@@ -7,7 +7,7 @@ ScaleColour <- proto(ScaleDiscrete, expr={
 ScaleHue <- proto(ScaleColour, expr={
   aliases <- c("scale_colour_discrete", "scale_fill_discrete", "scale_color_hue")
   
-  new <- function(., name=NULL, h=c(0,360), l=65, c=100, limits=NULL, breaks = NULL, labels=NULL, h.start = 0, direction = 1,  formatter = identity, variable) {
+  new <- function(., name=NULL, h=c(0,360) + 15, l=65, c=100, limits=NULL, breaks = NULL, labels=NULL, h.start = 0, direction = 1,  formatter = identity, variable) {
     .$proto(name=name, h=h, l=l, c=c, .input=variable, .output=variable, .labels = labels, breaks = breaks, direction = direction, start  = h.start, limits = limits, formatter = formatter)
   }
   
