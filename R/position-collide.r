c2cfbf5b6758d8658fb3100dd0dcbc6aba3869e3
hadley <h.wickham@gmail.com>
2009-05-03 18:53:50 -0500

    Another improvement to collision detection

--- R/position-collide.r
+++ R/position-collide.r
@@ -30,7 +30,7 @@ collide <- function(data, width = NULL, name, strategy, check.width = TRUE) {
   # Check for overlap
   data <- data[order(data$xmin), ]
   intervals <- as.numeric(t(unique(data[c("xmin", "xmax")])))
-  intervals <- intervals[!is.na(intervals)] / max(intervals)
+  intervals <- scale(intervals[!is.na(intervals)])
   if (any(diff(intervals) < -1e-6)) {
     stop(name, " requires non-overlapping x intervals", call. = FALSE)
     # This is where the algorithm from [L. Wilkinson. Dot plots. 
