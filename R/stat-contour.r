131ad75eae9267ee48e4567e7649a31840cba48c
hadley <h.wickham@gmail.com>
2010-01-05 16:26:50 -0600

    Fix stat_contour bug when x or y -ve

--- R/stat-contour.r
+++ R/stat-contour.r
@@ -16,7 +16,8 @@ StatContour <- proto(Stat, {
     }
     
     z <- tapply(data$z, data[c("x", "y")], identity)
-    cl <- contourLines(x = unique(data$x), y = unique(data$y), z = z, 
+    cl <- contourLines(
+      x = sort(unique(data$x)), y = sort(unique(data$y)), z = z, 
       levels = breaks)  
     cl <- lapply(cl, as.data.frame)
     
