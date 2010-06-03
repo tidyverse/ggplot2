8ea19110adbe6533c480e0df64d28d78678f2c72
hadley <h.wickham@gmail.com>
2010-06-03 06:41:32 -0500

    Change error to warning in position_collide

--- R/position-collide.r
+++ R/position-collide.r
@@ -37,7 +37,7 @@ collide <- function(data, width = NULL, name, strategy, check.width = TRUE) {
   intervals <- as.numeric(t(unique(data[c("xmin", "xmax")])))
   intervals <- scale(intervals[!is.na(intervals)])
   if (any(diff(intervals) < -1e-6)) {
-    stop(name, " requires non-overlapping x intervals", call. = FALSE)
+    warning(name, " requires non-overlapping x intervals", call. = FALSE)
     # This is where the algorithm from [L. Wilkinson. Dot plots. 
     # The American Statistician, 1999.] should be used
   }
