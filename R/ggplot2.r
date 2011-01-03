b59ce5575cd35267aff461c7ff78c5c04b196867
hadley <h.wickham@gmail.com>
2011-01-03 11:00:06 -0600

    Cleaning up imports/depends

--- R/ggplot2.r
+++ R/ggplot2.r
@@ -3,7 +3,7 @@
 #' @name ggplot2
 #' @docType package
 #' @aliases ggplot2 package-ggplot2
-#' @import plyr proto splines MASS RColorBrewer digest colorspace scales
+#' @import plyr proto digest scales
 NULL
 
 #' Prices of 50,000 round cut diamonds
