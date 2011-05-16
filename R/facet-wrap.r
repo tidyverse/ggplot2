d85da579e8bf10400b59a1d8a147438badff91e4
hadley <h.wickham@gmail.com>
2011-05-16 14:58:09 -0500

    Use same position mapping strategy in both facet_wrap and facet_grid

--- R/facet-wrap.r
+++ R/facet-wrap.r
@@ -258,12 +258,18 @@ FacetWrap <- proto(Facet, {
     lapply(data, function(l) {
       for(i in seq_along(.$scales$x)) {
         l[1, i] <- lapply(l[1, i], function(old) {
-          new <- scales_map_df(.$scales$x[[i]], old)
-          if (!is.null(.$scales$y[[i]])) {
-            new <- cbind(new, scales_map_df(.$scales$y[[i]], old))
-          }
-
-          cunion(new, old)
+          if (is.null(old)) return(data.frame())
+          new <- scale_map_df(.$scales$x[[i]], old)
+          if (length(new) == 0) return(old)
+          cbind(new, old[setdiff(names(old), names(new))])
+        }) 
+      }
+      for(i in seq_along(.$scales$y)) {
+        l[1, i] <- lapply(l[1, i], function(old) {
+          if (is.null(old)) return(data.frame())
+          new <- scale_map_df(.$scales$y[[i]], old)
+          if (length(new) == 0) return(old)
+          cbind(new, old[setdiff(names(old), names(new))])
         }) 
       }
       l
