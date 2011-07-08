6649e7a012ffca15cb1a579c1b427aecfb77f7f0
hadley <h.wickham@gmail.com>
2011-07-08 07:54:40 -0500

    Memoise Cartesian scale training

--- R/ggplot2.r
+++ R/ggplot2.r
@@ -3,7 +3,7 @@
 #' @name ggplot2
 #' @docType package
 #' @aliases ggplot2 package-ggplot2
-#' @import plyr digest scales grid reshape2
+#' @import plyr digest scales grid reshape2 memoise
 NULL
 
 #' Prices of 50,000 round cut diamonds
