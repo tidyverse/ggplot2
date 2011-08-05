9cb346ca1ad7addfa13bbcd86174845ac8c804d5
kohske takahashi <takahashi.kohske@gmail.com>
2011-08-05 16:54:39 +0900

    fix a bug in calculating sizes of gtable.

--- R/utilities-table.r
+++ R/utilities-table.r
@@ -281,21 +281,29 @@ compute_grob_heights <- function(grob_layout, heights) {
 
 compute_grob_dimensions <- function(grob_layout, dims) {
   # If any don't have explicit dims, then width is NULL
-  if (!all(grob_layout$type %in% names(dims))) {
+  if (!any(grob_layout$type %in% names(dims))) {
     return(unit(1, "null"))
   }
+
+  grob_layout <- subset(grob_layout, type %in% names(dims))
 
   dims <- unique(Map(function(type, pos) {
     type_width <- dims[[type]]
     if (length(type_width) == 1) type_width else type_width[pos]
   }, grob_layout$type, grob_layout$id))
   units <- vapply(dims, is.unit, logical(1))
-  raw_max <- unit(max(unlist(dims[!units])), "cm")
 
-  if (any(units)) {
-    unit_max <- max(do.call("unit.c", dims[units]))
-    max(raw_max, unit_max)
+  if (all(units)) {
+    if (all(lapply(dims, attr, "unit") == "null")) unit(max(unlist(dims)), "null")
+    else do.call("max", dims)
   } else {
-    raw_max
+    raw_max <- unit(max(unlist(dims[!units])), "cm")
+    if (any(units)) {
+      unit_max <- max(do.call("unit.c", dims[units]))
+      max(raw_max, unit_max)
+    }
+    else {
+      raw_max
+    }
   }
 }
