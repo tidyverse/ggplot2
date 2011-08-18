a9879a20aa7d5bb68d7609a4c4d2e7e5e8757c27
Brian Diggs <diggsb@ohsu.edu>
2011-08-18 16:04:39 -0700

    First pass at autoplot implementation

--- R/autoplot.r
+++ R/autoplot.r
@@ -1,15 +1,22 @@
-# Create a new autoplot
-# autoplot uses ggplot2 to draw a particular plot for a particular class in a single command
-#
-# @seealso \url{http://had.co.nz/ggplot2}
-# @alias autoplot
-# @alias autoplot.default
-# @keyword hplot
-# @arguments object of a class with an autoplot method
-# @arguments other arguments passed to specific methods
+#' Create a complete ggplot appropriate to a particular data type
+#'
+#' autoplot uses ggplot2 to draw a particular plot for a particular class
+#' in a single command. This defines the S3 generic that other classes and
+#' packages can extend.
+#'
+#' @param x an object
+#' @param ... other arguments passed to specific methods
+#' @return a ggplot object
+#' @method autoplot default
+#' @S3method autoplot default
+#' @export
+#' @seealso \url{http://had.co.nz/ggplot2}
+#' @seealso \code{\link{ggplot}}
+#' @seealso \code{\link{fortify}}
 autoplot <- function(x, ...) {
 	UseMethod("autoplot")
 }
+
 autoplot.default <- function(x, ...) {
 	error.msg <- paste("Objects of type",class(x),"not supported by autoplot.  Please use qplot() or ggplot() instead.\n")
 	stop(error.msg)
