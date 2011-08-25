9bc10cb9eff983a71e9f0a007a0028abbc1e16ab
Brian Diggs <diggsb@ohsu.edu>
2011-08-25 07:31:17 -0700

    Changes hadley requested
    4 Changes hadley requested in comments to the commit plus a condensing
    of the seealso directives.  Re-oxygenized.

--- R/autoplot.r
+++ R/autoplot.r
@@ -1,24 +1,21 @@
 #' Create a complete ggplot appropriate to a particular data type
 #'
-#' autoplot uses ggplot2 to draw a particular plot for a particular class
+#' \code{autoplot} uses ggplot2 to draw a particular plot for a particular class
 #' in a single command. This defines the S3 generic that other classes and
 #' packages can extend.
 #'
 #' @param x an object
 #' @param ... other arguments passed to specific methods
 #' @return a ggplot object
-#' @method autoplot default
-#' @S3method autoplot default
 #' @export
-#' @seealso \url{http://had.co.nz/ggplot2}
-#' @seealso \code{\link{ggplot}}
-#' @seealso \code{\link{fortify}}
+#' @seealso \code{\link{ggplot}} and \code{\link{fortify}}
 autoplot <- function(x, ...) {
 	UseMethod("autoplot")
 }
 
+#' @S3method autoplot default
 autoplot.default <- function(x, ...) {
 	error.msg <- paste("Objects of type",class(x),"not supported by autoplot.  Please use qplot() or ggplot() instead.\n")
-	stop(error.msg)
+	stop(error.msg, call.=FALSE)
 }
 
