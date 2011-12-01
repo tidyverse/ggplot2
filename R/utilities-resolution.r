e31df499961194ea3acf50b4a96905dd17cfc675
hadley <h.wickham@gmail.com>
2011-12-01 08:32:03 -0600

    Document resolution with roxygen2

--- R/utilities-resolution.r
+++ R/utilities-resolution.r
@@ -1,21 +1,22 @@
-# Resolution
-# Compute the "resolution" of a data vector, ie. what is the smallest non-zero
-# distance between adjacent values.
-#
-# If there is only one unique value, then the resolution is defined to be one. 
-# 
-# @param numeric vector
-# @param should a zero value be automatically included in the computation of resolution
-# @keyword hplot
-# @keyword internal 
-#X resolution(1:10)
-#X resolution((1:10) - 0.5)
-#X resolution((1:10) - 0.5, FALSE)
-#X resolution(c(1,2, 10, 20, 50))
+#' Compute the "resolution" of a data vector.
+#'
+#' The resolution is is the smallest non-zero distance between adjacent
+#' values.  If there is only one unique value, then the resolution is defined
+#' to be one. 
+#' 
+#' @param x numeric vector
+#' @param zero should a zero value be automatically included in the
+#'   computation of resolution
+#' @export
+#' @examples
+#' resolution(1:10)
+#' resolution((1:10) - 0.5)
+#' resolution((1:10) - 0.5, FALSE)
+#' resolution(c(1,2, 10, 20, 50))
 resolution <- function(x, zero = TRUE) {
+  if (zero_range(range(x, na.rm = TRUE))) return(1)
+  
   x <- unique(as.numeric(x))
-  if (length(x) == 1) return(1)
-
   if (zero) {
     x <- unique(c(0, x))
   }
