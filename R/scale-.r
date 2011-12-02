6559901bff1b287b3d34c60a75295c992eb79430
hadley <h.wickham@gmail.com>
2011-12-02 07:59:14 -0600

    Ensure breaks work even when outside limits. Fixes #261

--- R/scale-.r
+++ R/scale-.r
@@ -248,8 +248,8 @@ scale_breaks.continuous <- function(scale, limits = scale_limits(scale)) {
   }
   
   # Breaks in data space need to be converted back to transformed space
-  # And any breaks outside the dimensions need to be thrown away
-  breaks <- discard(scale$trans$trans(breaks), scale_dimension(scale))
+  # And any breaks outside the dimensions need to be flagged as missing
+  breaks <- censor(scale$trans$trans(breaks), scale_dimension(scale))
   if (length(breaks) == 0) {
     stop("Zero breaks in scale for ", paste(scale$aesthetics, collapse = "/"),
       call. = FALSE)
@@ -286,13 +286,15 @@ scale_breaks_minor.continuous <- function(scale, n = 2, b = scale_break_position
   if (zero_range(as.numeric(limits))) {
     breaks <- limits[1]
   } else if (is.null(scale$minor_breaks)) {
+    b <- b[!is.na(b)]
     if (length(b) == 1) {
-    breaks <-   b
+      breaks <- b
     } else {
       bd <- diff(b)[1]
       if (min(limits) < min(b)) b <- c(b[1] - bd, b)
       if (max(limits) > max(b)) b <- c(b, b[length(b)] + bd)
-      breaks <- unique(unlist(mapply(seq, b[-length(b)], b[-1], length=n+1, SIMPLIFY=F)))
+      breaks <- unique(unlist(mapply(seq, b[-length(b)], b[-1], length=n+1,
+        SIMPLIFY = FALSE)))
     }
   } else if (is.function(scale$minor_breaks)) {
     breaks <- scale$minor_breaks(scale$trans$inv(limits))
--- inst/tests/test-scales-breaks-labels.r
+++ inst/tests/test-scales-breaks-labels.r
@@ -1,11 +1,16 @@
 context("Scales: breaks and labels")
 
-init_scale <- function(...) {
-  sc <- scale_x_discrete(...)
-  scale_train(sc, factor(1:100))
-  expect_that(length(scale_limits(sc)), equals(100))
-  sc
-}
+
+
+
+test_that("labels match breaks, even when outside limits", {
+  sc <- scale_y_continuous(breaks=1:4, labels=1:4, limits = c(1, 3))
+  
+  expect_equal(scale_breaks(sc), c(1:3, NA))
+  expect_equal(scale_labels(sc), 1:4)
+  expect_equal(scale_breaks_minor(sc), c(1, 1.5, 2, 2.5, 3))
+})
+
 
 
 test_that("labels must match breaks", {
@@ -13,8 +18,14 @@ test_that("labels must match breaks", {
     throws_error("unequal lengths"))
   expect_that(scale_x_continuous(breaks = 1:3, labels = 1:2),
     throws_error("unequal lengths"))
-  
 })
+
+init_scale <- function(...) {
+  sc <- scale_x_discrete(...)
+  scale_train(sc, factor(1:100))
+  expect_that(length(scale_limits(sc)), equals(100))
+  sc
+}
 
 test_that("discrete labels match breaks", {
   
@@ -38,4 +49,5 @@ test_that("discrete labels match breaks", {
   expect_equal(length(scale_labels(sc)), 5)
   
 })
+
 
