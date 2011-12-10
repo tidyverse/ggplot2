98950d9dd208abd4b276c74bc66c6c6a17493768
hadley <h.wickham@gmail.com>
2011-12-10 08:00:35 -0600

    Add xlim and ylim to coord_fixed (Fixes #91)

--- R/coord-fixed.r
+++ R/coord-fixed.r
@@ -10,6 +10,7 @@
 #'
 #' @aliases coord_fixed coord_equal
 #' @export coord_fixed coord_equal
+#' @inheritParams coord_cartesian
 #' @examples
 #' # ensures that the ranges of axes are equal to the specified ratio by
 #' # adjusting the plot aspect ratio
@@ -19,8 +20,9 @@
 #' qplot(mpg, wt, data = mtcars) + coord_equal(ratio = 1/5)
 #' 
 #' # Resize the plot to see that the specified aspect ratio is mantained
-coord_fixed <- function(ratio = 1) {
-  coord(ratio = ratio, subclass = c("fixed", "cartesian"))
+coord_fixed <- function(ratio = 1, xlim = NULL, ylim = NULL, wise = FALSE) {
+  coord(limits = list(x = xlim, y = ylim), ratio = ratio, wise = wise,
+    subclass = c("fixed", "cartesian"))
 }
 coord_equal <- coord_fixed
 
