039abc0535575d381062a7b3870e21f98abec314
hadley <h.wickham@gmail.com>
2011-12-19 08:03:10 -0500

    Occassionally provide a hint or tip on startup. Fixes #284

---
+++ R/zzz.r
@@ -0,0 +1,16 @@
+tips <- c(
+  "Need help? Try the ggplot2 mailing list: http://groups.google.com/group/ggplot2.",
+  paste("Find out what's changed in ggplot2 with\n", 
+    "news(Version == \"", packageVersion("ggplot2"), 
+    "\", package = \"ggplot2\")", sep = ""),
+  "Use suppressPackageStartupMessages to eliminate package startup messages."
+)  
+  
+.onLoad <- function(...) {
+  if (runif(1) > 0.1) return()
+  
+  tip <- sample(tips, 1)
+  packageStartupMessage(tip)
+}
+  
+  
