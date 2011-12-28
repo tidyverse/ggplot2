ea50bf1c80159c2d00577d40dfe3de0ef7faf3ff
hadley <h.wickham@gmail.com>
2011-12-28 07:30:03 -0600

    Move tips inside function to avoid problems if ggplot2 not installed

--- R/zzz.r
+++ R/zzz.r
@@ -1,13 +1,13 @@
-tips <- c(
-  "Need help? Try the ggplot2 mailing list: http://groups.google.com/group/ggplot2.",
-  paste("Find out what's changed in ggplot2 with\n", 
-    "news(Version == \"", packageVersion("ggplot2"), 
-    "\", package = \"ggplot2\")", sep = ""),
-  "Use suppressPackageStartupMessages to eliminate package startup messages."
-)  
-
 .onAttach <- function(...) {
   if (stats::runif(1) > 0.1) return()
+
+  tips <- c(
+    "Need help? Try the ggplot2 mailing list: http://groups.google.com/group/ggplot2.",
+    paste("Find out what's changed in ggplot2 with\n", 
+      "news(Version == \"", packageVersion("ggplot2"), 
+      "\", package = \"ggplot2\")", sep = ""),
+    "Use suppressPackageStartupMessages to eliminate package startup messages."
+  )  
   
   tip <- sample(tips, 1)
   packageStartupMessage(tip)
