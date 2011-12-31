c3e20f9beaa11d612ffe51eb0f5c3ba4f193db20
baptiste auguie <baptiste.auguie@gmail.com>
2011-12-31 16:19:34 +1300

    documentation and examples for annotation_custom

--- R/annotation-custom.r
+++ R/annotation-custom.r
@@ -25,7 +25,7 @@ NULL
 ##'         xmin=3, xmax=6, ymin=2, ymax=8)
 ##' }
 ##' # Inset plot
-##' g <- ggplotGrob(qplot(1, 1))
+##' g <- ggplotGrob(qplot(1, 1) + opts(plot.background=theme_rect(col="black")))
 ##' base + annotation_custom(grob=g, xmin=1, xmax=10, ymin=8, ymax=10)
 ##' 
 annotation_custom <- function (grob, xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) { 
