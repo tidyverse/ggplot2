37a1672cbae29a7335c59a377911f90b135597d2
Winston Chang <winston@stdout.org>
2012-01-21 15:14:56 -0600

    coord-map: Rename range->proj and raw->range
    Change x.range->x.proj and x.raw->x.range, and same for y.

--- R/coord-map.r
+++ R/coord-map.r
@@ -59,8 +59,8 @@ coord_transform.map <- function(coord, data, details) {
   trans <- mproject(coord, data$x, data$y, details$orientation)
   out <- cunion(trans[c("x", "y")], data)
   
-  out$x <- rescale(out$x, 0:1, details$x.range)
-  out$y <- rescale(out$y, 0:1, details$y.range)
+  out$x <- rescale(out$x, 0:1, details$x.proj)
+  out$y <- rescale(out$y, 0:1, details$y.proj)
   out
 }
 mproject <- function(coord, x, y, orientation) {    
@@ -73,53 +73,53 @@ mproject <- function(coord, x, y, orientation) {
 
 #' @S3method coord_distance map
 coord_distance.map <- function(coord, x, y, details) {
-  max_dist <- dist_central_angle(details$x.raw, details$y.raw)
+  max_dist <- dist_central_angle(details$x.range, details$y.range)
   dist_central_angle(x, y) / max_dist
 }
 
 #' @S3method coord_aspect map
 coord_aspect.map <- function(coord, ranges) {
-  diff(ranges$y.range) / diff(ranges$x.range)
+  diff(ranges$y.proj) / diff(ranges$x.proj)
 }
 
 #' @S3method coord_train map
 coord_train.map <- function(coord, scales) {
-  x.raw <- coord$xlim %||% scale_dimension(scales$x)
-  y.raw <- coord$ylim %||% scale_dimension(scales$y)
-  orientation <- coord$orientation %||% c(90, 0, mean(x.raw))
+  x.range <- coord$xlim %||% scale_dimension(scales$x)
+  y.range <- coord$ylim %||% scale_dimension(scales$y)
+  orientation <- coord$orientation %||% c(90, 0, mean(x.range))
 
   # Increase chances of creating valid boundary region
   grid <- expand.grid(
-    x = seq(x.raw[1], x.raw[2], length = 50),
-    y = seq(y.raw[1], y.raw[2], length = 50)
+    x = seq(x.range[1], x.range[2], length = 50),
+    y = seq(y.range[1], y.range[2], length = 50)
   )
   range <- mproject(coord, grid$x, grid$y, orientation)$range
 
   # major and minor values in data space
-  x.range <- range[1:2]
+  x.proj <- range[1:2]
   x.major <- scale_breaks(scales$x)
   x.minor <- scale_breaks_minor(scales$x)
   x.labels <- scale_labels(scales$x, x.major)
 
-  y.range <- range[3:4]
+  y.proj <- range[3:4]
   y.major <- scale_breaks(scales$y)
   y.minor <- scale_breaks_minor(scales$y)
   y.labels <- scale_labels(scales$y, y.major)
 
   # if the scale is continuous, drop out-of-range values
   if (inherits(scales$x, "continuous")) {
-    major_inside_range <- x.raw[1] <= x.major & x.major <= x.raw[2]
+    major_inside_range <- x.range[1] <= x.major & x.major <= x.range[2]
     major_inside_range <- major_inside_range & !is.na(major_inside_range)
-    minor_inside_range <- x.raw[1] <= x.minor  & x.minor <= x.raw[2]
+    minor_inside_range <- x.range[1] <= x.minor  & x.minor <= x.range[2]
     minor_inside_range <- minor_inside_range & !is.na(minor_inside_range)
     x.major <- x.major[major_inside_range]
     x.minor <- x.major[minor_inside_range]
     x.labels <- x.labels[major_inside_range]
   }
   if (inherits(scales$y, "continuous")) {
-    major_inside_range <- y.raw[1] <= y.major & y.major <= y.raw[2]
+    major_inside_range <- y.range[1] <= y.major & y.major <= y.range[2]
     major_inside_range <- major_inside_range & !is.na(major_inside_range)
-    minor_inside_range <- y.raw[1] <= y.minor  & y.minor <= y.raw[2]
+    minor_inside_range <- y.range[1] <= y.minor  & y.minor <= y.range[2]
     minor_inside_range <- minor_inside_range & !is.na(minor_inside_range)
     y.major <- y.major[major_inside_range]
     y.minor <- y.major[minor_inside_range]
@@ -127,8 +127,8 @@ coord_train.map <- function(coord, scales) {
   }
 
   list(
-    x.raw = x.raw, y.raw = y.raw, orientation = orientation,
-    x.range = x.range, y.range = y.range, 
+    x.range = x.range, y.range = y.range, orientation = orientation,
+    x.proj = x.proj, y.proj = y.proj,
     x.major = x.major, x.minor = x.minor, x.labels = x.labels,
     y.major = y.major, y.minor = y.minor, y.labels = y.labels
   )
@@ -136,8 +136,8 @@ coord_train.map <- function(coord, scales) {
 
 #' @S3method coord_render_bg map
 coord_render_bg.map <- function(coord, details, theme) {    
-  xrange <- expand_range(details$x.raw, 0.2)
-  yrange <- expand_range(details$y.raw, 0.2)
+  xrange <- expand_range(details$x.range, 0.2)
+  yrange <- expand_range(details$y.range, 0.2)
   xgrid <- with(details, expand.grid(
     y = c(seq(yrange[1], yrange[2], len = 50), NA),
     x = x.major
@@ -167,7 +167,7 @@ coord_render_bg.map <- function(coord, details, theme) {
 coord_render_axis_h.map <- function(coord, details, theme) {
   x_intercept <- with(details, data.frame(
     x = x.major,
-    y = y.raw[1]
+    y = y.range[1]
   ))
   pos <- coord_transform(coord, x_intercept, details)
   
@@ -176,7 +176,7 @@ coord_render_axis_h.map <- function(coord, details, theme) {
 #' @S3method coord_render_axis_v map
 coord_render_axis_v.map <- function(coord, details, theme) {
   x_intercept <- with(details, data.frame(
-    x = x.raw[1],
+    x = x.range[1],
     y = y.major
   ))
   pos <- coord_transform(coord, x_intercept, details)
