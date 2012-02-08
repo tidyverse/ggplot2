b26fbb9c79a03938f2c3b9c9aad97a60a22bdca2
Winston Chang <winston@stdout.org>
2012-02-08 02:17:22 -0600

    geom_rug: Add control over which sides rugs are on

--- R/geom-rug.r
+++ R/geom-rug.r
@@ -1,36 +1,61 @@
 #' Marginal rug plots.
 #'
 #' @inheritParams geom_point
+#' @param sides A string that controls which sides of the plot the rugs appear on.
+#'   It can be set to a string containing any of \code{"trbl"}, for top, right,
+#'   bottom, and left.
 #' @export
 #' @examples
 #' p <- ggplot(mtcars, aes(x=wt, y=mpg))
 #' p + geom_point()
 #' p + geom_point() + geom_rug()
+#' p + geom_point() + geom_rug(sides="b")    # Rug on bottom only
+#' p + geom_point() + geom_rug(sides="trbl") # All four sides
 #' p + geom_point() + geom_rug(position='jitter')
-geom_rug <- function (mapping = NULL, data = NULL, stat = "identity", position = "identity", ...) { 
-  GeomRug$new(mapping = mapping, data = data, stat = stat, position = position, ...)
+geom_rug <- function (mapping = NULL, data = NULL, stat = "identity", position = "identity", sides = "bl", ...) {
+  GeomRug$new(mapping = mapping, data = data, stat = stat, position = position, sides = sides, ...)
 }
 
 GeomRug <- proto(Geom, {
   objname <- "rug"
 
-  draw <- function(., data, scales, coordinates, ...) {  
+  draw <- function(., data, scales, coordinates, sides, ...) {
     rugs <- list()
     data <- coord_transform(coordinates, data, scales)    
     if (!is.null(data$x)) {
-      rugs$x <- with(data, segmentsGrob(
-        x0 = unit(x, "native"), x1 = unit(x, "native"), 
-        y0 = unit(0, "npc"), y1 = unit(0.03, "npc"),
-        gp = gpar(col = alpha(colour, alpha), lty = linetype, lwd = size * .pt)
-      ))
+      if(grepl("b", sides)) {
+        rugs$x_b <- segmentsGrob(
+          x0 = unit(data$x, "native"), x1 = unit(data$x, "native"),
+          y0 = unit(0, "npc"), y1 = unit(0.03, "npc"),
+          gp = gpar(col = alpha(data$colour, data$alpha), lty = data$linetype, lwd = data$size * .pt)
+        )
+      }
+
+      if(grepl("t", sides)) {
+        rugs$x_t <- segmentsGrob(
+          x0 = unit(data$x, "native"), x1 = unit(data$x, "native"),
+          y0 = unit(1, "npc"), y1 = unit(0.97, "npc"),
+          gp = gpar(col = alpha(data$colour, data$alpha), lty = data$linetype, lwd = data$size * .pt)
+        )
+      }
     }  
 
     if (!is.null(data$y)) {
-      rugs$y <- with(data, segmentsGrob(
-        y0 = unit(y, "native"), y1 = unit(y, "native"), 
-        x0 = unit(0, "npc"), x1 = unit(0.03, "npc"),
-        gp = gpar(col = alpha(colour, alpha), lty = linetype, lwd = size * .pt)
-      ))
+      if(grepl("l", sides)) {
+        rugs$y_l <- segmentsGrob(
+          y0 = unit(data$y, "native"), y1 = unit(data$y, "native"),
+          x0 = unit(0, "npc"), x1 = unit(0.03, "npc"),
+          gp = gpar(col = alpha(data$colour, data$alpha), lty = data$linetype, lwd = data$size * .pt)
+        )
+      }
+
+      if(grepl("r", sides)) {
+        rugs$y_r <- segmentsGrob(
+          y0 = unit(data$y, "native"), y1 = unit(data$y, "native"),
+          x0 = unit(1, "npc"), x1 = unit(0.97, "npc"),
+          gp = gpar(col = alpha(data$colour, data$alpha), lty = data$linetype, lwd = data$size * .pt)
+        )
+      }
     }  
     
     gTree(children = do.call("gList", rugs))
