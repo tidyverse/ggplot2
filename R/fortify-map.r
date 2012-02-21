db27779b3835c552518eecff9704682e139cbd37
hadley <h.wickham@gmail.com>
2012-02-21 09:02:40 +0100

    Add missing try_require('maps')

--- R/fortify-map.r
+++ R/fortify-map.r
@@ -61,6 +61,7 @@ fortify.map <- function(model, data, ...) {
 #'   geom = "polygon")
 #' }
 map_data <- function(map, region = ".", exact = FALSE, ...) {
+  try_require("maps")
   fortify(map(map, region, exact = exact, plot = FALSE, fill = TRUE, ...))
 }
 
