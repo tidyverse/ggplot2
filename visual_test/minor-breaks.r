a510479b9f43e72dd7dae7d8b1e51e132785b622
Winston Chang <winston@stdout.org>
2012-03-16 18:57:46 -0500

    remove print statements from visual tests

--- visual_test/minor-breaks.r
+++ visual_test/minor-breaks.r
@@ -2,8 +2,8 @@ p <- ggplot(NULL, aes(1:3, 1:3)) + geom_point() +
   scale_x_continuous(breaks = 1:3, minor_breaks=c(1.25, 2.75)) +
   scale_y_continuous(breaks = 1:3, minor_breaks=c(1.25, 2.75))
 
-print(p)
-print(p + coord_polar())
+p
+p + coord_polar()
 
 df <- data.frame(
   date = seq(Sys.Date(), len=100, by="1 day")[sample(100, 50)],
@@ -13,14 +13,12 @@ df <- df[order(df$date), ]
 p <- qplot(date, price, data=df, geom="line") + 
   scale_x_date(labels = date_format("%m/%d"), breaks = date_breaks("month"), minor_breaks = date_breaks("week"))
 
-print(p)
-print(p + coord_polar())
+p
 
-p <- ggplot(NULL, aes(letters[1:3], 1:3)) + geom_point()
-print(p)
+p + coord_polar()
 
-p <- qplot(1:1e4, 1:1e4) + scale_x_continuous(trans = log2_trans()) + scale_y_log10()
-print(p)
+ggplot(NULL, aes(letters[1:3], 1:3)) + geom_point()
 
-p <- qplot(1:5, 1:5) + scale_x_continuous(trans = exp_trans(2)) + scale_y_continuous(trans = exp_trans(2))
-print(p)
+qplot(1:1e4, 1:1e4) + scale_x_continuous(trans = log2_trans()) + scale_y_log10()
+
+qplot(1:5, 1:5) + scale_x_continuous(trans = exp_trans(2)) + scale_y_continuous(trans = exp_trans(2))
