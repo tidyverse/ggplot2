f98fd616a513f0915579b78cbad528966182d673
Winston Chang <winston@stdout.org>
2012-05-25 12:49:49 -0500

    Convert element_xx so they return property lists

--- R/theme-elements.r
+++ R/theme-elements.r
@@ -5,7 +5,7 @@
 #' @export
 element_blank <- function() {
   structure(
-    function(...) zeroGrob(),
+    list(),
     class = c("element", "element_any")
   )  
 }
@@ -14,21 +14,15 @@ element_blank <- function() {
 #' 
 #' Most often used for backgrounds and borders.
 #' 
-#' @seealso \code{\link{rectGrob}} for underlying grid function
 #' @param fill fill colour
 #' @param colour border color
 #' @param size border size
 #' @param linetype border linetype
 #' @keywords dplot
 #' @export
-element_rect <- function(fill = NA, colour = "black", size = 0.5, linetype = 1) {
+element_rect <- function(fill = NULL, colour = NULL, size = NULL, linetype = NULL) {
   structure(
-    function(x = 0.5, y = 0.5, width = 1, height = 1, ...) {
-      rectGrob(
-        x, y, width, height, ...,
-        gp=gpar(lwd=size * .pt, col=colour, fill=fill, lty=linetype),
-      )
-    },
+    list(fill = fill, colour = colour, size = size, linetype = linetype),
     class = c("element", "element_rect")
   )
 }
@@ -37,21 +31,15 @@ element_rect <- function(fill = NA, colour = "black", size = 0.5, linetype = 1) 
 #'
 #' This element draws a line between two (or more) points
 #' 
-#' @seealso \code{\link{polylineGrob}} for underlying grid function,
-#'  \code{link{theme_segment}}
+#' @seealso \code{link{theme_segment}}
 #' @param colour line color
 #' @param size line size
 #' @param linetype line type
 #' @keywords dplot
 #' @export
-element_line <- function(colour = "black", size = 0.5, linetype = 1) {
+element_line <- function(colour = NULL, size = NULL, linetype = NULL) {
   structure(
-    function(x = 0:1, y = 0:1, ..., default.units = "npc") {
-      polylineGrob(
-        x, y, ..., default.units = default.units,
-        gp=gpar(lwd=size * .pt, col=colour, lty=linetype),
-      )
-    },
+    list(colour = colour, size = size, linetype = linetype),
     class = c("element", "element_line")
   )
 }
@@ -60,21 +48,15 @@ element_line <- function(colour = "black", size = 0.5, linetype = 1) {
 #'
 #' This element draws segments between a set of points
 #' 
-#' @seealso \code{\link{segmentsGrob}} for underlying grid function,
-#'   \code{link{theme_line}}
+#' @seealso \code{link{theme_line}}
 #' @param colour line color
 #' @param size line size
 #' @param linetype line type
 #' @keywords dplot
 #' @export
-element_segment <- function(colour = "black", size = 0.5, linetype = 1) {
+element_segment <- function(colour = NULL, size = NULL, linetype = NULL) {
   structure(
-    function(x0 = 0, y0 = 0, x1 = 1, y1 = 1, ...) {
-      segmentsGrob(
-        x0, y0, x1, y1, ..., default.units = "npc",
-        gp=gpar(col=colour, lty=linetype, lwd = size * .pt),
-      )
-    },
+    list(colour = colour, size = size, linetype = linetype),
     class = c("element", "element_segment")
   )
 }
@@ -82,7 +64,6 @@ element_segment <- function(colour = "black", size = 0.5, linetype = 1) {
 
 #' Theme element: text.
 #' 
-#' @seealso \code{\link{textGrob}} for underlying grid function
 #' @param family font family
 #' @param face font face ("plain", "italic", "bold")
 #' @param colour text colour
@@ -93,40 +74,12 @@ element_segment <- function(colour = "black", size = 0.5, linetype = 1) {
 #' @param lineheight line height
 #' @keywords dplot
 #' @export
-element_text <- function(family = "", face = "plain", colour = "black", size = 10, hjust = 0.5, vjust = 0.5, angle = 0, lineheight = 1.1) {
-
-  vj <- vjust
-  hj <- hjust
-  angle <- angle %% 360
-  
-  if (angle == 90) {
-    xp <- vj
-    yp <- hj
-  } else if (angle == 180) {
-    xp <- 1 - hj
-    yp <- vj
-  } else if (angle == 270) {
-    xp <- vj
-    yp <- 1 - hj
-  }else {
-    xp <- hj
-    yp <- vj
-  }
+element_text <- function(family = NULL, face = NULL, colour = NULL,
+  size = NULL, hjust = NULL, vjust = NULL, angle = NULL, lineheight = NULL) {
 
   structure(
-    function(label, x = xp, y = yp, ..., vjust = vj, hjust = hj, default.units = "npc") {
-
-      textGrob(
-        label, x, y, hjust = hjust, vjust = vjust, ...,
-        default.units = default.units,
-        gp = gpar(
-          fontsize = size, col = colour, 
-          fontfamily = family, fontface = face, 
-          lineheight = lineheight
-        ),
-        rot = angle
-      )
-    },
+    list(family = family, face = face, colour = colour, size = size,
+      hjust = hjust, vjust = vjust, angle = angle, lineheight = lineheight),
     class = c("element", "element_text")
   )
 }
