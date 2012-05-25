6fc02e007bf7c5b3b72c6d43cba753ab7ea9b90a
Winston Chang <winston@stdout.org>
2012-05-25 15:27:32 -0500

    Add some functions for rel()

--- R/theme.r
+++ R/theme.r
@@ -368,3 +368,9 @@ update_element <- function(name, ...) {
 rel <- function(x) {
   structure(x, class = "rel")
 }
+
+#' @S3method print rel
+print.rel <- function(x, ...) print(noquote(paste(x, " *", sep = "")))
+
+#' Reports whether x is a rel object
+is.rel <- function(x) inherits(x, "rel")
