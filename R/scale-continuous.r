c718e2d23efd311f5509ee872d833c0125595139
hadley <h.wickham@gmail.com>
2012-06-08 13:26:13 -0500

    Use na.value in scale_continuous. Fixes #522

--- R/scale-continuous.r
+++ R/scale-continuous.r
@@ -82,7 +82,8 @@ scale_y_continuous <- function(..., expand = waiver()) {
 # can tell the difference between continuous and discrete data.
 #' @S3method scale_map position_c
 scale_map.position_c <- function(scale, x, limits = scale_limits(scale)) {
-  as.numeric(scale$oob(x, limits))
+  scaled <- as.numeric(scale$oob(x, limits))
+  ifelse(!is.na(scaled), scaled, scale$na.value)
 }
 
 # Transformed scales ---------------------------------------------------------
