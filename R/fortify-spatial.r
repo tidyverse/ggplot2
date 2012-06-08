2c3e54ce3637d87a85f96f9bfed4477a6205bc75
hadley <h.wickham@gmail.com>
2012-06-08 09:49:00 -0500

    Remove gpclib dependency. And improve documentation for spatial fortify methods. Fixes #440

--- R/fortify-spatial.r
+++ R/fortify-spatial.r
@@ -3,20 +3,23 @@
 #' To figure out the correct variable name for region, inspect 
 #' \code{as.data.frame(model)}.
 #' 
-#' @S3method fortify SpatialPolygons
-#' @S3method fortify SpatialPolygonsDataFrame
-#' @S3method fortify Polygons
-#' @S3method fortify Polygon
-#' @S3method fortify SpatialLinesDataFrame
-#' @S3method fortify Lines
-#' @S3method fortify Line
 #' @param model \code{SpatialPolygonsDataFrame} to convert into a dataframe.
 #' @param data not used by this method
 #' @param region name of variable used to split up regions
 #' @param ... not used by this method
 #' @name fortify.sp
+#' @examples
+#' if (require("maptools")) {
+#'  sids <- system.file("shapes/sids.shp", package="maptools")
+#'  nc1 <- readShapePoly(sids, 
+#'    proj4string = CRS("+proj=longlat +datum=NAD27"))
+#'  nc1_df <- fortify(nc1) 
+#' }
 NULL
 
+#' @rdname fortify.sp
+#' @export
+#' @method fortify SpatialPolygonsDataFrame
 fortify.SpatialPolygonsDataFrame <- function(model, data, region = NULL, ...) {
   attr <- as.data.frame(model)
   # If not specified, split into regions based on first variable in attributes
@@ -26,12 +29,12 @@ fortify.SpatialPolygonsDataFrame <- function(model, data, region = NULL, ...) {
   }
   
   # Figure out how polygons should be split up into the region of interest
-  
   polys <- split(as.numeric(row.names(attr)), addNA(attr[, region], TRUE))
   cp <- polygons(model)
   
+  try_require("maptools")
+  
   # Union together all polygons that make up a region
-  try_require(c("gpclib", "maptools"))
   unioned <- unionSpatialPolygons(cp, invert(polys))
   
   coords <- fortify(unioned)
@@ -39,10 +42,16 @@ fortify.SpatialPolygonsDataFrame <- function(model, data, region = NULL, ...) {
   coords
 }
 
+#' @rdname fortify.sp
+#' @export
+#' @method fortify SpatialPolygons
 fortify.SpatialPolygons <- function(model, data, ...) {
   ldply(model@polygons, fortify)
 }
 
+#' @rdname fortify.sp
+#' @export
+#' @method fortify Polygons
 fortify.Polygons <- function(model, data, ...) {
   subpolys <- model@Polygons
   pieces <- ldply(seq_along(subpolys), function(i) {
@@ -59,6 +68,9 @@ fortify.Polygons <- function(model, data, ...) {
   })
 }
 
+#' @rdname fortify.sp
+#' @export
+#' @method fortify Polygon
 fortify.Polygon <- function(model, data, ...) {
   df <- as.data.frame(model@coords)
   names(df) <- c("long", "lat")
@@ -67,10 +79,16 @@ fortify.Polygon <- function(model, data, ...) {
   df
 }
 
+#' @rdname fortify.sp
+#' @export
+#' @method fortify SpatialLinesDataFrame
 fortify.SpatialLinesDataFrame <- function(model, data, ...) {
   ldply(model@lines, fortify)
 }
 
+#' @rdname fortify.sp
+#' @export
+#' @method fortify Lines
 fortify.Lines <- function(model, data, ...) {
   lines <- model@Lines
   pieces <- ldply(seq_along(lines), function(i) {
@@ -87,6 +105,9 @@ fortify.Lines <- function(model, data, ...) {
   })
 }
 
+#' @rdname fortify.sp
+#' @export
+#' @method fortify Line
 fortify.Line <- function(model, data, ...) {
   df <- as.data.frame(model@coords)
   names(df) <- c("long", "lat")
