fe59395ea4ed9ee88e942000de639a9e1bb0d0cd
Winston Chang <winston@stdout.org>
2012-06-25 23:44:39 -0500

    ecdf: Clean up example

--- R/stat-ecdf.r
+++ R/stat-ecdf.r
@@ -9,7 +9,7 @@
 #' @export
 #' @examples
 #' \donttest{
-#' qplot(c(rnorm(1000)), stat = "ecdf", geom = "step")
+#' qplot(rnorm(1000), stat = "ecdf", geom = "step")
 #' 
 #' df <- data.frame(x = c(rnorm(100, 0, 3), rnorm(100, 0, 10)),
 #'                  g = gl(2, 100))
