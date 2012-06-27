5b0e9cb6e40c8aaf70084d09ea904b9028f0202b
Winston Chang <winston@stdout.org>
2012-06-27 13:55:51 -0500

    Properly close device in tests

--- inst/tests/test-aes-setting.r
+++ inst/tests/test-aes-setting.r
@@ -12,8 +12,11 @@ test_that("Aesthetic parameters must match length of data", {
   
   set_colours("red")
   expect_error(set_colours(rep("red", 2)), "Incompatible lengths")
+  dev.off()  # Need to manually close device because of error
   expect_error(set_colours(rep("red", 3)), "Incompatible lengths")
+  dev.off()
   expect_error(set_colours(rep("red", 4)), "Incompatible lengths")
+  dev.off()
   set_colours(rep("red", 5))
   
   
