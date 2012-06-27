6fde6291e6fb9437b4a64504f7299bfb394a62e5
Winston Chang <winston@stdout.org>
2012-06-27 13:47:17 -0500

    Don't open on-screen windows in tests

--- inst/tests/test-aes-setting.r
+++ inst/tests/test-aes-setting.r
@@ -5,7 +5,9 @@ test_that("Aesthetic parameters must match length of data", {
   p <- ggplot(df, aes(x, y))
   
   set_colours <- function(colours) {
+    pdf(file=NULL)
     print(p + geom_point(colour = colours))
+    dev.off()
   }
   
   set_colours("red")
