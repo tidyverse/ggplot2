tidyverse/ggplot2

commit 0719eedf48668ea83950e6111d8c64f5bc175b4a

Don't try so hard for unique grob names. #459

R/utilities-grid.r
@@ -3,7 +3,7 @@
 # 
 # @keyword internal
 ggname <- function(prefix, grob) {
-  grob$name <- grobName(grob, prefix)
+  grob$name <- paste(prefix, class(grob)[1L], sep = ".")  
   grob
 }
 

commit 03a796248fab32e2d1e84b91da933bce31a8d011

Undo grob name change
This reverts 0719eed, which caused rendering problems for some geoms.

R/utilities-grid.r
@@ -3,7 +3,7 @@
 # 
 # @keyword internal
 ggname <- function(prefix, grob) {
-  grob$name <- paste(prefix, class(grob)[1L], sep = ".")  
+  grob$name <- grobName(grob, prefix)
   grob
 }
 