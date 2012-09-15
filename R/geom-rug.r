3a818a54410279335e62fbac6cad4dff6b2bf12a
Jeff Arnold <jeffrey.arnold@gmail.com>
2012-09-15 14:41:24 -0400

    geom_rug: rugwidth takes units objects

--- R/geom-rug.r
+++ R/geom-rug.r
@@ -7,8 +7,8 @@
 #' @param sides A string that controls which sides of the plot the rugs appear on.
 #'   It can be set to a string containing any of \code{"trbl"}, for top, right,
 #'   bottom, and left.
-#' @param rugwidth The width the rug segments. This should be a number in [0,1]
-#' in "npc" units. See the help for \code{\line{unit}}.
+#' @param rugwidth The width the rug segments. This should be a \code{\link{unit}}
+#' object.
 #'
 #' @export
 #' @examples
@@ -18,29 +18,33 @@
 #' p + geom_point() + geom_rug(sides="b")    # Rug on bottom only
 #' p + geom_point() + geom_rug(sides="trbl") # All four sides
 #' p + geom_point() + geom_rug(position='jitter')
-geom_rug <- function (mapping = NULL, data = NULL, stat = "identity", position = "identity", sides = "bl", rugwidth=0.03 ...) {
+geom_rug <- function (mapping = NULL, data = NULL, stat = "identity", position = "identity", sides = "bl", rugwidth=unit(0.03, "npc"), ...) {
   GeomRug$new(mapping = mapping, data = data, stat = stat, position = position, sides = sides, rugwidth=rugwidth, ...)
 }
 
 GeomRug <- proto(Geom, {
   objname <- "rug"
 
-  draw <- function(., data, scales, coordinates, sides, rugwidth=0.03, ...) {
+  draw <- function(., data, scales, coordinates, sides, rugwidth=units(0.03, "npc"), ...) {
     rugs <- list()
     data <- coord_transform(coordinates, data, scales)
     if (!is.null(data$x)) {
       if(grepl("b", sides)) {
+        y0b <- unit(0, "npc")
+        y1b <- y0b + rugwidth
         rugs$x_b <- segmentsGrob(
           x0 = unit(data$x, "native"), x1 = unit(data$x, "native"),
-          y0 = unit(0, "npc"), y1 = unit(rugwidth, "npc"),
+          y0 = y0b, y1 = y1b,
           gp = gpar(col = alpha(data$colour, data$alpha), lty = data$linetype, lwd = data$size * .pt)
         )
       }
 
       if(grepl("t", sides)) {
+        y0t <- unit(1, "npc")
+        y1t <- y0t - rugwidth
         rugs$x_t <- segmentsGrob(
           x0 = unit(data$x, "native"), x1 = unit(data$x, "native"),
-          y0 = unit(1, "npc"), y1 = unit(1 - rugwidth, "npc"),
+          y0 = y0t, y1 = y1t,
           gp = gpar(col = alpha(data$colour, data$alpha), lty = data$linetype, lwd = data$size * .pt)
         )
       }
@@ -48,17 +52,21 @@ GeomRug <- proto(Geom, {
 
     if (!is.null(data$y)) {
       if(grepl("l", sides)) {
+        x0l <- unit(0, "npc")
+        x1l <- x0l + rugwidth
         rugs$y_l <- segmentsGrob(
           y0 = unit(data$y, "native"), y1 = unit(data$y, "native"),
-          x0 = unit(0, "npc"), x1 = unit(rugwidth, "npc"),
+          x0 = x0l, x1 = x1l,
           gp = gpar(col = alpha(data$colour, data$alpha), lty = data$linetype, lwd = data$size * .pt)
         )
       }
 
       if(grepl("r", sides)) {
+        x0r = unit(1, "npc")
+        x1r = x0r - rugwidth
         rugs$y_r <- segmentsGrob(
           y0 = unit(data$y, "native"), y1 = unit(data$y, "native"),
-          x0 = unit(1, "npc"), x1 = unit(1 - rugwidth, "npc"),
+          x0 = x0r, x1 = x1r,
           gp = gpar(col = alpha(data$colour, data$alpha), lty = data$linetype, lwd = data$size * .pt)
         )
       }
