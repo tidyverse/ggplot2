308d77ab13cb7cbd59f5a68a128a53e4e9d09f2e
Winston Chang <winston@stdout.org>
2012-10-24 14:47:05 -0500

    gg_dep: export and add help

--- R/utilities.r
+++ R/utilities.r
@@ -197,16 +197,35 @@ if (packageVersion("plyr") <= package_version("1.7.1")) {
 }
 
 
-# Give a deprecation error, warning, or messsage, depending on version number.
-#
-# @param version The _last_ version of ggplot2 where this function was good
-#   (in other words, the last version where it was not deprecated).
-# @param message The message to print
+#' Give a deprecation error, warning, or messsage, depending on version number.
+#'
+#' Version numbers have the format <major>.<minor>.<subminor>, like 0.9.2.
+#' This function compares the current version number of ggplot2 against the
+#' specified \code{version}, which is the most recent version before the
+#' function (or other object) was deprecated.
+#'
+#' \code{gg_dep} will give an error, warning, or message, depending on the
+#' difference between the current ggplot2 version and the specified
+#' \code{version}.
+#'
+#' If the current major number is greater than \code{version}'s major number,
+#' or if the current minor number is more than 1 greater than \code{version}'s
+#' minor number, give an error.
+#'
+#' If the current minor number differs from \code{version}'s minor number by
+#' one, give a warning.
+#'
+#' If the current subminor number differs from \code{version}'s subminor
+#' number, print a message.
+#'
+#' @param version The last version of ggplot2 where this function was good
+#'   (in other words, the last version where it was not deprecated).
+#' @param message The message to print.
+#' @export
 gg_dep <- function(version, msg) {
   v <- as.package_version(version)
   cv <- packageVersion("ggplot2")
 
-  # Version number is major.minor.subminor, like 0.9.2
   # If current major number is greater than last-good major number, or if
   #  current minor number is more than 1 greater than last-good minor number,
   #  give error.
