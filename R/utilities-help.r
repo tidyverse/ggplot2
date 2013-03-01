87d9a62b8fe7405724ece6b894fd6b73866e6c6a
Winston Chang <winston@stdout.org>
2013-03-01 11:01:22 -0600

    Suppress warnings when generating help

--- R/utilities-help.r
+++ R/utilities-help.r
@@ -1,14 +1,15 @@
 aesthetics <- function(x) {
-	req_aes <- x$required_aes
-	def_aes <- names(x$default_aes())
-	def_aes <- setdiff(def_aes, req_aes)
-	if (length(req_aes) == 0){
-		return(sort(names(x$default_aes())))
-	}
-	if (length(def_aes) == 0){
-		return(paste("\\strong{", sort(x$required_aes), "}",sep = ""))
-	}
-	return(c(paste("\\strong{",sort(x$required_aes), "}", sep = ""), sort(def_aes)))
+  req_aes <- x$required_aes
+  def_aes <- names(x$default_aes())
+  def_aes <- setdiff(def_aes, req_aes)
+  if (length(req_aes) == 0){
+    # Suppress warnings which occur when sorting NULL
+    return(suppressWarnings(sort(names(x$default_aes()))))
+  }
+  if (length(def_aes) == 0){
+    return(paste("\\strong{", sort(x$required_aes), "}",sep = ""))
+  }
+  return(c(paste("\\strong{", sort(x$required_aes), "}", sep = ""), sort(def_aes)))
 }
 geom_aesthetics <- function(x) {
   aesthetics(Geom$find(x))
@@ -20,9 +21,9 @@ stat_aesthetics <- function(x) {
 rd_aesthetics <- function(type, name) {
   obj <- get(firstUpper(type))
   aes <- aesthetics(obj$find(name))
-  
-  paste("\\code{", type, "_", name, "} ", 
-    "understands the following aesthetics (required aesthetics are in bold):\n\n", 
+
+  paste("\\code{", type, "_", name, "} ",
+    "understands the following aesthetics (required aesthetics are in bold):\n\n",
     "\\itemize{\n",
     paste("  \\item \\code{", aes, "}", collapse = "\n", sep = ""),
     "\n}\n", sep = "")
