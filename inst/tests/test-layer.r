efa35f97b9cdf900dc51c91e7f12a9dd5cdf439e
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-02-25 16:32:51 +0100

    remove unnecessary namespace prefix from the tests

--- inst/tests/test-layer.r
+++ inst/tests/test-layer.r
@@ -1,7 +1,7 @@
 context("Layer")
 
 test_that("Correctly decide if a variable is a calculated aesthetic", {
-  expect_true(ggplot2:::is_calculated_aes(aes(x=..density..)))
-  expect_false(ggplot2:::is_calculated_aes(aes(x=a..x..b)))
-  expect_equal(as.character(ggplot2:::strip_dots(aes(x=..density..))), "density")
+  expect_true(is_calculated_aes(aes(x=..density..)))
+  expect_false(is_calculated_aes(aes(x=a..x..b)))
+  expect_equal(as.character(strip_dots(aes(x=..density..))), "density")
 })
