448d78e6f7cf8b77b1d0958a675c9138c767fa0a
Jean-Olivier Irisson <irisson@normalesup.org>
2014-03-17 13:43:38 +0100

    Fix coding style for brewer scales
    Add spaces around `=`

--- R/scale-brewer.r
+++ R/scale-brewer.r
@@ -20,7 +20,7 @@
 #' @export
 #' @examples
 #' dsamp <- diamonds[sample(nrow(diamonds), 1000), ]
-#' (d <- qplot(carat, price, data=dsamp, colour=clarity))
+#' (d <- qplot(carat, price, data = dsamp, colour = clarity))
 #'
 #' # Change scale label
 #' d + scale_colour_brewer()
@@ -28,16 +28,16 @@
 #' d + scale_colour_brewer(expression(clarity[beta]))
 #'
 #' # Select brewer palette to use, see ?scales::brewer_pal for more details
-#' d + scale_colour_brewer(type="seq")
-#' d + scale_colour_brewer(type="seq", palette=3)
+#' d + scale_colour_brewer(type = "seq")
+#' d + scale_colour_brewer(type = "seq", palette = 3)
 #'
-#' d + scale_colour_brewer(palette="Blues")
-#' d + scale_colour_brewer(palette="Set1")
+#' d + scale_colour_brewer(palette = "Blues")
+#' d + scale_colour_brewer(palette = "Set1")
 #'
 #' # scale_fill_brewer works just the same as
 #' # scale_colour_brewer but for fill colours
-#' ggplot(diamonds, aes(x=price, fill=cut)) +
-#'   geom_histogram(position="dodge", binwidth=1000) +
+#' ggplot(diamonds, aes(x = price, fill = cut)) +
+#'   geom_histogram(position = "dodge", binwidth = 1000) +
 #'   scale_fill_brewer()
 #'
 #' # Generate map data
@@ -46,14 +46,14 @@
 #' names(volcano3d) <- c("x", "y", "z")
 #'
 #' # Basic plot
-#' v <- ggplot() + geom_tile(aes(x=x, y=y, fill=z), data=volcano3d)
+#' v <- ggplot() + geom_tile(aes(x = x, y = y, fill = z), data = volcano3d)
 #' v
 #' v + scale_fill_distiller()
-#' v + scale_fill_distiller(palette=2)
-#' v + scale_fill_distiller(type="div")
-#' v + scale_fill_distiller(palette="Spectral")
-#' v + scale_fill_distiller(palette="Spectral", trans="reverse")
-#' v + scale_fill_distiller(type="qual")
+#' v + scale_fill_distiller(palette = 2)
+#' v + scale_fill_distiller(type = "div")
+#' v + scale_fill_distiller(palette = "Spectral")
+#' v + scale_fill_distiller(palette = "Spectral", trans = "reverse")
+#' v + scale_fill_distiller(type = "qual")
 #' # Not appropriate for continuous data, issues a warning
 scale_colour_brewer <- function(..., type = "seq", palette = 1) {
   discrete_scale("colour", "brewer", brewer_pal(type, palette), ...)
@@ -71,7 +71,7 @@ scale_colour_distiller <- function(..., type = "seq", palette = 1, values = NULL
   # warn about using a qualitative brewer palette to generate the gradient
   type <- match.arg(type, c("seq", "div", "qual"))
   if (type == "qual") {
-    warning("Using a discrete colour palette in a continuous scale.\n  Consider using type=\"seq\" or type=\"div\" instead")
+    warning("Using a discrete colour palette in a continuous scale.\n  Consider using type = \"seq\" or type = \"div\" instead")
   }
   continuous_scale("colour", "distiller",
     gradient_n_pal(brewer_pal(type, palette)(6), values, space), na.value = na.value, ...)
@@ -83,14 +83,14 @@ scale_colour_distiller <- function(..., type = "seq", palette = 1, values = NULL
 scale_fill_distiller <- function(..., type = "seq", palette = 1, values = NULL, space = "Lab", na.value = "grey50") {
   type <- match.arg(type, c("seq", "div", "qual"))
   if (type == "qual") {
-    warning("Using a discrete colour palette in a continuous scale.\n  Consider using type=\"seq\" or type=\"div\" instead")
+    warning("Using a discrete colour palette in a continuous scale.\n  Consider using type = \"seq\" or type = \"div\" instead")
   }
   continuous_scale("fill", "distiller",
     gradient_n_pal(brewer_pal(type, palette)(6), values, space), na.value = na.value, ...)
 }
 
 # icon.brewer <- function() {
-#   rectGrob(c(0.1, 0.3, 0.5, 0.7, 0.9), width=0.21, 
-#     gp=gpar(fill=RColorBrewer::brewer.pal(5, "PuOr"), col=NA)
+#   rectGrob(c(0.1, 0.3, 0.5, 0.7, 0.9), width = 0.21, 
+#     gp = gpar(fill = RColorBrewer::brewer.pal(5, "PuOr"), col = NA)
 #   )
 # }
