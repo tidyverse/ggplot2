cfc3cbe915029719bea453e0a353e7f5af430bc6
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-03-24 00:10:41 +0100

    add failing test

--- inst/tests/test-layer.r
+++ inst/tests/test-layer.r
@@ -4,4 +4,7 @@ test_that("Correctly decide if a variable is a calculated aesthetic", {
   expect_true(is_calculated_aes(aes(x=..density..)))
   expect_false(is_calculated_aes(aes(x=a..x..b)))
   expect_equal(as.character(strip_dots(aes(x=..density..))), "density")
+  expect_true(is_calculated_aes(aes(x=mean(..density..))))
+  expect_false(is_calculated_aes(aes(x=mean(a..x..b))))
+  expect_equal(as.character(strip_dots(aes(x=mean(..density..)))), "mean(density)")
 })
