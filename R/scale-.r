ecf230c8e57b4c691b72907e28916f52ecca83d5
tonytonov <tonytonov@tonytonov-VirtualBox.(none)>
2014-04-28 15:35:17 +0400

    Throws error if lower limit greater than upper for continuous scales; fixes #906

--- R/scale-.r
+++ R/scale-.r
@@ -57,9 +57,12 @@ continuous_scale <- function(aesthetics, scale_name, palette, name = NULL, break
   if (is.null(breaks) && !is_position_aes(aesthetics) && guide != "none") {
     guide <- "none"
   }
-
+  
   trans <- as.trans(trans)
   if (!is.null(limits)) {
+      if (length(limits) == 2 & limits[1] > limits[2]) {
+        stop("Lower limit greater than upper for continuous scale")
+      }
     limits <- trans$trans(limits)
   }
 
