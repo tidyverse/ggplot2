90cddc110ea24b72877ce1457c5c5c88b1c4fae5
Anton Antonov <tonytonov@gmail.com>
2014-05-21 02:37:31 -0700

    Fix for reverse scales and NAs in limits

--- R/scale-.r
+++ R/scale-.r
@@ -59,13 +59,15 @@ continuous_scale <- function(aesthetics, scale_name, palette, name = NULL, break
   }
   
   trans <- as.trans(trans)
-  if (!is.null(limits)) {
-      if (length(limits) == 2 & limits[1] > limits[2]) {
-        stop("Lower limit greater than upper for continuous scale")
-      }
+  if (!is.null(limits) & length(limits) == 2) {
+    if (trans$name != "reverse" & 
+          !any(is.na(limits)) &
+          limits[1] > limits[2]) {
+      stop("Lower limit greater than upper for continuous scale")
+    }
     limits <- trans$trans(limits)
   }
-
+  
   structure(list(
     call = match.call(),
 
