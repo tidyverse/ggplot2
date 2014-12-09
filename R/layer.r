7adb43166109f1464b164c1cd9e3ab20c3a077f6
Sven E. Templer <sven.templer@gmail.com>
2014-12-09 21:48:39 +0100

    layer.r typo: added full stop

--- R/layer.r
+++ R/layer.r
@@ -158,7 +158,7 @@ Layer <- proto(expr = {
 
     wrong <- lengths != 1 & lengths != n
     if (any(wrong)) {
-      stop("Aesthetics must either be length one, or the same length as the data",
+      stop("Aesthetics must either be length one, or the same length as the data.",
         "\nProblems: ", paste(aesthetics[wrong], collapse = ", "), call. = FALSE)
     }
 
