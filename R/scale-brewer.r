204ef9740003ddf70545cf093fe97ac40c4ad597
Jean-Olivier Irisson <irisson@normalesup.org>
2015-03-31 13:36:36 +0200

    Reverse brewer palettes by default
    Sequential brewer palettes (which are the most appropriate for continuous
    interpolation by scale_*_distiller) are presented from dark to light
    luminance on the colorbrewer website but created from light to dark
    by brewer.pal.
    Similarly, some diverging palettes have some sort of intuitive order which
    is not inverted by the combination of brewer.pal and continuous_scale
    
    Reversing the colours gives the expected result:
    - light for higher values
    - blue for low values, red for high values.

--- R/scale-brewer.r
+++ R/scale-brewer.r
@@ -74,7 +74,7 @@ scale_colour_distiller <- function(..., type = "seq", palette = 1, values = NULL
     warning("Using a discrete colour palette in a continuous scale.\n  Consider using type = \"seq\" or type = \"div\" instead", call. = FALSE)
   }
   continuous_scale("colour", "distiller",
-    gradient_n_pal(brewer_pal(type, palette)(6), values, space), na.value = na.value, ...)
+    gradient_n_pal(rev(brewer_pal(type, palette)(6)), values, space), na.value = na.value, ...)
   # NB: 6 colours per palette gives nice gradients; more results in more saturated colours which do not look as good
 }
 
@@ -86,7 +86,7 @@ scale_fill_distiller <- function(..., type = "seq", palette = 1, values = NULL, 
     warning("Using a discrete colour palette in a continuous scale.\n  Consider using type = \"seq\" or type = \"div\" instead", call. = FALSE)
   }
   continuous_scale("fill", "distiller",
-    gradient_n_pal(brewer_pal(type, palette)(6), values, space), na.value = na.value, ...)
+    gradient_n_pal(rev(brewer_pal(type, palette)(6)), values, space), na.value = na.value, ...)
 }
 
 # icon.brewer <- function() {
