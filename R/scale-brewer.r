9c65dcba70c931331c64787e777551e34668a5b5
Jean-Olivier Irisson <irisson@normalesup.org>
2015-03-31 13:37:47 +0200

    Use a continuous colourbar guide with scale_*_distiller

--- R/scale-brewer.r
+++ R/scale-brewer.r
@@ -67,26 +67,26 @@ scale_fill_brewer <- function(..., type = "seq", palette = 1) {
 
 #' @export
 #' @rdname scale_brewer
-scale_colour_distiller <- function(..., type = "seq", palette = 1, values = NULL, space = "Lab", na.value = "grey50") {
+scale_colour_distiller <- function(..., type = "seq", palette = 1, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar") {
   # warn about using a qualitative brewer palette to generate the gradient
   type <- match.arg(type, c("seq", "div", "qual"))
   if (type == "qual") {
     warning("Using a discrete colour palette in a continuous scale.\n  Consider using type = \"seq\" or type = \"div\" instead", call. = FALSE)
   }
   continuous_scale("colour", "distiller",
-    gradient_n_pal(rev(brewer_pal(type, palette)(6)), values, space), na.value = na.value, ...)
+    gradient_n_pal(rev(brewer_pal(type, palette)(6)), values, space), na.value = na.value, guide = guide, ...)
   # NB: 6 colours per palette gives nice gradients; more results in more saturated colours which do not look as good
 }
 
 #' @export
 #' @rdname scale_brewer
-scale_fill_distiller <- function(..., type = "seq", palette = 1, values = NULL, space = "Lab", na.value = "grey50") {
+scale_fill_distiller <- function(..., type = "seq", palette = 1, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar") {
   type <- match.arg(type, c("seq", "div", "qual"))
   if (type == "qual") {
     warning("Using a discrete colour palette in a continuous scale.\n  Consider using type = \"seq\" or type = \"div\" instead", call. = FALSE)
   }
   continuous_scale("fill", "distiller",
-    gradient_n_pal(rev(brewer_pal(type, palette)(6)), values, space), na.value = na.value, ...)
+    gradient_n_pal(rev(brewer_pal(type, palette)(6)), values, space), na.value = na.value, guide = guide, ...)
 }
 
 # icon.brewer <- function() {
