58557bef8bc664ae7c7dbbd28bcdba215727f8c4
Michael Sachs <sachsmc@gmail.com>
2015-04-02 09:54:09 -0400

    Documentation and missing se parameter

--- R/stat-km.R
+++ R/stat-km.R
@@ -1,14 +1,31 @@
-stat_km <- function (mapping = NULL, data = NULL, geom = "km", position = "identity", ...) {
-  StatKm$new(mapping = mapping, data = data, geom = geom, position = position, ...)
+#' Kaplan Meier Estimate of Survival
+#'
+#' @inheritParams stat_identity
+#' @param se display confidence interval around KM curve? (TRUE by default, use
+#'   \code{conf.int} to control significance level which is 0.95 by default)
+#' @param ... Other arguments passed to \link{survival::survfit.formula}
+#' @return a data.frame with additional columns: \item{x}{x in data}
+#'   \item{y}{Kaplan-Meier Survival Estimate at x} \item{ymin}{Lower confidence
+#'   limit of KM curve, if \code{se = TRUE}} \item{ymax}{Upper confidence limit
+#'   of KM curve, if \code{se = FALSE}}
+#' @export
+#' @examples
+#' \donttest{
+#' ggplot(survival::lung, aes(time, status, color = sex)) + stat_km()
+#' qplot(survival::lung, time, status = status, geom = "km")
+#' }
+
+stat_km <- function (mapping = NULL, data = NULL, se = TRUE, geom = "km", position = "identity", ...) {
+  StatKm$new(mapping = mapping, data = data, se = se, geom = geom, position = position, ...)
 }
 
 StatKm <- proto(Stat, {
   objname <- "km"
 
-  calculate <- function(., data, scales, mark.time = TRUE, ...) {
+  calculate <- function(., data, scales, se, ...) {
 
     try_require("survival")
-    sf <- survfit(Surv(data$x, data$status) ~ 1, ...)
+    sf <- survfit(Surv(data$x, data$status) ~ 1, se.fit = se, ...)
     x <- sf$time
     y <- sf$surv
 
