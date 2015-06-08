e6c66323e5766baea31216c25fe39410c8cf5b2b
hadley <h.wickham@gmail.com>
2015-06-08 16:20:19 -0500

    Export alpha from scales package.
    Fixes #1107

--- R/utilities.r
+++ R/utilities.r
@@ -1,3 +1,14 @@
+#' Create a transparent colour.
+#'
+#' @name alpha
+#' @export
+#' @examples
+#' ggplot(mpg, aes(displ, hwy)) +
+#'   geom_point(alpha = 0.5, colour = "blue")
+#'
+#' ggplot(mpg, aes(displ, hwy)) +
+#'   geom_point(colour = alpha("blue", 0.5))
+NULL
 
 # Null default
 # Analog of || from ruby
