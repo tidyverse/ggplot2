8da03e6a3ee1f932c27303b93f7eca693e6793b1
hadley <h.wickham@gmail.com>
2015-06-18 10:25:14 -0500

    Reduce tick length in theme_minimal

--- R/theme-defaults.r
+++ R/theme-defaults.r
@@ -185,7 +185,8 @@ theme_minimal <- function(base_size = 12, base_family = "") {
       panel.border      = element_blank(),
       strip.background  = element_blank(),
       plot.background   = element_blank(),
-      axis.ticks        = element_blank()
+      axis.ticks        = element_blank(),
+      axis.ticks.length = unit(0, "lines")
     )
 }
 
