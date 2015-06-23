c65f9eb75e19d3f91fa310c623123379cff0e7c9
Brian Diggs <diggsb@ohsu.edu>
2015-06-22 23:17:40 -0700

    Shrunk testing data set; added actual expect_ test
    Reduced the test data set to just two points and 
    rounded to fewer decimals.
    
    Previously, the test was implicitly does-not-
    throw-an-error which that code would have done, 
    sepcifically 
    Error in Ops.POSIXt((x - from[1]), diff(from)) : 
      '/' not defined for "POSIXt" objects
    Instead, went one step up tot he ggplot_build
    object and checked for specific names in the
    data.

--- inst/tests/test-annotate.r
+++ inst/tests/test-annotate.r
@@ -1,24 +1,21 @@
 context("annotate")
 
 test_that("dates in segment annotation work", {
-  dt <-
-    structure(list(month = structure(c(1356998400, 1359676800, 1362096000,
-      1364774400, 1367366400, 1370044800, 1372636800, 1375315200, 1377993600,
-      1380585600, 1383264000, 1385856000), tzone = "UTC", class = c("POSIXct",
-      "POSIXt")), total = c(-5.4586134857604, -13.7043187714622, 3.25254944094792,
-      -10.3260511491301, 6.44178978776937, 2.46127352381964, -1.98673390397442,
-      -16.3227453308624, 11.7148615457185, -2.23934628094151, 5.5084335509504,
-      -10.7700077344507)), row.names = c(NA, -12L), class = "data.frame",
-      .Names = c("month", "total"))
+
+  dt <- structure(list(month = structure(c(1364774400, 1377993600),
+      class = c("POSIXct", "POSIXt"), tzone = "UTC"), total = c(-10.3,
+      11.7)), .Names = c("month", "total"), row.names = c(NA, -2L), class =
+      "data.frame")
 
   p <- qplot(month, total, data = dt)
 
-  ggplot_gtable(ggplot_build(p + annotate("segment",
-               x = as.POSIXct("2013-04-01"),
-               xend = as.POSIXct("2013-07-01"),
-               y = -10,
-               yend = 10)))
+  gb <- ggplot_build(p + annotate("segment",
+                                  x = as.POSIXct("2013-04-01"),
+                                  xend = as.POSIXct("2013-07-01"),
+                                  y = -10,
+                                  yend = 10))
 
+  expect_true(all(c("xend", "yend") %in% names(gb$data[[2]])))
 })
 
 test_that("segment annotations transform with scales", {
