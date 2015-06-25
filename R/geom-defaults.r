d3005593d9f222d4d89994ccf29b1032532c8538
Winston Chang <winston@stdout.org>
2015-06-25 15:47:37 -0500

    Fix update_geom_defaults and update_stat_defaults

--- R/geom-defaults.r
+++ R/geom-defaults.r
@@ -9,20 +9,32 @@
 #' update_geom_defaults("point", list(colour = "black"))
 #' @rdname update_defaults
 update_geom_defaults <- function(geom, new) {
-  g <- Geom$find(geom)
+  # R6 TODO: Avoid instantiation. This will not work currently.
+  g <- Geom$new()$find(geom)$new()
   old <- g$default_aes()
 
   aes <- defaults(new, old)
 
-  g$default_aes <- eval(substitute(function() aes, list(aes = aes)))
+  # R6 TODO: This is is a workaround until we figure out a way to do singletons
+  # g$default_aes <- eval(substitute(function() aes, list(aes = aes)))
+  Geom$new()$find(geom)$set("public", "default_aes",
+    eval(substitute(function() aes, list(aes = aes))),
+    overwrite = TRUE
+  )
 }
 
 #' @rdname update_defaults
 #' @export
 update_stat_defaults <- function(stat, new) {
-  g <- Stat$find(stat)
+  # R6 TODO: Avoid instantiation. This will not work currently.
+  g <- Stat$new()$find(stat)$new()
   old <- g$default_aes()
 
   aes <- defaults(new, old)
-  g$default_aes <- eval(substitute(function() aes, list(aes = aes)))
+  # g$default_aes <- eval(substitute(function() aes, list(aes = aes)))
+  # R6 TODO: This is is a workaround until we figure out a way to do singletons
+  Stat$new()$find(stat)$set("public", "default_aes",
+    eval(substitute(function() aes, list(aes = aes))),
+    overwrite = TRUE
+  )
 }
