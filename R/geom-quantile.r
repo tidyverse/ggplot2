7630f2674ebaea46f0d9be091d251ea24e179047
hadley <h.wickham@gmail.com>
2015-07-22 10:11:24 -0500

    Combine docs for geom_quantile and stat_quantile

--- R/geom-quantile.r
+++ R/geom-quantile.r
@@ -8,13 +8,27 @@
 #' @export
 #' @inheritParams geom_point
 #' @inheritParams geom_path
-#' @seealso See \code{\link{stat_quantile}} for examples.
+#' @param geom,stat Use to override the default connection between
+#'   \code{geom_quantile} and \code{stat_quantile}.
 #' @examples
-#' # See stat_quantile for examples
-geom_quantile <- function (mapping = NULL, data = NULL, stat = "quantile",
+#' m <- ggplot(mpg, aes(displ, 1 / hwy)) + geom_point()
+#' m + geom_quantile()
+#' m + geom_quantile(quantiles = 0.5)
+#' q10 <- seq(0.05, 0.95, by = 0.05)
+#' m + geom_quantile(quantiles = q10)
+#'
+#' # You can also use rqss to fit smooth quantiles
+#' m + geom_quantile(method = "rqss")
+#' # Note that rqss doesn't pick a smoothing constant automatically, so
+#' # you'll need to tweak lambda yourself
+#' m + geom_quantile(method = "rqss", lambda = 1)
+#'
+#' # Set aesthetics to fixed value
+#' m + geom_quantile(colour = "red", size = 2, alpha = 0.5)
+geom_quantile <- function(mapping = NULL, data = NULL, stat = "quantile",
   position = "identity", lineend = "butt", linejoin = "round", linemitre = 1,
-  na.rm = FALSE, show_guide = NA, inherit.aes = TRUE, ...)
-{
+  na.rm = FALSE, show_guide = NA, inherit.aes = TRUE, ...) {
+
   Layer$new(
     data = data,
     mapping = mapping,
@@ -39,8 +53,6 @@ GeomQuantile <- proto2(
     default_aes = defaults(
       aes(weight = 1, colour = "#3366FF", size = 0.5),
       GeomPath$default_aes
-    ),
-
-    guide_geom = function(self) "path"
+    )
   )
 )
--- R/stat-quantile.r
+++ R/stat-quantile.r
@@ -1,8 +1,3 @@
-#' Continuous quantiles.
-#'
-#' @section Aesthetics:
-#' \Sexpr[results=rd,stage=build]{ggplot2:::rd_aesthetics("stat", "quantile")}
-#'
 #' @param quantiles conditional quantiles of y to calculate and display
 #' @param formula formula relating y variables to x variables
 #' @param method Quantile regression method to use.  Currently only supports
@@ -13,35 +8,7 @@
 #' @return a data.frame with additional columns:
 #'   \item{quantile}{quantile of distribution}
 #' @export
-#' @examples
-#' \donttest{
-#' if (require("ggplot2movies")) {
-#' msamp <- movies[sample(nrow(movies), 1000), ]
-#' m <- ggplot(msamp, aes(year, rating)) + geom_point()
-#' m + stat_quantile()
-#' m + stat_quantile(quantiles = 0.5)
-#' q10 <- seq(0.05, 0.95, by=0.05)
-#' m + stat_quantile(quantiles = q10)
-#'
-#' # You can also use rqss to fit smooth quantiles
-#' m + stat_quantile(method = "rqss")
-#' # Note that rqss doesn't pick a smoothing constant automatically, so
-#' # you'll need to tweak lambda yourself
-#' m + stat_quantile(method = "rqss", lambda = 10)
-#' m + stat_quantile(method = "rqss", lambda = 100)
-#'
-#' # Use 'votes' as weights for the quantile calculation
-#' m + stat_quantile(aes(weight=votes))
-#'
-#' # Change scale
-#' m + stat_quantile(aes(colour = ..quantile..), quantiles = q10)
-#' m + stat_quantile(aes(colour = ..quantile..), quantiles = q10) +
-#'   scale_colour_gradient2(midpoint = 0.5)
-#'
-#' # Set aesthetics to fixed value
-#' m + stat_quantile(colour = "red", size = 2, linetype = 2)
-#' }
-#' }
+#' @rdname geom_quantile
 stat_quantile <- function (mapping = NULL, data = NULL, geom = "quantile",
   position = "identity", quantiles = c(0.25, 0.5, 0.75), formula = NULL,
   method = "rq", na.rm = FALSE, show_guide = NA, inherit.aes = TRUE, ...)
