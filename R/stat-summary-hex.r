bd1be648b26b3c5ddc6cc5c1d3790da32f467379
hadley <h.wickham@gmail.com>
2015-07-23 13:59:25 -0500

    Clean up of stat_summary_hex.
    Includes and fixes #1156

--- R/stat-summary-hex.r
+++ R/stat-summary-hex.r
@@ -1,26 +1,24 @@
 #' Apply function for 2D hexagonal bins.
 #'
+#' \code{stat_summary2d} is a hexagonal variation of \code{\link{stat_summary}}.
+#' The data are divided into hexagonl bins defined by \code{x} and \code{y}.
+#' The values of \code{z} in each cell is are summarised with \code{fun}.
+#'
 #' @section Aesthetics:
-#' \Sexpr[results=rd,stage=build]{ggplot2:::rd_aesthetics("stat", "summary_hex")}
-#'
-#' \code{stat_summary2d} is hexagonal version of \code{\link{stat_summary}}. The data are devided by \code{x} and \code{y}.
-#' \code{z} in each cell is passed to arbitral summary function.
-#'
 #' \code{stat_summary_hex} requires the following aesthetics:
-#'
 #' \itemize{
 #'  \item \code{x}: horizontal position
 #'  \item \code{y}: vertical position
 #'  \item \code{z}: value passed to the summary function
 #' }
 #'
-#' @seealso \code{\link{stat_summary2d}} for rectangular summarization. \code{\link{stat_bin2d}} for the hexagon-ing options.
-#' @title Apply function for 2D hexagonal bins.
+#' @seealso \code{\link{stat_summary2d}} for rectangular summarization.
+#'   \code{\link{stat_bin2d}} for the hexagon-ing options.
 #' @inheritParams stat_identity
-#' @param bins see \code{\link{stat_binhex}}
+#' @inheritParams stat_binhex
 #' @param drop drop if the output of \code{fun} is \code{NA}.
 #' @param fun function for summary.
-#' @param ... parameters passed to \code{fun}
+#' @param fun.args A list of extra arguments to pass to \code{fun}
 #' @export
 #' @examples
 #' d <- ggplot(diamonds, aes(carat, depth, z = price))
@@ -28,11 +26,12 @@
 #'
 #' # Specifying function
 #' d + stat_summary_hex(fun = function(x) sum(x^2))
-#' d + stat_summary_hex(fun = var, na.rm = TRUE)
-stat_summary_hex <- function (mapping = NULL, data = NULL, geom = "hex",
-  position = "identity", bins = 30, drop = TRUE, fun = mean, show_guide = NA,
-  inherit.aes = TRUE, ...)
-{
+#' d + stat_summary_hex(fun = var)
+#' d + stat_summary_hex(fun = "quantile", fun.args = list(probs = 0.5))
+stat_summary_hex <- function(mapping = NULL, data = NULL, geom = "hex",
+                             position = "identity", bins = 30, drop = TRUE,
+                             fun = "mean", fun.args = list(), show_guide = NA,
+                             inherit.aes = TRUE, ...) {
   layer(
     data = data,
     mapping = mapping,
@@ -44,7 +43,8 @@ stat_summary_hex <- function (mapping = NULL, data = NULL, geom = "hex",
     stat_params = list(
       bins = bins,
       drop = drop,
-      fun = fun
+      fun = fun,
+      fun.args = fun.args
     ),
     params = list(...)
   )
@@ -56,10 +56,9 @@ StatSummaryHex <- proto2("StatSummaryHex", Stat,
   required_aes = c("x", "y", "z"),
 
   calculate = function(data, scales, binwidth = NULL, bins = 30, drop = TRUE,
-    fun = mean, ...)
-  {
-    try_require("hexbin")
-    data <- remove_missing(data, FALSE, c("x", "y", "z"), name="stat_summary_hex")
+                       fun = mean, fun.args = list(), ...) {
+    data <- remove_missing(data, FALSE, c("x", "y", "z"),
+      name = "stat_summary_hex")
 
     if (is.null(binwidth)) {
       binwidth <- c(
@@ -67,8 +66,6 @@ StatSummaryHex <- proto2("StatSummaryHex", Stat,
         diff(scale_dimension(scales$y, c(0, 0))) / bins
       )
     }
-
-    try_require("hexbin")
 
     # Convert binwidths into bounds + nbins
     x <- data$x
@@ -87,16 +84,16 @@ StatSummaryHex <- proto2("StatSummaryHex", Stat,
     ybins <- diff(ybnds) / binwidth[2]
 
     # Call hexbin
-    hb <- hexbin(
+    hb <- hexbin::hexbin(
       x, xbnds = xbnds, xbins = xbins,
       y, ybnds = ybnds, shape = ybins / xbins,
       IDs = TRUE
     )
 
-    value <- tapply(data$z, hb@cID, fun, ...)
+    value <- do.call(tapply, c(list(quote(data$z), quote(hb@cID), quote(fun)), fun.args))
 
     # Convert to data frame
-    ret <- data.frame(hcell2xy(hb), value)
+    ret <- data.frame(hexbin::hcell2xy(hb), value)
     if (drop) ret <- na.omit(ret)
     ret
   }
