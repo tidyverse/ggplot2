2d6df440ea518cc394e1a239719478204db6ced3
hadley <h.wickham@gmail.com>
2015-07-23 09:01:50 -0500

    Document and export key drawing functions #1192

--- R/legend-draw.r
+++ R/legend-draw.r
@@ -1,3 +1,19 @@
+#' Key drawing functions
+#'
+#' Each Geom has an associated function that draws the key when the geom needs
+#' to be displayed in a legend. These are the options built into ggplot2.
+#'
+#' @return A grid grob.
+#' @param self The Geom object
+#' @param data A single row data frame containing the scaled aesthetics to
+#'   display in this key
+#' @param params A list of additional parameters supplied to the geom.
+#' @keywords internal
+#' @name draw_key
+NULL
+
+#' @export
+#' @rdname draw_key
 legend_point <- function(self, data, params) {
   pointsGrob(0.5, 0.5,
     pch = data$shape,
@@ -10,6 +26,8 @@ legend_point <- function(self, data, params) {
   )
 }
 
+#' @export
+#' @rdname draw_key
 legend_abline <- function(self, data, params) {
   segmentsGrob(0, 0, 1, 1,
     gp = gpar(
@@ -21,6 +39,8 @@ legend_abline <- function(self, data, params) {
   )
 }
 
+#' @export
+#' @rdname draw_key
 legend_polygon <- function(self, data, params) {
   grobTree(
     rectGrob(gp = gpar(
@@ -37,10 +57,14 @@ legend_polygon <- function(self, data, params) {
   )
 }
 
+#' @export
+#' @rdname draw_key
 legend_blank <- function(self, data, params) {
   zeroGrob()
 }
 
+#' @export
+#' @rdname draw_key
 legend_boxplot <- function(self, data, params) {
   grobTree(
     linesGrob(0.5, c(0.1, 0.25)),
@@ -56,6 +80,8 @@ legend_boxplot <- function(self, data, params) {
   )
 }
 
+#' @export
+#' @rdname draw_key
 legend_crossbar <- function(self, data, params) {
   grobTree(
     rectGrob(height = 0.5, width = 0.75),
@@ -69,6 +95,8 @@ legend_crossbar <- function(self, data, params) {
   )
 }
 
+#' @export
+#' @rdname draw_key
 legend_path <- function(self, data, params) {
   segmentsGrob(0.1, 0.5, 0.9, 0.5,
     gp = gpar(
@@ -81,6 +109,8 @@ legend_path <- function(self, data, params) {
   )
 }
 
+#' @export
+#' @rdname draw_key
 legend_dotplot <- function(self, data, params) {
   pointsGrob(0.5, 0.5, size = unit(.5, "npc"),
     pch = 21,
@@ -91,6 +121,8 @@ legend_dotplot <- function(self, data, params) {
   )
 }
 
+#' @export
+#' @rdname draw_key
 legend_pointrange <- function(self, data, params) {
   grobTree(
     legend_path(self, data, params),
@@ -98,6 +130,8 @@ legend_pointrange <- function(self, data, params) {
   )
 }
 
+#' @export
+#' @rdname draw_key
 legend_smooth <- function(self, data, params) {
   data$fill <- alpha(data$fill, data$alpha)
   data$alpha <- 1
@@ -108,6 +142,8 @@ legend_smooth <- function(self, data, params) {
   )
 }
 
+#' @export
+#' @rdname draw_key
 legend_text <- function(self, data, params) {
   textGrob("a", 0.5, 0.5,
     rot = data$angle,
@@ -120,6 +156,8 @@ legend_text <- function(self, data, params) {
   )
 }
 
+#' @export
+#' @rdname draw_key
 legend_vline <- function(self, data, params) {
   segmentsGrob(0.5, 0, 0.5, 1,
     gp = gpar(
