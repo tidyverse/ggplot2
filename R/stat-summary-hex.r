827a8a7924dccaf03434e88f5ea15767907eab0d
hadley <h.wickham@gmail.com>
2015-07-23 14:22:35 -0500

    Fix stat summary hex arg inconsistency

--- R/stat-summary-hex.r
+++ R/stat-summary-hex.r
@@ -56,7 +56,7 @@ StatSummaryHex <- ggproto("StatSummaryHex", Stat,
   required_aes = c("x", "y", "z"),
 
   calculate = function(data, scales, binwidth = NULL, bins = 30, drop = TRUE,
-                       fun = mean, fun.args = list(), ...) {
+                       fun = "mean", fun.args = list(), ...) {
     data <- remove_missing(data, FALSE, c("x", "y", "z"),
       name = "stat_summary_hex")
 
