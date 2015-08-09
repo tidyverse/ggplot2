47bd835064a42ae4b997d80f34283c607bdf08a8
hadley <h.wickham@gmail.com>
2015-08-09 05:43:46 -0700

    Improve position_dodge docs

--- R/position-dodge.r
+++ R/position-dodge.r
@@ -7,25 +7,31 @@
 #' @family position adjustments
 #' @export
 #' @examples
+#' ggplot(mtcars, aes(factor(cyl), fill = factor(vs))) +
+#'   geom_bar(position = "dodge")
 #' \donttest{
-#' ggplot(mtcars, aes(x=factor(cyl), fill=factor(vs))) +
-#'   geom_bar(position="dodge")
-#' ggplot(diamonds, aes(x=price, fill=cut)) + geom_bar(position="dodge")
+#' ggplot(diamonds, aes(price, fill = cut)) +
+#'   geom_histogram(position="dodge")
 #' # see ?geom_boxplot and ?geom_bar for more examples
 #'
-#' # Dodging things with different widths is tricky
-#' df <- data.frame(x=c("a","a","b","b"), y=1:4, g = rep(1:2, 2))
-#' (p <- ggplot(df, aes(x, y)) +
-#'       geom_bar(stat = "identity", position = "dodge", aes(group = g)))
+#' # To dodge items with different widths, you need to be explicit
+#' df <- data.frame(x=c("a","a","b","b"), y=2:5, g = rep(1:2, 2))
+#' p <- ggplot(df, aes(x, y, group = g)) +
+#'   geom_bar(
+#'     stat = "identity", position = "dodge",
+#'     fill = "grey50", colour = "black"
+#'   )
+#' p
 #'
-#' p + geom_linerange(aes(ymin = y-1, ymax = y+1), position="dodge")
+#' # A line range has no width:
+#' p + geom_linerange(aes(ymin = y-1, ymax = y+1), position = "dodge")
 #' # You need to explicitly specify the width for dodging
 #' p + geom_linerange(aes(ymin = y-1, ymax = y+1),
 #'   position = position_dodge(width = 0.9))
 #'
 #' # Similarly with error bars:
 #' p + geom_errorbar(aes(ymin = y-1, ymax = y+1), width = 0.2,
-#'   position="dodge")
+#'   position = "dodge")
 #' p + geom_errorbar(aes(ymin = y-1, ymax = y+1, width = 0.2),
 #'   position = position_dodge(width = 0.90))
 #' }
