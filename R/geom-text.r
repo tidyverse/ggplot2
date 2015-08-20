da4efa4bd61ee36470a0e9995bada59771db319f
hadley <h.wickham@gmail.com>
2015-08-20 07:24:59 -0500

    Tweak geom_text size

--- R/geom-text.r
+++ R/geom-text.r
@@ -184,7 +184,7 @@ GeomText <- ggproto("GeomText", Geom,
 
   required_aes = c("x", "y", "label"),
 
-  default_aes = aes(colour = "black", size = 5, angle = 0, hjust = 0.5,
+  default_aes = aes(colour = "black", size = 3.88, angle = 0, hjust = 0.5,
     vjust = 0.5, alpha = NA, family = "", fontface = 1, lineheight = 1.2),
 
   draw_key = draw_key_text
