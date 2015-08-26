96c08c29f807fae4c32a4141a25c498d7b058ece
hadley <h.wickham@gmail.com>
2015-08-26 08:50:16 -0500

    Special-case the span argument to loess.
    Fixes #1292

--- R/stat-smooth.r
+++ R/stat-smooth.r
@@ -9,6 +9,9 @@
 #' @param fullrange should the fit span the full range of the plot, or just
 #'   the data
 #' @param level level of confidence interval to use (0.95 by default)
+#' @param span Controls the amount of smoothing for the default loess smoother.
+#'   Smaller numbers produce wigglier lines, larger numbers produce smoother
+#'   lines.
 #' @param n number of points to evaluate smoother at
 #' @param method.args List of additional arguments passed on to the modelling
 #'   function defined by \code{method}.
@@ -25,7 +28,7 @@
 #' @rdname geom_smooth
 stat_smooth <- function(mapping = NULL, data = NULL, geom = "smooth",
   position = "identity", method = "auto", formula = y ~ x, se = TRUE, n = 80,
-  fullrange = FALSE, level = 0.95, method.args = list(),
+  span = 0.75, fullrange = FALSE, level = 0.95, method.args = list(),
   na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, ...)
 {
   layer(
@@ -44,7 +47,8 @@ stat_smooth <- function(mapping = NULL, data = NULL, geom = "smooth",
       fullrange = fullrange,
       level = level,
       na.rm = na.rm,
-      method.args = method.args
+      method.args = method.args,
+      span = span
     ),
     params = list(...)
   )
@@ -78,9 +82,9 @@ StatSmooth <- ggproto("StatSmooth", Stat,
   },
 
   compute_group = function(data, scales, method = "auto", formula = y~x,
-                           se = TRUE, n = 80, fullrange = FALSE, xseq = NULL,
-                           level = 0.95, method.args = list(), na.rm = FALSE,
-                           ...) {
+                           se = TRUE, n = 80, span = 0.75, fullrange = FALSE,
+                           xseq = NULL, level = 0.95, method.args = list(),
+                           na.rm = FALSE, ...) {
     if (length(unique(data$x)) < 2) {
       # Not enough data to perform fit
       return(data.frame())
@@ -104,6 +108,11 @@ StatSmooth <- ggproto("StatSmooth", Stat,
         xseq <- seq(range[1], range[2], length.out = n)
       }
     }
+    # Special case span because it's the most commonly used model argument
+    if (identical(method, "loess")) {
+      method.args$span <- span
+    }
+
     if (is.character(method)) method <- match.fun(method)
 
     base.args <- list(quote(formula), data = quote(data), weights = quote(weight))
