710991b90a3a2ed7966f8f7732a4679982b711c8
Winston Chang <winston@stdout.org>
2015-09-24 12:33:47 -0500

    Disable broken geom_raster visual test

--- visual_test/geom-raster.r
+++ visual_test/geom-raster.r
@@ -39,9 +39,9 @@ ggplot(df, aes(x, y, fill = z)) + geom_raster() + geom_point(colour = "red") +
   xlim(-2, 2) + ylim(-2, 2)
 save_vtest("3 x 1, set limits")
 
-ggplot(df, aes(x, y, fill = z)) + geom_raster(hpad = 0.25, vpad = 0) +
-  geom_point(colour = "red")
-save_vtest("3 x 1, just = (0, 0)")
+# ggplot(df, aes(x, y, fill = z)) + geom_raster(hpad = 0.25, vpad = 0) +
+#   geom_point(colour = "red")
+# save_vtest("3 x 1, just = (0, 0)")
 
 # Categorical fill, irregular swatches ---------------------------------------
 
