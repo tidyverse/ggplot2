859400030cb3f7d6ce12ad65555764b052c4c0dc
Winston Chang <winston@stdout.org>
2015-09-25 15:29:46 -0500

    Add tests for stat_bar order

--- tests/testthat/test-stat-bin.R
+++ tests/testthat/test-stat-bin.R
@@ -19,3 +19,23 @@ test_that("stat_bar throws error when y aesthetic present", {
   expect_error(p <- ggplot_build(ggplot(dat, aes(x)) + stat_bar(y = 5)),
     "Unknown parameters: y")
 })
+
+test_that("stat_bar preserves x order for continuous and discrete", {
+  # x is numeric
+  b <- ggplot_build(ggplot(mtcars, aes(carb)) + geom_bar())
+  expect_identical(b$data[[1]]$x, c(1,2,3,4,6,8))
+  expect_identical(b$data[[1]]$y, c(7,10,3,10,1,1))
+
+  # x is factor where levels match numeric order
+  mtcars$carb2 <- factor(mtcars$carb)
+  b <- ggplot_build(ggplot(mtcars, aes(carb2)) + geom_bar())
+  expect_identical(b$data[[1]]$x, 1:6)
+  expect_identical(b$data[[1]]$y, c(7,10,3,10,1,1))
+
+  # x is factor levels differ from numeric order
+  mtcars$carb3 <- factor(mtcars$carb, levels = c(4,1,2,3,6,8))
+  b <- ggplot_build(ggplot(mtcars, aes(carb3)) + geom_bar())
+  expect_identical(b$data[[1]]$x, 1:6)
+  expect_identical(b$panel$ranges[[1]]$x.labels, c("4","1","2","3","6","8"))
+  expect_identical(b$data[[1]]$y, c(10,7,10,3,1,1))
+})
