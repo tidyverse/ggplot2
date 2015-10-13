ceffa5d4d8984b65699bd21f05fc3a4c0e88f2b1
hadley <h.wickham@gmail.com>
2015-10-13 07:55:02 -0500

    Alias colors to colours in scale_*_gradientn
    Fixes #1290

--- R/scale-gradientn.r
+++ R/scale-gradientn.r
@@ -1,14 +1,18 @@
 #' @inheritParams scales::gradient_n_pal
-#' @param colours Vector of colours to use for n-colour gradient.
+#' @param colours,colors Vector of colours to use for n-colour gradient.
 #' @rdname scale_gradient
 #' @export
-scale_colour_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar") {
+scale_colour_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar", colors) {
+  colours <- if (missing(colours)) colors else colours
+
   continuous_scale("colour", "gradientn",
     gradient_n_pal(colours, values, space), na.value = na.value, guide = guide, ...)
 }
 #' @rdname scale_gradient
 #' @export
-scale_fill_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar") {
+scale_fill_gradientn <- function(..., colours, values = NULL, space = "Lab", na.value = "grey50", guide = "colourbar", colors) {
+  colours <- if (missing(colours)) colors else colours
+
   continuous_scale("fill", "gradientn",
     gradient_n_pal(colours, values, space), na.value = na.value, guide = guide, ...)
 }
