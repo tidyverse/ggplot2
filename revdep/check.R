025d1987f289b19b699d7bb687372e85f18d6c25
hadley <h.wickham@gmail.com>
2016-02-11 07:03:50 -0600

    Use github remotes for gtable and scales

--- revdep/check.R
+++ revdep/check.R
@@ -1,10 +1,7 @@
 library("devtools")
 
-withr::with_libpaths(getOption("devtools.revdep.libpath"), {
-  install_github("hadley/gtable")
-  install_github("hadley/scales")
-})
-
 res <- revdep_check(threads = 6)
 revdep_check_save_summary()
 revdep_check_print_problems()
+
+# revdep_email(date = "Feb 26", only_problems = TRUE, draft = FALSE)
