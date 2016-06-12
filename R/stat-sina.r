ce8af07c32fc8d0485d1f2eb6436d3606b9a0944
Nikos Sidiropoulos <nikos.sidiro@gmail.com>
2016-06-13 00:44:18 +0200

    Using namespace plyr.

--- R/stat-sina.r
+++ R/stat-sina.r
@@ -110,7 +110,7 @@ StatSina <- ggproto("StatSina", Stat,
     #scale all bins based on their density relative to the densiest bin
     if (scale) {
       group_scaling_factor <-
-        ddply(data, .(group), mutate,
+        plyr::ddply(data, "group", plyr::mutate,
               group_max = max(bin_counts))$group_max / max(data$bin_counts)
     } else {
       group_scaling_factor <- 1
