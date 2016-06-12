c8c742b4356232aac4da11899a310b3d8beed452
Nikos Sidiropoulos <nikos.sidiro@gmail.com>
2016-06-12 18:18:33 +0200

    layer returns ..scaled.. and ..bin_counts.. . Group-wise scaling is with plyr. Neighbourhood renamed to counts.

--- R/stat-sina.r
+++ R/stat-sina.r
@@ -3,19 +3,25 @@
 #' @inheritParams stat_bin
 #' @param bins	Number of bins. Overridden by binwidth. Defaults to 50.
 #' @param scale Logical. When set to \code{TRUE} x-coordinate widths across all
-#' groups are scaled based on the densiest are in the plot. Default: \code{TRUE}
-#' between 0 and 1.
+#' groups are scaled based on the densiest area in the plot.
+#' Default: \code{TRUE}
 #' @param method Choose the method to spread the samples within the same
-#' neighbourhood along the x-axis. Available methods: "density",
-#' "neighbourhood" (can be abbreciated, e.g. "d"). See \code{Details}.
+#' bin along the x-axis. Available methods: "density", "counts" (can be
+#' abbreviated, e.g. "d"). See \code{Details}.
 #' @param maxwidth Control the maximum width the points can spread into. Values
+#' between 0 and 1.
 #' @param adjust Adjusts the bandwidth of the density kernel when
 #' \code{method == "density"} (see \code{\link[stats]{density}}).
-#' @param neighbour_limit If the samples within the same y-axis bin are more
-#' than neighbour_limit, the samples's X coordinates will be adjusted.
+#' @param bin_limit If the samples within the same y-axis bin are more
+#' than \code{bin_limit}, the samples's X coordinates will be adjusted.
+#' @section Computed variables:
+#' \describe{
+#'   \item{bin_counts}{sample counts per bin per group}
+#'   \item{scaled}{adjusted x-coordinates}
+#' }
 #' @export
 stat_sina <-function(mapping = NULL, data = NULL,
-                     geom = "point", position = "identity",
+                     geom = "sina", position = "identity",
                      ...,
                      binwidth = NULL,
                      bins = NULL,
@@ -23,11 +29,11 @@ stat_sina <-function(mapping = NULL, data = NULL,
                      method = "density",
                      maxwidth = NULL,
                      adjust = 1,
-                     neighbour_limit = 1,
+                     bin_limit = 1,
                      na.rm = FALSE,
                      show.legend = NA,
                      inherit.aes = TRUE) {
-  method <- match.arg(method, c("density", "neighbourhood"))
+  method <- match.arg(method, c("density", "counts"))
 
   layer(
     data = data,
@@ -44,7 +50,7 @@ stat_sina <-function(mapping = NULL, data = NULL,
       method = method,
       maxwidth = maxwidth,
       adjust = adjust,
-      neighbour_limit = neighbour_limit,
+      bin_limit = bin_limit,
       na.rm = na.rm,
       ...
       )
@@ -60,6 +66,8 @@ StatSina <- ggproto("StatSina", Stat,
 
   required_aes = c("x", "y"),
 
+  default_aes = aes(xend = ..scaled..),
+
   setup_data = function(data, params) {
 
     if (is.double(data$x) && !has_groups(data) && any(data$x != data$x[1L])) {
@@ -73,7 +81,7 @@ StatSina <- ggproto("StatSina", Stat,
 
   setup_params = function(data, params) {
 
-    #Limit maxwidth to 0.96 to leave some space between classes
+    #Limit maxwidth to 0.96 to leave some space between groups
     if (!is.null(params$maxwidth))
       params$maxwidth <- (min(abs(params$maxwidth), .96))
     else
@@ -86,10 +94,9 @@ StatSina <- ggproto("StatSina", Stat,
     params
   },
 
-
   compute_panel = function(self, data, scales, binwidth = NULL, bins = NULL,
                            scale = TRUE, method = "density", maxwidth = NULL,
-                           adjust = 1, neighbour_limit = 1, na.rm = FALSE) {
+                           adjust = 1, bin_limit = 1, na.rm = FALSE) {
 
     if (!is.null(binwidth))
       bins <- bin_breaks_width(scales$y$dimension(), binwidth)
@@ -98,59 +105,61 @@ StatSina <- ggproto("StatSina", Stat,
 
     data <- ggproto_parent(Stat, self)$compute_panel(data, scales,
       scale = scale, method = method, maxwidth = maxwidth, adjust = adjust,
-      neighbour_limit = neighbour_limit, bins = bins$breaks, na.rm = na.rm)
+      bin_limit = bin_limit, bins = bins$breaks, na.rm = na.rm)
 
-    #scale all neighbourhoods based on their density relative to the
-    #densiest neighbourhood
-    if (scale)
-      group_scaling_factor <- data$max_neighbours / max(data$max_neighbours)
-    else
+    #scale all bins based on their density relative to the densiest bin
+    if (scale) {
+      group_scaling_factor <-
+        ddply(data, .(group), mutate,
+              group_max = max(bin_counts))$group_max / max(data$bin_counts)
+    } else {
       group_scaling_factor <- 1
+    }
 
-    #translate x coordinates
-    data$x <- data$x + data$x_translation * group_scaling_factor
+    data$scaled <- data$x + data$x_translation * group_scaling_factor
+    data$x_translation <- NULL
     data
   },
 
   compute_group = function(data, scales, scale = TRUE, method = "density",
-                           maxwidth = NULL, adjust = 1, neighbour_limit = 1,
+                           maxwidth = NULL, adjust = 1, bin_limit = 1,
                            bins = NULL, na.rm = FALSE) {
 
-    #initialize x_translation to 0
-    data$x_translation <- rep(0, nrow(data))
+    #initialize x_translation and bin_counts to 0
+    data$x_translation <- data$bin_counts <- rep(0, nrow(data))
 
     #if group has less than 2 points return as is
     if (nrow(data) < 2) {
-      data$max_neighbours <- 1
+      data$max_bin_counts <- 1
       return(data)
     }
 
     #per bin sample count
-    neighbours <- table(findInterval(data$y, bins))
+    bin_counts <- table(findInterval(data$y, bins))
 
     #per bin sample density
     if (method == "density") {
       densities <- stats::density(data$y, adjust = adjust)
 
-      #confine the samples in a (-maxwidth/2, -maxwidth/2) area around the class
-      #center
+      #confine the samples in a (-maxwidth/2, -maxwidth/2) area around the
+      #group's center
       if (max(densities$y) > 0.5 * maxwidth)
         intra_scaling_factor <- 0.5 * maxwidth / max(densities$y)
       else
         intra_scaling_factor <- 1
 
     } else {
-      #allow up to 50 samples in a neighbourhood without scaling
-      if (max(neighbours) > 50 * maxwidth) {
-        intra_scaling_factor <- 50 * maxwidth / max(neighbours)
+      #allow up to 50 samples in a bin without scaling
+      if (max(bin_counts) > 50 * maxwidth) {
+        intra_scaling_factor <- 50 * maxwidth / max(bin_counts)
       } else
         intra_scaling_factor <- 1
     }
 
-    for (i in names(neighbours)) {
+    for (i in names(bin_counts)) {
 
-      #examine neighbourhoods with more than 'neighbour_limit' samples
-      if (neighbours[i] > neighbour_limit){
+      #examine bins with more than 'bin_limit' samples
+      if (bin_counts[i] > bin_limit){
         cur_bin <- bins[ as.integer(i) : (as.integer(i) + 1)]
 
         #find samples in the current bin and translate their X coord.
@@ -160,18 +169,18 @@ StatSina <- ggproto("StatSina", Stat,
         if (method == "density")
           xmax <- mean(densities$y[findInterval(densities$x, cur_bin) == 1])
         else
-          xmax <- neighbours[i] / 100
+          xmax <- bin_counts[i] / 100
 
         #assign the samples uniformely within the specified range
-        x_translation <- stats::runif(neighbours[i], - xmax, xmax )
+        x_translation <- stats::runif(bin_counts[i], - xmax, xmax)
 
         #scale and store new x coordinates
         data$x_translation[points] <- x_translation * intra_scaling_factor
+        #store bin counts. Used for group-wise scaling.
+        data$bin_counts[points] <- bin_counts[i]
       }
     }
 
-    #return the max neighbour count per group. Used for group-wise scaling.
-    data$max_neighbours <- max(neighbours)
     data
   }
 )
