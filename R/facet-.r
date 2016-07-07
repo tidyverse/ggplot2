021aa0be09445f297cf43e779f2a54b8eeba0e8f
thomasp85 <thomasp85@gmail.com>
2016-07-07 15:16:18 +0200

    Documenting main methods of Facet class

--- R/facet-.r
+++ R/facet-.r
@@ -11,6 +11,13 @@ is.facet <- function(x) inherits(x, "Facet")
 Facet <- ggproto("Facet", NULL,
   shrink = FALSE,
 
+  # Take input data and define a mapping between facetting variables and ROW,
+  # COL and PANEL keys
+  #
+  # @param data A list of data.frames, the first being the plot data and the
+  # subsequent individual layer data
+  #
+  # @return A data.frame with columns for PANEL, ROW, COL, and facetting vars
   layout = function(data, vars = NULL, drop = TRUE) {
     if (length(vars) == 0) return(data.frame())
 
@@ -47,27 +54,99 @@ Facet <- ggproto("Facet", NULL,
 
     base
   },
+  # Calls layout and decorate the output with addition information such as
+  # and axis information
+  #
+  # @param data A list of data.frames, the first being the plot data and the
+  # subsequent individual layer data
+  #
+  # @return As $layout but potentially with additional columns depending on
+  # subclass implementation
   train = function(self, data) {
     stop("Not implemented", call. = FALSE)
   },
+  # Decorate data with PANEL keys based on a layout
+  #
+  # @param data A list of data.frames, the first being the plot data and the
+  # subsequent individual layer data
+  #
+  # @param layout A layout as returned by $train
+  #
+  # @return A list of data.frames based on input with a PANEL column mapping
+  # data to specific panels. Data can be changed, e.g. if margin=TRUE it will
+  # be duplicated
   map = function(self, data, layout) {
     stop("Not implemented", call. = FALSE)
   },
-  render = function(self, panels_grob, coord, theme, geom_grobs) {
+  # Renders the plot area into a gtable
+  #
+  # @param panel A panel object containing layout, scales and ranges
+  #
+  # @param coord A Coord object specifying the coordinate system
+  #
+  # @param theme A theme object
+  #
+  # @param geom_grobs The generated grobs for each layer as a list, each
+  # element split into panels
+  #
+  # @return A gtable with the central plot area
+  render = function(self, panel, coord, theme, geom_grobs) {
     stop("Not implemented", call. = FALSE)
   },
+  # Renders the facet strips. This is not a required method as such but used
+  # by convention by FacetGrid and FacetWrap. Extensions want necessarly need to
+  # implement it
+  #
+  # @param panel A panel object containing layout, scales and ranges
+  #
+  # @param theme A theme object
+  #
+  # @return A list of grobs named after their position. t=top, b=bottom,
+  # l=left, r=right
   strips = function(self, panel, theme) {
     stop("Not implemented", call. = FALSE)
   },
+  # Renders the plot area(s). This is not a required method as such but used
+  # by convention by FacetGrid and FacetWrap. Extensions want necessarly need to
+  # implement it
+  #
+  # @param panel A panel object containing layout, scales and ranges
+  #
+  # @param coord A Coord object specifying the coordinate system
+  #
+  # @param theme A theme object
+  #
+  # @param geom_grobs The generated grobs for each layer as a list, each
+  # element split into panels
+  #
+  # @return Depends on subclass. FacetGrid returns a TableGrob, while FacetWrap
+  # returns a lis of gTree
   panels = function(self, panel, coord, theme, geom_grobs) {
     stop("Not implemented", call. = FALSE)
   },
+  # Renders the axes for the plot. This is not a required method as such but used
+  # by convention by FacetGrid and FacetWrap. Extensions want necessarly need to
+  # implement it
+  #
+  # @param panel A panel object containing layout, scales and ranges
+  #
+  # @param coord A Coord object specifying the coordinate system
+  #
+  # @param theme A theme object
+  #
+  # @param geom_grobs The generated grobs for each layer as a list, each
+  # element split into panels
+  #
+  # @return A list of grobs named after their position. t=top, b=bottom,
+  # l=left, r=right
   axes = function(self, panel, coord, theme) {
     stop("Not implemented", call. = FALSE)
   },
+  # Create a string representation of the facetting variables
   vars = function(self) {
     stop("Not implemented", call. = FALSE)
   },
+  # Prints information of the object, using the $vars method
   print = function(self) {
     cat("<", class(self)[[1]], ">\n", sep = "")
     cat("Variables: ", self$vars(), '\n', sep = "")
@@ -85,7 +164,7 @@ unique_combs <- function(df) {
 
   unique_values <- plyr::llply(df, ulevels)
   rev(expand.grid(rev(unique_values), stringsAsFactors = FALSE,
-                  KEEP.OUT.ATTRS = TRUE))
+    KEEP.OUT.ATTRS = TRUE))
 }
 
 df.grid <- function(a, b) {
