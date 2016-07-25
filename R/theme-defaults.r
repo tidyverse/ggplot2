f33b83504d676f4a9fafe552296d15c3e3c9349a
Jean-Olivier Irisson <irisson@normalesup.org>
2016-07-25 16:31:31 +0200

    Match theme_bw with theme_grey better
    Keep contrast and size of gridlines (in particular, previously the minor
    grid lines were thicker than the major ones which looked weird)
    
    Remove the border around the legend key which complicates things visually
    for little gain
    
    Outline panel and facet strips with a dark-ish grey

--- R/theme-defaults.r
+++ R/theme-defaults.r
@@ -154,14 +154,16 @@ theme_bw <- function(base_size = 11, base_family = "") {
   # Starts with theme_grey and then modify some parts
   theme_grey(base_size = base_size, base_family = base_family) %+replace%
     theme(
-      axis.text         = element_text(size = rel(0.8)),
-      axis.ticks        = element_line(colour = "black"),
-      legend.key        = element_rect(colour = "grey80"),
+      # white background and dark border
       panel.background  = element_rect(fill = "white", colour = NA),
-      panel.border      = element_rect(fill = NA, colour = "grey50"),
-      panel.grid.major  = element_line(colour = "grey90", size = 0.2),
-      panel.grid.minor  = element_line(colour = "grey98", size = 0.5),
-      strip.background  = element_rect(fill = "grey80", colour = "grey50", size = 0.2)
+      panel.border      = element_rect(fill = NA, colour = "grey20"),
+      # make gridlines dark, same contrast with white as in theme_grey
+      panel.grid.major  = element_line(colour = "grey92"),
+      panel.grid.minor  = element_line(colour = "grey92", size = 0.25),
+      # contour strips to match panel contour
+      strip.background  = element_rect(fill = "grey85", colour = "grey20"),
+      # match legend key to background
+      legend.key        = element_rect(fill = "white", colour=NA)
     )
 }
 
