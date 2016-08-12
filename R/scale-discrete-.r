2ebbf895609ed88087a347b562523961ebd5209a
thomasp85 <thomasp85@gmail.com>
2016-08-12 14:56:37 +0200

    Check null directly on the range object

--- R/scale-discrete-.r
+++ R/scale-discrete-.r
@@ -122,8 +122,8 @@ ScaleDiscretePosition <- ggproto("ScaleDiscretePosition", ScaleDiscrete,
 
     if (self$is_empty()) {
       c(0, 1)
-    } else if (is.null(d_range)) { # only continuous
       expand_range(c_range, expand[1], 0 , 1)
+    } else if (is.null(self$range$range)) { # only continuous
     } else if (is.null(c_range)) { # only discrete
       expand_range(c(1, length(d_range)), 0, expand[2], 1)
     } else { # both
