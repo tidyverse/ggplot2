0ef095a0e5b970045fcdaa939ff107ca81a84f7f
thomasp85 <thomasp85@gmail.com>
2016-09-20 13:34:43 +0200

    Try out with scale_apha

--- R/scale-alpha.r
+++ R/scale-alpha.r
@@ -3,9 +3,7 @@
 #' \code{scale_alpha} is an alias for \code{scale_alpha_continuous} since
 #' that is the most common use of alpha, and it saves a bit of typing.
 #'
-#' @param ... Other arguments passed on to \code{\link{continuous_scale}}
-#'   or \code{\link{discrete_scale}} as appropriate, to control name, limits,
-#'   breaks, labels and so forth.
+#' @inheritParams continuous_scale
 #' @param range range of output alpha values.  Should lie between 0 and 1.
 #' @export
 #' @examples
@@ -19,8 +17,17 @@
 #' (p <- ggplot(mtcars, aes(mpg, cyl)) +
 #'   geom_point(aes(alpha = factor(cyl))))
 #' p + scale_alpha_discrete(range = c(0.4, 0.8))
-scale_alpha <- function(..., range = c(0.1, 1)) {
-  continuous_scale("alpha", "alpha_c", rescale_pal(range), ...)
+scale_alpha <- function(name = waiver(), range = c(0.1, 1), breaks = waiver(),
+                        minor_breaks = waiver(), labels = waiver(),
+                        limits = NULL, oob = censor, expand = waiver(),
+                        na.value = NA_real_, trans = "identity",
+                        guide = "legend") {
+  args <- as.list(environment)
+  args$aesthetics <- "alpha"
+  args$name <- "alpha_c"
+  args$palette <- rescale_pal(range)
+  args$range <- NULL
+  do.call(continuous_scale, args)
 }
 
 #' @rdname scale_alpha
@@ -28,8 +35,16 @@ scale_alpha <- function(..., range = c(0.1, 1)) {
 scale_alpha_continuous <- scale_alpha
 
 #' @rdname scale_alpha
+#' @inheritParams discrete_scale
 #' @export
-scale_alpha_discrete <- function(..., range = c(0.1, 1)) {
-  discrete_scale("alpha", "alpha_d",
-    function(n) seq(range[1], range[2], length.out = n), ...)
+scale_alpha_discrete <- function(name = waiver(), range = c(0.1, 1),
+                                 breaks = waiver(), labels = waiver(),
+                                 limits = NULL, expand = waiver(),
+                                 na.value = NA, drop = TRUE, guide = "legend") {
+  args <- as.list(environment)
+  args$aesthetics <- "alpha"
+  args$name <- "alpha_d"
+  args$palette <- function(n) seq(range[1], range[2], length.out = n)
+  args$range <- NULL
+  do.call(discrete_scale, args)
 }
