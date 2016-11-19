27adc70cd6a639ae0f7fd49c834013a48ebe22b6
drsimonj <drsimonjackson@gmail.com>
2016-11-20 08:02:43 +1100

    Add order_by

--- R/order-.r
+++ R/order-.r
@@ -9,18 +9,72 @@ NULL
 #' @export
 Order <- ggproto("Order", NULL,
 
-  order_map = NULL,
+  order_map = list(),
+  params = list(),
 
-  train_order = function(self, order_aes, data) {
+  train_order = function(self, data) {
+    to_order <- self$params$to_order
+    order_by <- self$params$order_by
 
+    # Checks on ordering variables -----
+
+    if (!all(c(to_order, order_by) %in% .all_aesthetics)) {
+      stop("Valid aesthetics are required for ordering such as x or y.")
+    }
+
+    convert_to <- to_order %in% names(.base_to_ggplot)
+    to_order[convert_to] <- unname(.base_to_ggplot[to_order[convert_to]])
+
+    convert_by <- order_by %in% names(.base_to_ggplot)
+    order_by[convert_by] <- unname(.base_to_ggplot[order_by[convert_by]])
+
+    if (!all(c(to_order, order_by) %in% names(data)))
+      stop("One or more of the ordering variables cannot be found.
+           Please specify aesthetics used by the geom layers such as x or y.")
+
+    # Ordering -----
+
+    # This will only work for ONE order_by
+    # Need to pass ... to tapply
+    # Will also need something to handle different facet options (via reference to PANEL)
+    order_map <- lapply(data[to_order], function(o) {
+      order_as <- order(tapply(data[[order_by]], o, self$params$order_f, self$params$na.rm))
+
+      new <- as.factor(o)
+      new <- as.numeric(factor(new, levels = levels(new)[order_as]))
+
+      data.frame(old = o, new = new)
+    })
+
+    self$order_map <- order_map
+    order_map
   },
 
   order_data = function(self, data) {
+    order_map <- self$order_map
+    if (length(order_map) == 0) order_map <- self$train_order(data)
 
+    # 1-to-1 reordering
+    data[names(order_map)] <- lapply(order_map, function(i) i$new)
+
+    return(data)
   },
 
   order_scales = function(self, scales) {
+    if (length(self$order_map) == 0) stop("order_scales cannot be executed before train_data or order_data")
+    order_map <- lapply(self$order_map, unique_order)
 
+
+    ## NOT READY
+    return(order_map)
   }
 
 )
+
+
+
+# Helpers -----------------------------------------------------------------
+
+unique_order <- function(order_map) {
+  order_map[!duplicated.data.frame(order_map), ]
+}
---
+++ R/order-by.r
@@ -0,0 +1,53 @@
+#' @include order-.r
+NULL
+
+#' Order by
+#'
+#' @param ordering a formula with the axes (of the tabular display) to order on
+#'   the LHS and the aesthetics (of the tabular display) to use for determining
+#'   the order on the RHS. Example x ~ y to order the x axis by values on y
+#' @param facets How to handle ordering across facets. "within" will produce
+#'   different axis ordering within each facet panel. "betwee" will order the
+#'   panels themselves. "all" will do both.
+#' @param order_f Function used to create ordering.
+#' @param na.rm Should missing values be removed? Passed to order_f
+#' @param ... Additional arguments to pass to order_f
+#' @param na.rm
+#' @export
+order_by <- function(ordering, facets = "all", order_f = max, na.rm = TRUE, ...) {
+
+  # Ordering can either be a formula, a string, or a list of things to be
+  # convert to quoted
+  if (is.character(ordering)) {
+    ordering <- stats::as.formula(ordering)
+  }
+  if (is.formula(ordering)) {
+    lhs <- function(x) if (length(x) == 2) NULL else x[-3]
+    rhs <- function(x) if (length(x) == 2) x else x[-2]
+
+    to_order <- as.quoted(lhs(ordering))
+    #to_order <- to_order[!sapply(to_order, identical, as.name("."))]
+    order_by <- as.quoted(rhs(ordering))
+    #order_by <- order_by[!sapply(order_by, identical, as.name("."))]
+  }
+  if (is.list(ordering)) {
+    to_order <- as.quoted(ordering[[1]])
+    order_by <- as.quoted(ordering[[2]])
+  }
+  if (length(to_order) == 0) {
+    stop("Must specify at least one axis to order", call. = FALSE)
+  }
+  if (length(order_by) == 0) {
+    stop("Must specify at least one aesthetic to order by", call. = FALSE)
+  }
+  to_order <- as.character(to_order)
+  order_by <- as.character(order_by)
+
+
+  facets <- match.arg(facets, c("all", "within", "between"))
+
+  ggproto(NULL, Order,
+          params = list(to_order = to_order, order_by = order_by,
+                        facets = facets, order_f = order_f, na.rm = na.rm, ...)
+  )
+}
