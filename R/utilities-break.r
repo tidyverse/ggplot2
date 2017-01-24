f561c3448eb7b7addff0a9251e01cab08d3bb688
hadley <h.wickham@gmail.com>
2017-01-24 17:53:42 -0600

    Remove redundant link
    Fixes #1993

--- R/utilities-break.r
+++ R/utilities-break.r
@@ -10,7 +10,6 @@
 #' @param n number of intervals to create, OR
 #' @param length length of each interval
 #' @param ... other arguments passed on to \code{\link{cut}}
-#' @seealso \code{\link{cut_number}}
 #' @export
 #' @examples
 #' table(cut_interval(1:100, 10))
