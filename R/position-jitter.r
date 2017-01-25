9c9951e8ad0185f1de61583866803bf755861b6d
Kirill Müller <krlmlr+r@mailbox.org>
2017-01-25 09:50:40 +0100

    use only one with_seed() call

--- R/position-jitter.r
+++ R/position-jitter.r
@@ -58,9 +58,9 @@ PositionJitter <- ggproto("PositionJitter", Position,
   },
 
   compute_layer = function(data, params, panel) {
-    trans_x <- if (params$width > 0) function(x) with_seed(params$seed, jitter(x, amount = params$width))
-    trans_y <- if (params$height > 0) function(x) with_seed(params$seed, jitter(x, amount = params$height))
+    trans_x <- if (params$width > 0) function(x) jitter(x, amount = params$width)
+    trans_y <- if (params$height > 0) function(x) jitter(x, amount = params$height)
 
-    transform_position(data, trans_x, trans_y)
+    with_seed(params$seed, transform_position(data, trans_x, trans_y))
   }
 )
