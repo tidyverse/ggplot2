d32ddf6156a3564818d79921e49e6e49f982533a
hadley <h.wickham@gmail.com>
2017-02-07 17:25:06 -0600

    Add world map example

--- R/sf.R
+++ R/sf.R
@@ -47,6 +47,17 @@
 #'   geom_sf() +
 #'   annotate("point", x = -80, y = 35, colour = "red", size = 4)
 #'
+#' # Thanks to the power of sf, ageom_sf nicely handles varying projections
+#' # setting the aspect ratio correctly.
+#' library(maps)
+#' world1 <- sf::st_as_sf(map('world', plot = FALSE, fill = TRUE))
+#' ggplot() + geom_sf(data = world1)
+#'
+#' world2 <- sf::st_transform(
+#'   world1,
+#'   "+proj=laea +y_0=0 +lon_0=155 +lat_0=-90 +ellps=WGS84 +no_defs"
+#' )
+#' ggplot() + geom_sf(data = world2)
 #' @name ggsf
 NULL
 
