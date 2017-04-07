2e5d867b9bb31cb52db907828c5f113a9b1c8d95
Stefan <stefan.hoj-edwards@roslin.ed.ac.uk>
2017-04-07 14:13:20 +0100

    Added flexible cartesian coordinate objects with capped lines.

---
+++ R/coord_capped.r
@@ -0,0 +1,152 @@
+#' Cartesian coordinates with capped axis lines.
+#'
+#' Caps the axis lines to the outer ticks to e.g. indicate range of values.
+#' Methods correspond to \code{\link{coord_cart}} and \code{\link{coord_flip}}
+#'
+#' This function is a simple override of \code{\link{coord_flex_cart}}
+#' and \code{\link{coord_flex_flip}},
+#' which allows shorthand specification of what to cap.
+#'
+#' NB! A panel-border is typically drawn on top such that it covers tick marks,
+#' grid lines, and axis lines.
+#' Many themes also do not draw axis lines.
+#' To ensure the modified axis lines are visible, use
+#' \code{theme(panel.border=element_blank(), axis.lines=element_line())}.
+#'
+#' @param top,left,bottom,right Either a function returned from
+#'   \code{\link{capped_horisontal}} or \code{\link{brackets_horisontal}}.
+#'   If string, it is assumed to be shorthand for
+#'   \code{capped_horisontal(capped)} or similar for vertical.
+#' @param gap Both ends are \emph{always} capped by this proportion.
+#'   Usually a value between 0 and 1.
+#' @rdname coord_capped
+#' @export
+#' @inheritParams coord_flex_cart
+#' @examples
+#' # Notice how the axis lines of the following plot meet in the lower-left corner.
+#' p <- ggplot(mtcars, aes(x = mpg)) + geom_dotplot() +
+#'   theme_bw() +
+#'   theme(panel.border=element_blank(), axis.line=element_line())
+#' p
+#'
+#' # We can introduce a gap by capping the ends:
+#' p + coord_capped_cart(bottom='none', left='none')
+#'
+#' # The lower limit on the y-axis is 0. We can cap the line to this value.
+#' # Notice how the x-axis line extends through the plot when we no long
+#' # define its capping.
+#' p + coord_capped_cart(left='both')
+#'
+#' # It it also works on the flipped.
+#' p + coord_capped_flip(bottom='both')
+coord_capped_cart <- function(xlim = NULL,
+                              ylim = NULL,
+                              expand = TRUE,
+                              top = waiver(),
+                              left = waiver(),
+                              bottom = waiver(),
+                              right = waiver(),
+                              gap = 0.01) {
+  if (is.character(top)) top <- capped_horisontal(top, gap=gap)
+  if (is.character(bottom)) bottom <- capped_horisontal(bottom, gap=gap)
+  if (is.character(left)) left <- capped_vertical(left, gap=gap)
+  if (is.character(right)) right <- capped_vertical(right, gap=gap)
+  ggproto(NULL, CoordFlexCartesian,
+          limits = list(x = xlim, y = ylim),
+          expand = expand,
+          top = top,
+          left = left,
+          bottom = bottom,
+          right = right
+  )
+}
+
+
+#' @rdname coord_capped
+#' @export
+#' @inheritParams coord_flex_cart
+coord_capped_flip <- function(xlim = NULL,
+                              ylim = NULL,
+                              expand = TRUE,
+                              top = waiver(),
+                              left = waiver(),
+                              bottom = waiver(),
+                              right = waiver(),
+                              gap = 0.01) {
+  if (is.character(top)) top <- capped_horisontal(top, gap=gap)
+  if (is.character(bottom)) bottom <- capped_horisontal(bottom, gap=gap)
+  if (is.character(left)) left <- capped_vertical(left, gap=gap)
+  if (is.character(right)) right <- capped_vertical(right, gap=gap)
+  ggproto(NULL, CoordFlexFlipped,
+          limits = list(x = xlim, y = ylim),
+          expand = expand,
+          top = top,
+          left = left,
+          bottom = bottom,
+          right = right
+  )
+}
+
+# Capped axis lines -----------------------------------------------------------
+
+#' @param capped Which end to cap the line. Can be one of (where relevant):
+#'   \code{both}, \code{none}, \code{left}, \code{right}, \code{top}, \code{bottom}.
+#' @inheritParams coord_capped_cart
+#' @rdname coord_capped
+#' @export
+capped_horisontal <- function(capped = c('both','left','right','none'),
+                              gap = 0.01) {
+  capped <- match.arg(capped)
+  # scale_details: aka. panel_params
+  # axis: primary or secondary
+  # scale: "x" or "y"
+  # position: top or bottom / left or right
+  # theme:
+  function(scale_details, axis, scale, position, theme) {
+    agrob <- render_axis(scale_details, axis, "x", position, theme)
+    if (agrob$name == 'NULL') return(agrob)
+
+    r <- range(as.numeric( switch(axis,
+      primary=scale_details$x.major,
+      secondary=scale_details$x.sec.major, scale_details$x.major
+    )))
+    i <- which(grepl('line', names(agrob$children)))
+    agrob$children[[i]]$x <- switch(capped,
+      none =  unit(c(min(0 + gap, r[1]), max(1 - gap, r[2])), 'native'),
+      left =  unit(c(r[1], max(1 - gap, r[2])), 'native'),
+      right = unit(c(min(0 + gap, r[1]), r[2]), 'native'),
+      both =  unit(r, 'native')
+    )
+    agrob
+  }
+}
+
+#' @inheritParams capped_horisontal
+#' @rdname coord_capped
+#' @export
+capped_vertical <- function(capped = c('top','bottom','both','none'),
+                            gap = 0.01) {
+  capped <- match.arg(capped)
+  # scale_details: aka. panel_params
+  # axis: primary or secondary
+  # scale: "x" or "y"
+  # position: top or bottom / left or right
+  # theme:
+  function(scale_details, axis, scale, position, theme) {
+    agrob <- render_axis(scale_details, axis, "y", position, theme)
+    if (agrob$name == 'NULL') return(agrob)
+
+    r <- range(as.numeric( switch(axis,
+      primary=scale_details$y.major,
+      secondary=scale_details$y.sec.major, scale_details$y.major
+    )))
+    i <- which(grepl('line', names(agrob$children)))
+    agrob$children[[i]]$y <- switch(capped,
+      none =    unit(c(min(0 + gap,r[1]), max(1 - gap, r[2])), 'native'),
+      bottom =  unit(c(r[1], max(1 - gap, r[2])), 'native'),
+      top = unit(c(min(0 + gap, r[1]), r[2]), 'native'),
+      both =  unit(r, 'native')
+    )
+    agrob
+  }
+}
---
+++ R/coord_flex.r
@@ -0,0 +1,157 @@
+# Flexible Cartesian coordinates ----------------------------------------------
+# ggproto objects for these are defined in a later chunk.
+
+#' Cartesian coordinates with flexible options for drawing axes
+#'
+#' Allows user to inject a function for drawing axes, such as
+#' \code{\link{capped_horisontal}} or \code{\link{brackets_horisontal}}.
+#'
+#' NB! A panel-border is typically drawn on top such that it covers tick marks,
+#' grid lines, and axis lines.
+#' Many themes also do not draw axis lines.
+#' To ensure the modified axis lines are visible, use
+#' \code{theme(panel.border=element_blank(), axis.lines=element_line())}.
+#'
+#' @section User defined functions:
+#' The provided function in \code{top}, \code{right}, \code{bottom}, and \code{left}
+#' defaults to \code{render_axis} which is defined in \file{coord-.r}, which in
+#' turns calls \code{guide_axis} (see \file{guides-axis.r}).
+#'
+#' The provided function is with the arguments
+#' \code{scale_details}, \code{axis}, \code{scale}, \code{position}, and \code{theme},
+#' and the function should return an \code{\link{absoluteGrob}} object.
+#'
+#' For examples of modifying the drawn object, see e.g.
+#' \code{\link{capped_horisontal}} or \code{\link{brackets_horisontal}}.
+#'
+#' @rdname coord_flex
+#' @param top,left,bottom,right Function for drawing axis lines, ticks, and labels,
+#'    use e.g. \code{\link{capped_horisontal}} or \code{\link{brackets_horisontal}}.
+#' @export
+#' @inheritParams coord_cartesian
+#' @examples
+#' # ensures that the ranges of axes are equal to the specified ratio by
+#' # adjusting the plot aspect ratio
+#'
+#' p <- ggplot(mtcars, aes(mpg, wt)) + geom_point()
+#' p + coord_fixed(ratio = 1)
+#' p + coord_fixed(ratio = 5)
+#' p + coord_fixed(ratio = 1/5)
+coord_flex_cart <- function(xlim = NULL,
+                            ylim = NULL,
+                            expand = TRUE,
+                            top = waiver(),
+                            left = waiver(),
+                            bottom = waiver(),
+                            right = waiver()) {
+  ggproto(NULL, CoordFlexCartesian,
+          limits = list(x = xlim, y = ylim),
+          expand = expand,
+          top = top,
+          left = left,
+          bottom = bottom,
+          right = right
+  )
+}
+
+#' @rdname coord_flex
+#' @export
+#' @inheritParams  coord_flex_cart
+#'
+coord_flex_flip <- function(xlim = NULL,
+                            ylim = NULL,
+                            expand = TRUE,
+                            top = waiver(),
+                            left = waiver(),
+                            bottom = waiver(),
+                            right = waiver()) {
+  ggproto(NULL, CoordFlexFlipped,
+          limits = list(x = xlim, y = ylim),
+          expand = expand,
+          top = top,
+          left = left,
+          bottom = bottom,
+          right = right
+  )
+}
+
+#' @rdname coord_flex
+#' @inheritParams  coord_flex_cart
+#' @export
+#' @param ratio aspect ratio, expressed as \code{y / x}.
+coord_flex_fixed <- function(ratio = 1,
+                             xlim = NULL,
+                             ylim = NULL,
+                             expand = TRUE,
+                             top = waiver(),
+                             left = waiver(),
+                             bottom = waiver(),
+                             right = waiver()) {
+ggproto(NULL, CoordFixed,
+        limits = list(x = xlim, y = ylim),
+        ratio = ratio,
+        expand = expand,
+        top = top,
+        left = left,
+        bottom = bottom,
+        right = right
+  )
+}
+
+
+
+# Helper functions ------------------------------------------------------------
+
+f_zeroGrob <- function(...) zeroGrob()
+
+# flex_render_axis_h and _v were lifted from the render_axis_h and _v of
+# ancestral class "Coord" in coord-.r
+# For each top/bottom or left/right axis, they basically just call what ever
+# function the coord_flex classes were given.
+flex_render_axis_h <- function(self, scale_details, theme) {
+  arrange <- scale_details$x.arrange %||% c("primary", "secondary")
+  top <- self$top %|W|% render_axis
+  bottom <- self$bottom %|W|% render_axis
+  list(
+    top = top(scale_details, arrange[1], "x", "top", theme),
+    bottom = bottom(scale_details, arrange[2], "x", "bottom", theme)
+  )
+}
+flex_render_axis_v <- function(self, scale_details, theme) {
+  arrange <- scale_details$y.arrange %||% c("primary","secondary")
+  left <- self$left %|W|% render_axis
+  right <- self$right %|W|% render_axis
+  list(
+    left = left(scale_details, arrange[1], 'y', 'left', theme),
+    right = right(scale_details, arrange[2], 'y', 'right', theme)
+  )
+}
+
+# ggproto objects -------------------------------------------------------------
+
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+CoordFlexCartesian <- ggproto('CoordFlexCartesian', `_inherit` = CoordCartesian,
+  render_axis_h = flex_render_axis_h,
+  render_axis_v = flex_render_axis_v
+)
+
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+CoordFlexFlipped <- ggproto('CoordFlexFlipped',  `_inherit` = CoordFlip,
+  render_axis_h = flex_render_axis_h,
+  render_axis_v = flex_render_axis_v
+)
+
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+CoordFlexFixed <- ggproto('CoordFlexFlipped',  `_inherit` = CoordFixed,
+  render_axis_h = flex_render_axis_h,
+  render_axis_v = flex_render_axis_v
+)
