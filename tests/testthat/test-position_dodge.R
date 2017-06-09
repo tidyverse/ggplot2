3ea5881ad3044ee1881785f5ed1b61e4ed815eab
Hassan Kibirige <has2k1@gmail.com>
2017-06-09 12:11:42 -0500

    Fix position_dodge
    When calculating the groups in a bin, do it per panel then get the
    maximum across all panel.
    
    fixes #2153

--- tests/testthat/test-position_dodge.R
+++ tests/testthat/test-position_dodge.R
@@ -1,13 +1,15 @@
 context("position_dodge")
 
 test_that("can control whether to preserve total or individual width", {
-  df <- data.frame(x = c("a", "b", "b"), y = c("a", "a", "b"))
+  df <- data.frame(x = c("a", "b", "b", "b"), y = c("a", "a", "b", "b"), z = c(1, 1, 2, 1))
 
   p_total <- ggplot(df, aes(x, fill = y)) +
     geom_bar(position = position_dodge(preserve = "total"), width = 1)
   p_single <- ggplot(df, aes(x, fill = y)) +
     geom_bar(position = position_dodge(preserve = "single"), width = 1)
+  p_single_facet <- p_single + facet_wrap(~z)
 
   expect_equal(layer_data(p_total)$x, c(1, 2.25, 1.75))
   expect_equal(layer_data(p_single)$x, c(0.75, 2.25, 1.75))
+  expect_equal(layer_data(p_single_facet)$x, c(0.75, 2.25, 1.75, 1.75))
 })
