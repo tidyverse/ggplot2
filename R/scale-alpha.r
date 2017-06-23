e9670b417f9cc28094a518c3458c7e29b2a69ed4
Kara Woo <woo.kara@gmail.com>
2017-06-23 14:46:48 -0700

    Warn when using alpha for factors (but not ordered factors)

--- R/scale-alpha.r
+++ R/scale-alpha.r
@@ -29,6 +29,14 @@ scale_alpha_continuous <- scale_alpha
 #' @rdname scale_alpha
 #' @export
 scale_alpha_discrete <- function(..., range = c(0.1, 1)) {
+  warning("Using alpha for a discrete variable is not advised.", call. = FALSE)
+  discrete_scale("alpha", "alpha_d",
+    function(n) seq(range[1], range[2], length.out = n), ...)
+}
+
+#' @rdname scale_alpha
+#' @export
+scale_alpha_ordinal <- function(..., range = c(0.1, 1)) {
   discrete_scale("alpha", "alpha_d",
     function(n) seq(range[1], range[2], length.out = n), ...)
 }
