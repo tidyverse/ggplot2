10942d118eda2c23ae27692cf0296c1ad6735b10
Kara Woo <woo.kara@gmail.com>
2017-07-09 16:23:50 -0700

    Scale boxes across all the data based on the max number that need to be dodged from one another
    i.e. if there are 2 boxes per group all widths get divided by 2

--- R/position-boxdodge.r
+++ R/position-boxdodge.r
@@ -52,35 +52,27 @@ pos_boxdodge <- function(df, width, n = NULL) {
     df$xmax <- df$x
   }
   
-  df$indiv_width <- df$xmax - df$xmin
+  # Maximum number of boxes that need to be dodged from one another
+  n <- max(table(df$x))
+  
+  df$new_width <- (df$xmax - df$xmin) / n
+  df$xmin <- df$x - (df$new_width / 2)
+  df$xmax <- df$x + (df$new_width / 2)
 
-  ## How wide is each group?
-  group_sizes <- plyr::ddply(df, "x", plyr::summarize, size = sum(indiv_width))
+  # Find the total width of each group of boxes
+  group_sizes <- plyr::ddply(df, "x", plyr::summarize, size = sum(new_width))
+  
+  # Starting xmin for each group of boxes
+  starts <- group_sizes$x - (group_sizes$size / 2)
 
-  ## What do we need to multiply each group width by to ensure that the largest is
-  ## the target width?
-  rescale_factor <- width / max(group_sizes$size)
-
-  ## Multiply by rescale factor
-  df$rescaled_width <-  df$indiv_width * rescale_factor
-
-  ## Now what are the total group sizes?
-  rescaled_group_sizes <- plyr::ddply(
-                                  df,
-                                  "x",
-                                  plyr::summarize,
-                                  size = sum(rescaled_width)
-                                )
-
-  ## starting xmin for each group
-  starts <- rescaled_group_sizes$x - rescaled_group_sizes$size/2
-
+  # Set the boxes in place
   for (i in seq_along(starts)) {
-    divisions <- cumsum(c(starts[i], df[df$x == i, "rescaled_width"]))
+    divisions <- cumsum(c(starts[i], df[df$x == i, "new_width"]))
     df[df$x == i, "xmin"] <- divisions[-length(divisions)]
     df[df$x == i, "xmax"] <- divisions[-1]
   }
 
+  # x values get moved to between xmin and xmax
   df$x <- rowMeans(df[, c("xmin", "xmax")])
   
   df
