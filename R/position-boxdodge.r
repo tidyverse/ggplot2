b689a4cbec703325477e69342894a96bcfe2e2c0
Kara Woo <woo.kara@gmail.com>
2017-07-17 12:32:22 -0700

    "boxes" -> "elements" since this is no longer only used for boxes

--- R/position-boxdodge.r
+++ R/position-boxdodge.r
@@ -87,8 +87,8 @@ pos_boxdodge <- function(df, width, n = NULL, padding = 0.05) {
   df$newx <- newx[df$xid]
   
   if (is.null(n)) {
-    # If n is null, preserve total widths of boxes at each position by dividing
-    # widths by the number of elements at that position
+    # If n is null, preserve total widths of elements at each position by
+    # dividing widths by the number of elements at that position
     n <- table(df$xid)
     df$new_width <- (df$xmax - df$xmin) / as.numeric(n[df$xid])
   } else {
@@ -98,7 +98,7 @@ pos_boxdodge <- function(df, width, n = NULL, padding = 0.05) {
   df$xmin <- df$x - (df$new_width / 2)
   df$xmax <- df$x + (df$new_width / 2)
 
-  # Find the total width of each group of boxes
+  # Find the total width of each group of elements
   group_sizes <- aggregate(
     list(size = df$new_width),
     list(newx = df$newx),
@@ -108,7 +108,7 @@ pos_boxdodge <- function(df, width, n = NULL, padding = 0.05) {
   # Starting xmin for each group of elements
   starts <- group_sizes$newx - (group_sizes$size / 2)
 
-  # Set the boxes in place
+  # Set the elements in place
   for (i in seq_along(starts)) {
     divisions <- cumsum(c(starts[i], df[df$xid == i, "new_width"]))
     df[df$xid == i, "xmin"] <- divisions[-length(divisions)]
@@ -118,12 +118,12 @@ pos_boxdodge <- function(df, width, n = NULL, padding = 0.05) {
   # x values get moved to between xmin and xmax
   df$x <- (df$xmin + df$xmax) / 2
 
-  # If no boxes occupy the same position, there is no need to add padding
+  # If no elements occupy the same position, there is no need to add padding
   if (!any(duplicated(df$xid))) {
     return(df)
   }
   
-  # Shrink boxes to add space between them
+  # Shrink elements to add space between them
   df$pad_width <- df$new_width * (1 - padding)
   df$xmin <- df$x + (df$pad_width / 2)
   df$xmax <- df$x - (df$pad_width / 2)
