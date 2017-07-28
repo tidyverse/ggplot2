91d8397b77ddd3a18a9b00dc633a4d85d218c08f
Kara Woo <woo.kara@gmail.com>
2017-07-28 10:13:34 -0700

    Write visual tests for geom_sf with vdiffr

--- tests/testthat/test-geom-sf.R
+++ tests/testthat/test-geom-sf.R
@@ -7,9 +7,13 @@ test_that("geom_sf draws correctly", {
 
   f <- system.file("gpkg/nc.gpkg", package="sf")
   nc <- sf::read_sf(f)
-  ggplot() + geom_sf(data = nc)
-  ggplot() + geom_sf(data = nc) + coord_sf(datum = 4326)
+
+  vdiffr::expect_doppelganger("North Carolina county boundaries",
+    ggplot() + geom_sf(data = nc) + coord_sf(datum = 4326)
+  )
 
   pts <- sf::st_sf(a = 1:2, geometry = sf::st_sfc(sf::st_point(0:1), sf::st_point(1:2)))
-  ggplot() + geom_sf(data = pts)
+  vdiffr::expect_doppelganger("spatial points",
+    ggplot() + geom_sf(data = pts)
+  )
 })
