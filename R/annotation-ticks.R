d4a945c41a48ef80c2e6277e42b8e9274fffc28a
Jonathan Sidi <yonicd@gmail.com>
2017-08-06 23:18:43 -0400

    correct script syntax and spacing

--- R/annotation-ticks.R
+++ R/annotation-ticks.R
@@ -16,51 +16,51 @@
 #'
 #' p <- ggplot(msleep, aes(bodywt, brainwt)) + geom_point()
 #'
-#' #Default behavior
+#' # Default behavior
 #'
-#' #add identity scale minor ticks on y axis
-#' p + annotation_ticks(sides='l')
+#' # add identity scale minor ticks on y axis
+#' p + annotation_ticks(sides = 'l')
 #'
-#' #add identity scale minor ticks on x,y axis
-#' p + annotation_ticks(sides='lb')
+#' # add identity scale minor ticks on x,y axis
+#' p + annotation_ticks(sides = 'lb')
 #'
-#' #Control number of minor ticks of each side independently
+#' # Control number of minor ticks of each side independently
 #'
-#' #add identity scale minor ticks on x,y axis
-#' p + annotation_ticks(sides='lb',ticks_per_base=c(10,5))
+#' # add identity scale minor ticks on x,y axis
+#' p + annotation_ticks(sides = 'lb', ticks_per_base = c(10,5))
 #'
-#' #log10 scale
+#' # log10 scale
 #' p1 <- p + scale_x_log10()
 #'
-#' #add minor ticks on log10 scale
-#' p1 + annotation_ticks(sides='b',scale='log10')
+#' # add minor ticks on log10 scale
+#' p1 + annotation_ticks(sides = 'b', scale = 'log10')
 #'
-#' #add minor ticks on both scales
-#' p1 + annotation_ticks(sides='lb',scale=c('identity','log10'))
+#' # add minor ticks on both scales
+#' p1 + annotation_ticks(sides = 'lb', scale = c('identity','log10'))
 #'
-#' #add minor ticks on both scales, but force x axis to be identity
-#' p1+annotation_ticks(sides='lb',scale='identity')
+#' # add minor ticks on both scales, but force x axis to be identity
+#' p1 + annotation_ticks(sides = 'lb', scale = 'identity')
 #'
 #'
-#' #log scale
-#' p2 <- p + scale_x_continuous(trans='log')
+#' # log scale
+#' p2 <- p + scale_x_continuous(trans = 'log')
 #'
-#' #add minor ticks on log scale
-#' p2 + annotation_ticks(sides='b',scale='log')
+#' # add minor ticks on log scale
+#' p2 + annotation_ticks(sides = 'b', scale = 'log')
 #'
-#' #add minor ticks on both scales
-#' p2 + annotation_ticks(sides='lb',scale=c('identity','log'))
+#' # add minor ticks on both scales
+#' p2 + annotation_ticks(sides = 'lb', scale = c('identity','log'))
 #'
-#' #add minor ticks on both scales, but force x axis to be identity
-#' p2 + annotation_ticks(sides='lb',scale='identity')
+#' # add minor ticks on both scales, but force x axis to be identity
+#' p2 + annotation_ticks(sides = 'lb', scale = 'identity')
 #'
 #' @import grid
 annotation_ticks <- function(sides = "b",
                              scale = 'identity',
                              scaled = TRUE,
-                             short = grid::unit(0.1,"cm"),
-                             mid = grid::unit(0.2, "cm"),
-                             long = grid::unit(0.3, "cm"),
+                             short = unit(0.1,"cm"),
+                             mid = unit(0.2, "cm"),
+                             long = unit(0.3, "cm"),
                              colour = "black",
                              size = 0.5,
                              linetype = 1,
@@ -86,13 +86,13 @@ annotation_ticks <- function(sides = "b",
     # }
 
     #check for invalid side
-    if(nchar(gsub('[b|t|l|r]','',sides))>0)
-      stop(gsub('[b|t|l|r]','',sides),' is not a valid side: b,t,l,r are valid')
+    if(grepl("[^btlr]", sides))
+      stop(gsub('[btlr]','',sides),' is not a valid side: b,t,l,r are valid')
 
     #split sides to character vector
     sides <- strsplit(sides,'')[[1]]
 
-    if((length(sides)!=length(scale))){
+    if( length(sides) != length(scale) ){
 
       if(length(scale)==1){
 
@@ -108,15 +108,15 @@ annotation_ticks <- function(sides = "b",
 
     base <- sapply(scale,function(x) switch(x,'identity'=10,'log10'=10,'log'=exp(1)),USE.NAMES = FALSE)
 
-    if(!'ticks_per_base'%in%names(match.call())){
+    if( missing(ticks_per_base) ){
 
       ticks_per_base <- base - 1
 
     }else{
 
-      if((length(sides)!=length(ticks_per_base))){
+      if( (length(sides) != length(ticks_per_base)) ){
 
-        if(length(ticks_per_base)==1){
+        if( length(ticks_per_base)==1 ){
 
           ticks_per_base <- rep(ticks_per_base,length(sides))
 
@@ -129,7 +129,7 @@ annotation_ticks <- function(sides = "b",
       }
     }
 
-    delog<-scale%in%'identity'
+    delog <- scale %in% 'identity'
 
 
     layer(
@@ -140,19 +140,20 @@ annotation_ticks <- function(sides = "b",
       position = PositionIdentity,
       show.legend = FALSE,
       inherit.aes = FALSE,
-      params = list(base = base,
-                    sides = sides,
-                    scaled = scaled,
-                    short = short,
-                    mid = mid,
-                    long = long,
-                    colour = colour,
-                    size = size,
-                    linetype = linetype,
-                    alpha = alpha,
-                    ticks_per_base = ticks_per_base,
-                    delog = delog,
-                    ...)
+      params = list(
+        base = base,
+        sides = sides,
+        scaled = scaled,
+        short = short,
+        mid = mid,
+        long = long,
+        colour = colour,
+        size = size,
+        linetype = linetype,
+        alpha = alpha,
+        ticks_per_base = ticks_per_base,
+        delog = delog,
+        ...)
       )
 }
 
@@ -166,41 +167,39 @@ GeomTicks <- ggproto("GeomTicks", Geom,
     data
 },
 
-draw_panel = function(
-  data,
-  panel_scales,
-  coord,
-  base = c(10, 10),
-  sides = c("b","l"),
-  scaled = TRUE,
-  short = grid::unit(0.1, "cm"),
-  mid = grid::unit(0.2, "cm"),
-  long = grid::unit(0.3, "cm"),
-  ticks_per_base = base - 1,
-  delog = c(x = TRUE, y = TRUE)){
+  draw_panel = function(data,
+                        panel_scales,
+                        coord,
+                        base = c(10, 10),
+                        sides = c("b","l"),
+                        scaled = TRUE,
+                        short = unit(0.1, "cm"),
+                        mid = unit(0.2, "cm"),
+                        long = unit(0.3, "cm"),
+                        ticks_per_base = base - 1,
+                        delog = c(x = TRUE, y = TRUE)){
 
     ticks <- list()
 
     # Convert these units to numbers so that they can be put in data frames
-    short <- grid::convertUnit(short, "cm", valueOnly = TRUE)
-    mid <- grid::convertUnit(mid, "cm", valueOnly = TRUE)
-    long <- grid::convertUnit(long, "cm", valueOnly = TRUE)
+    short <- convertUnit(short, "cm", valueOnly = TRUE)
+    mid <- convertUnit(mid, "cm", valueOnly = TRUE)
+    long <- convertUnit(long, "cm", valueOnly = TRUE)
 
     for (s in 1:length(sides)) {
         if (grepl("[b|t]", sides[s])) {
 
             # Get positions of x tick marks
-            xticks <- calc_ticks(
-              base = base[s],
-              minpow = floor(panel_scales$x.range[1]),
-              maxpow = ceiling(panel_scales$x.range[2]),
-              majorTicks = panel_scales$x.major_source,
-              start = 0,
-              shortend = short,
-              midend = mid,
-              longend = long,
-              ticks_per_base = ticks_per_base[s],
-              delog = delog[s])
+            xticks <- calc_ticks(base = base[s],
+                                 minpow = floor(panel_scales$x.range[1]),
+                                 maxpow = ceiling(panel_scales$x.range[2]),
+                                 majorTicks = panel_scales$x.major_source,
+                                 start = 0,
+                                 shortend = short,
+                                 midend = mid,
+                                 longend = long,
+                                 ticks_per_base = ticks_per_base[s],
+                                 delog = delog[s])
 
             if (scaled) {
                 if (!delog[s])
@@ -213,37 +212,48 @@ draw_panel = function(
 
             # Make the grobs
             if (grepl("b", sides[s])) {
-                ticks$x_b <- with(data, segmentsGrob(x0 = grid::unit(xticks$x, "native"),
-                  x1 = grid::unit(xticks$x, "native"), y0 = grid::unit(xticks$start,
-                    "cm"), y1 = grid::unit(xticks$end, "cm"), gp = grid::gpar(col = alpha(colour,
-                    alpha), lty = linetype, lwd = size * .pt)))
+                                       ticks$x_b <-  with(data,
+                                                          segmentsGrob(x0 = unit(xticks$x, "native"),
+                                                                       x1 = unit(xticks$x, "native"),
+                                                                       y0 = unit(xticks$start, "cm"),
+                                                                       y1 = unit(xticks$end, "cm"),
+                                                                       gp = gpar(col = alpha(colour,alpha),
+                                                                                 lty = linetype,
+                                                                                 lwd = size * .pt)
+                                                                       )
+                                                          )
             }
             if (grepl("t", sides[s])) {
-                ticks$x_t <- with(data, grid::segmentsGrob(x0 = grid::unit(xticks$x,
-                  "native"), x1 = grid::unit(xticks$x, "native"), y0 = grid::unit(1,
-                  "npc") - grid::unit(xticks$start, "cm"), y1 = unit(1, "npc") - grid::unit(xticks$end,
-                  "cm"), gp = grid::gpar(col = alpha(colour, alpha), lty = linetype,
-                  lwd = size * .pt)))
+                                        ticks$x_t <- with(data,
+                                                          segmentsGrob(x0 = unit(xticks$x,"native"),
+                                                                       x1 = unit(xticks$x, "native"),
+                                                                       y0 = unit(1, "npc") - unit(xticks$start, "cm"),
+                                                                       y1 = unit(1, "npc") - unit(xticks$end, "cm"),
+                                                                       gp = gpar(col = alpha(colour, alpha),
+                                                                                 lty = linetype,
+                                                                                 lwd = size * .pt)
+                                                                       )
+                                                          )
             }
         }
 
 
         if (grepl("[l|r]", sides[s])) {
-            yticks <- calc_ticks(
-              base = base[s],
-              minpow = floor(panel_scales$y.range[1]),
-              maxpow = ceiling(panel_scales$y.range[2]),
-              majorTicks = panel_scales$y.major_source,
-              start = 0,
-              shortend = short,
-              midend = mid,
-              longend = long,
-              ticks_per_base = ticks_per_base[s],
-              delog = delog[s])
+            yticks <- calc_ticks(base = base[s],
+                                 minpow = floor(panel_scales$y.range[1]),
+                                 maxpow = ceiling(panel_scales$y.range[2]),
+                                 majorTicks = panel_scales$y.major_source,
+                                 start = 0,
+                                 shortend = short,
+                                 midend = mid,
+                                 longend = long,
+                                 ticks_per_base = ticks_per_base[s],
+                                 delog = delog[s])
 
             if (scaled) {
-                if (!delog[s])
-                  yticks$value <- log(yticks$value, base[s])
+                if (!delog[s]){
+                    yticks$value <- log(yticks$value, base[s])
+                  }
             }
 
             names(yticks)[names(yticks) == "value"] <- "y"  # Rename to 'y' for coordinates$transform
@@ -251,39 +261,49 @@ draw_panel = function(
 
             # Make the grobs
             if (grepl("l", sides[s])) {
-                ticks$y_l <- with(data, grid::segmentsGrob(y0 = grid::unit(yticks$y,
-                  "native"), y1 = grid::unit(yticks$y, "native"), x0 = grid::unit(yticks$start,
-                  "cm"), x1 = grid::unit(yticks$end, "cm"), gp = grid::gpar(col = alpha(colour,
-                  alpha), lty = linetype, lwd = size * .pt)))
+                ticks$y_l <- with(data,
+                                  segmentsGrob(y0 = unit(yticks$y, "native"),
+                                               y1 = unit(yticks$y, "native"),
+                                               x0 = unit(yticks$start, "cm"),
+                                               x1 = unit(yticks$end, "cm"),
+                                               gp = gpar(col = alpha(colour, alpha),
+                                                         lty = linetype, lwd = size * .pt)
+                                               )
+                                  )
             }
             if (grepl("r", sides[s])) {
-                ticks$y_r <- with(data, grid::segmentsGrob(y0 = grid::unit(yticks$y,
-                  "native"), y1 = grid::unit(yticks$y, "native"), x0 = grid::unit(1,
-                  "npc") - grid::unit(yticks$start, "cm"), x1 = grid::unit(1, "npc") -
-                  grid::unit(yticks$end, "cm"), gp = grid::gpar(col = alpha(colour,
-                  alpha), lty = linetype, lwd = size * .pt)))
+                ticks$y_r <- with(data,
+                                  segmentsGrob(y0 = unit(yticks$y, "native"),
+                                               y1 = unit(yticks$y, "native"),
+                                               x0 = unit(1, "npc") - unit(yticks$start, "cm"),
+                                               x1 = unit(1, "npc") - unit(yticks$end, "cm"),
+                                               gp = gpar(col = alpha(colour, alpha),
+                                                         lty = linetype,
+                                                         lwd = size * .pt)
+                                               )
+                                  )
             }
         }
     }
-    grid::gTree(children = do.call("gList", ticks))
-}, default_aes = aes(colour = "black", size = 0.5, linetype = 1, alpha = 1))
+    gTree(children = do.call("gList", ticks))
+},
+  default_aes = aes(colour = "black", size = 0.5, linetype = 1, alpha = 1))
 
 
 # Calculate the position of log tick marks Returns data frame with: - value: the
 # position of the log tick on the data axis, for example 1, 2, ..., 9, 10, 20, ...
 # - start: on the other axis, start position of the line (usually 0) - end: on the
 # other axis, end position of the line (for example, .1, .2, or .3)
-calc_ticks <- function(
-  base = 10,
-  ticks_per_base = base - 1,
-  minpow = 0,
-  maxpow = minpow + 1,
-  majorTicks = 0,
-  start = 0,
-  shortend = 0.1,
-  midend = 0.2,
-  longend = 0.3,
-  delog = FALSE){
+calc_ticks <- function(base = 10,
+                       ticks_per_base = base - 1,
+                       minpow = 0,
+                       maxpow = minpow + 1,
+                       majorTicks = 0,
+                       start = 0,
+                       shortend = 0.1,
+                       midend = 0.2,
+                       longend = 0.3,
+                       delog = FALSE){
 
     # Number of blocks of tick marks
     reps <- maxpow - minpow
@@ -314,14 +334,16 @@ calc_ticks <- function(
 
     if (delog) {
 
-        ticksCopy = ticks
+        ticksCopy <- ticks
 
-        regScale = log(ticks, base)
+        regScale <- log(ticks, base)
 
-        majorTicks = sort(unique(c(minpow,
-                                   regScale[which(regScale %in% majorTicks)],
-                                   maxpow,
-                                   majorTicks)
+        majorTicks <- sort(
+                            unique(
+                                    c(minpow,
+                                      regScale[which(regScale %in% majorTicks)],
+                                      maxpow,
+                                      majorTicks)
                                  )
                           )
 
@@ -330,12 +352,12 @@ calc_ticks <- function(
         if (length(majorTicks) > 1) {
 
             for (i in 1:(length(majorTicks) - 1)) {
-                expandScale = c(expandScale,
+                expandScale <- c(expandScale,
                                 seq(majorTicks[i], majorTicks[i + 1], length.out = (ticks_per_base + 1))
                                 )
             }
 
-            ticks = unique(expandScale)
+            ticks <- unique(expandScale)
 
             # Set all of the ticks short
             tickend <- rep(shortend, length(ticks))
