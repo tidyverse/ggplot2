66d7f2f6518456d7db382baf5319062b62d7692a
Kara Woo <woo.kara@gmail.com>
2017-08-11 12:00:02 -0700

    Strip x and y positions should be NULL in call to title_spec
    This will allow them to be modified based on justification AND angle

--- R/margins.R
+++ R/margins.R
@@ -27,42 +27,34 @@ title_spec <- function(label, x, y, hjust, vjust, angle, gp = gpar(),
 
   if (is.null(label)) return(zeroGrob())
 
-  if (missing(x) & missing(y)) {
-    text_grob <- textGrob(label, rot = angle, gp = gp)
-
-    # Used to place debugging point
-    x <- unit(hjust, "npc")
-    y <- unit(vjust, "npc")
+  angle <- angle %% 360
+  if (angle == 90) {
+    xp <- 1 - vjust
+    yp <- hjust
+  } else if (angle == 180) {
+    xp <- 1 - hjust
+    yp <- 1 - vjust
+  } else if (angle == 270) {
+    xp <- vjust
+    yp <- 1 - hjust
   } else {
-    angle <- angle %% 360
-    if (angle == 90) {
-      xp <- 1 - vjust
-      yp <- hjust
-    } else if (angle == 180) {
-      xp <- 1 - hjust
-      yp <- 1 - vjust
-    } else if (angle == 270) {
-      xp <- vjust
-      yp <- 1 - hjust
-    } else {
-      xp <- hjust
-      yp <- vjust
-    }
-
-    n <- max(length(x), length(y), 1)
-    x <- x %||% unit(rep(xp, n), "npc")
-    y <- y %||% unit(rep(yp, n), "npc")
-
-    text_grob <- textGrob(
-      label,
-      x,
-      y,
-      hjust = hjust,
-      vjust = vjust,
-      rot = angle,
-      gp = gp
-    )
+    xp <- hjust
+    yp <- vjust
   }
+
+  n <- max(length(x), length(y), 1)
+  x <- x %||% unit(rep(xp, n), "npc")
+  y <- y %||% unit(rep(yp, n), "npc")
+
+  text_grob <- textGrob(
+    label,
+    x,
+    y,
+    hjust = hjust,
+    vjust = vjust,
+    rot = angle,
+    gp = gp
+  )
 
   # The grob dimensions don't include the text descenders, so add on using
   # a little trigonometry. This is only exactly correct when vjust = 1.
