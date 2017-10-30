810bd28619a00d6fd1913239b3dcb94388276076
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-10-30 11:29:28 +0100

    move method def out of test_that block to make sure dispatch works

--- tests/testthat/test-plot-construction.R
+++ tests/testthat/test-plot-construction.R
@@ -6,11 +6,11 @@ test_that("Custom object cannot be added without generic", {
   expect_error(p + custom_object, "Don't know how to add custom_object to a plot")
 })
 
+ggplot_add.test_object <- function(object, p, objectname) {
+  10
+}
 test_that("Methods can be defined for adding custom objects", {
   p <- ggplot()
   custom_object <- structure(list(), class = 'test_object')
-  ggplot_add.test_object <- function(object, p, objectname) {
-    10
-  }
   expect_equal(p + custom_object, 10)
 })
