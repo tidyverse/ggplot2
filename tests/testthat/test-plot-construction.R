94093c04a8d5bba42dca73dfe51acc8e3797e82d
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-10-30 15:48:13 +0100

    Move test method into testblock again

--- tests/testthat/test-plot-construction.R
+++ tests/testthat/test-plot-construction.R
@@ -6,10 +6,10 @@ test_that("Custom object cannot be added without generic", {
   expect_error(p + custom_object, "Don't know how to add custom_object to a plot")
 })
 
-ggplot_add.test_object <- function(object, p, objectname) {
-  10
-}
 test_that("Methods can be defined for adding custom objects", {
+  ggplot_add.test_object <- function(object, plot, object_name) {
+    10
+  }
   p <- ggplot()
   custom_object <- structure(list(), class = 'test_object')
   expect_equal(p + custom_object, 10)
