2d89fff46be22a40b56631ccc335398b77b2fc45
hadley <h.wickham@gmail.com>
2017-10-31 14:47:04 -0500

    Backport grid fix for R3.1/3.2

---
+++ R/backports.R
@@ -0,0 +1,6 @@
+if (getRversion() <= 3.2) {
+  absolute.units.unit <- getFromNamespace("absolute.units.unit", "grid")
+  absolute.units.unit.list <- getFromNamespace("absolute.units.unit.list", "grid")
+  absolute.units.unit.arithmetic <- getFromNamespace("absolute.units.unit.arithmetic", "grid")
+}
+
