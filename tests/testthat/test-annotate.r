32d0ca8f6a3ea0f2da812a2698f4ac3c97ad33d5
Sergio Oller <sergioller@gmail.com>
2017-11-04 23:41:38 +0100

    Support pipe with ggplot (annotate)

--- tests/testthat/test-annotate.r
+++ tests/testthat/test-annotate.r
@@ -30,6 +30,38 @@ test_that("segment annotations transform with scales", {
   vdiffr::expect_doppelganger("line matches points", plot)
 })
 
+context("annotate (tidy API)")
+
+test_that("dates in segment annotation work", {
+  dt <- structure(list(month = structure(c(1364774400, 1377993600),
+                                         class = c("POSIXct", "POSIXt"), tzone = "UTC"), total = c(-10.3,
+                                                                                                   11.7)), .Names = c("month", "total"), row.names = c(NA, -2L), class =
+                    "data.frame")
+
+  p <- ggplot(dt, aes(month, total)) %>%
+    geom_point() %>%
+    annotate("segment",
+             x = as.POSIXct("2013-04-01"),
+             xend = as.POSIXct("2013-07-01"),
+             y = -10,
+             yend = 10
+    )
+
+  expect_true(all(c("xend", "yend") %in% names(layer_data(p, 2))))
+})
+
+test_that("segment annotations transform with scales", {
+  # Line should match data points
+  df <- data.frame(x = c(1, 10), y = c(10, 1))
+  plot <- ggplot(df, aes(x, y)) %>%
+    geom_point() %>%
+    annotate("segment", x = 1, y = 10, xend = 10, yend = 1, colour = "red") +
+    scale_y_reverse(NULL, breaks = NULL) +
+    scale_x_continuous(NULL, breaks = NULL)
+
+  vdiffr::expect_doppelganger("line matches points", plot)
+})
+
 test_that("annotation_* has dummy data assigned and don't inherit aes", {
   custom <- annotation_custom(zeroGrob())
   logtick <- annotation_logticks()
