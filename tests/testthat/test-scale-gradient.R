b90c62be9fcbeb39bdbdc0758d9c723a0a870358
Victor Lei <viclei@gmail.com>
2017-11-14 19:00:39 -0500

    Add test for limit fix for `scale_*_gradient2()`.

--- tests/testthat/test-scale-gradient.R
+++ tests/testthat/test-scale-gradient.R
@@ -3,16 +3,11 @@ context("scale_gradient")
 # Limits ------------------------------------------------------------------
 
 test_that("points outside the limits are plotted as NA", {
-  p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Petal.Length) ) +
-          geom_point() +
-          scale_color_gradient2(low = muted("green"), high = muted("navy"),
-                                mid = "gray80",
-                                midpoint = 3, limits = c(2, 6),
-                                na.value = "orange")
-  p_data <- ggplot_build(p)$data[[1]]
+  df <- data.frame(x = c(0, 1, 2))
+  p <- ggplot(df, aes(x, 1, fill = x)) +
+    geom_col() +
+    scale_fill_gradient2(limits = c(-1, 1), midpoint = 2, na.value = "orange")
 
-  p_cand <- p_data[p_data$colour == 'orange', c('x', 'y')]  # NA values are orange
-  p_correct <- iris[iris$Petal.Length > 6 | iris$Petal.Length < 2, c('Sepal.Length', 'Sepal.Width')]
-
-  expect_equivalent(p_cand, p_correct)
+  correct_fill <- c("#B26D65", "#DCB4AF", "orange")
+  expect_equivalent(layer_data(p)$fill, correct_fill)
 })
