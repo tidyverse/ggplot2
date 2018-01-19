f1c7d51934b46fc151c05375d976f16686b4d059
Jim Hester <james.f.hester@gmail.com>
2018-01-19 09:00:50 -0500

    Reset the random stream after running .onAttach (#2409)
    This avoids confusion when calling `set.seed()` before attaching
    ggplot2.

--- R/zzz.r
+++ R/zzz.r
@@ -1,17 +1,19 @@
 .onAttach <- function(...) {
-  if (!interactive() || stats::runif(1) > 0.1) return()
+  withr::with_preserve_seed({
+    if (!interactive() || stats::runif(1) > 0.1) return()
 
-  tips <- c(
-    "Need help? Try the ggplot2 mailing list: http://groups.google.com/group/ggplot2.",
-    "Find out what's changed in ggplot2 at http://github.com/tidyverse/ggplot2/releases.",
-    "Use suppressPackageStartupMessages() to eliminate package startup messages.",
-    "Stackoverflow is a great place to get help: http://stackoverflow.com/tags/ggplot2.",
-    "Need help getting started? Try the cookbook for R: http://www.cookbook-r.com/Graphs/",
-    "Want to understand how all the pieces fit together? Buy the ggplot2 book: http://ggplot2.org/book/"
-  )
+    tips <- c(
+      "Need help? Try the ggplot2 mailing list: http://groups.google.com/group/ggplot2.",
+      "Find out what's changed in ggplot2 at http://github.com/tidyverse/ggplot2/releases.",
+      "Use suppressPackageStartupMessages() to eliminate package startup messages.",
+      "Stackoverflow is a great place to get help: http://stackoverflow.com/tags/ggplot2.",
+      "Need help getting started? Try the cookbook for R: http://www.cookbook-r.com/Graphs/",
+      "Want to understand how all the pieces fit together? Buy the ggplot2 book: http://ggplot2.org/book/"
+      )
 
-  tip <- sample(tips, 1)
-  packageStartupMessage(paste(strwrap(tip), collapse = "\n"))
+    tip <- sample(tips, 1)
+    packageStartupMessage(paste(strwrap(tip), collapse = "\n"))
+  })
 }
 
 release_questions <- function() {
