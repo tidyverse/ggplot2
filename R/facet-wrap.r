db49117da50a7abe083d0968d9c1c583127768c0
adeel.hassan <adeel.hassan@afiniti.com>
2018-04-30 02:31:26 -0600

    fix flatten_if bug in facet_wrap

--- R/facet-wrap.r
+++ R/facet-wrap.r
@@ -114,7 +114,7 @@ facet_wrap <- function(facets, nrow = NULL, ncol = NULL, scales = "fixed",
 
   # Flatten all facets dimensions into a single one
   facets_list <- as_facets_list(facets)
-  facets <- rlang::flatten_if(facets_list, is.list)
+  facets <- rlang::flatten_if(facets_list, rlang::is_list)
 
   ggproto(NULL, FacetWrap,
     shrink = shrink,
