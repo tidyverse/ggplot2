ed087cd02a7909c092a8576e7c82130ff6091858
Claus Wilke <wilke@austin.utexas.edu>
2018-05-10 16:43:51 -0500

    disable consistency check for geom_smooth draw arguments, add visual tests

--- tests/testthat/test-geom-smooth.R
+++ tests/testthat/test-geom-smooth.R
@@ -8,3 +8,21 @@ test_that("Data is ordered by x", {
 
   expect_equal(layer_data(ps)[c("x", "y")], df[order(df$x), ])
 })
+
+# Visual tests ------------------------------------------------------------
+
+test_that("geom_smooth() works with alternative stats", {
+  df <- data.frame(x = c(1, 1, 2, 2, 1, 1, 2, 2),
+                   y = c(1, 2, 2, 3, 2, 3, 1, 2),
+                   fill = c(rep("A", 4), rep("B", 4)))
+
+  expect_doppelganger("ribbon turned on in geom_smooth", {
+    ggplot(df, aes(x, y, color = fill, fill = fill)) +
+      geom_smooth(stat = "summary") # ribbon on by default
+  })
+
+  expect_doppelganger("ribbon turned off in geom_smooth", {
+    ggplot(df, aes(x, y, color = fill, fill = fill)) +
+      geom_smooth(stat = "summary", se = FALSE) # ribbon is turned off via `se = FALSE`
+  })
+})
