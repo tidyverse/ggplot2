tidyverse/ggplot2

commit 4c1964bc650028acaa8c606de341dcbd6cfb2e78

Restore notion of plot environment to find default scales

R/quick-plot.r
@@ -123,7 +123,7 @@ qplot <- function(x, y = NULL, ..., data, facets = NULL, margins = FALSE,
     }
   }
 
-  p <- ggplot(data, mapping, environment = NULL)
+  p <- ggplot(data, mapping, environment = caller_env)
 
   if (is.null(facets)) {
     p <- p + facet_null()
@@ -139,7 +139,7 @@ qplot <- function(x, y = NULL, ..., data, facets = NULL, margins = FALSE,
   for (g in geom) {
     # We reevaluate constants once per geom for historical reasons?
     params <- lapply(consts, rlang::eval_tidy)
-    p <- p + do.call(paste0("geom_", g), params)
+    p <- p + do.call(paste0("geom_", g), params, envir = caller_env)
   }
 
   logv <- function(var) var %in% strsplit(log, "")[[1]]

R/layer.r
@@ -227,7 +227,7 @@ Layer <- ggproto("Layer", NULL,
       data <- data[res, , drop = FALSE]
     }
 
-    scales_add_defaults(plot$scales, data, aesthetics)
+    scales_add_defaults(plot$scales, data, aesthetics, plot$plot_env)
 
     # Evaluate and check aesthetics
     aesthetics <- compact(aesthetics)
@@ -287,7 +287,7 @@ Layer <- ggproto("Layer", NULL,
     names(stat_data) <- names(new)
 
     # Add any new scales, if needed
-    scales_add_defaults(plot$scales, data, new)
+    scales_add_defaults(plot$scales, data, new, plot$plot_env)
     # Transform the values, if the scale say it's ok
     # (see stat_spoke for one exception)
     if (self$stat$retransform) {

R/plot.r
@@ -70,28 +70,21 @@
 #'     width = 0.4
 #'   )
 ggplot <- function(data = NULL, mapping = aes(), ...,
-                   environment = NULL) {
+                   environment = parent.frame()) {
   UseMethod("ggplot")
 }
 
 #' @export
 ggplot.default <- function(data = NULL, mapping = aes(), ...,
-                           environment = NULL) {
+                           environment = parent.frame()) {
   ggplot.data.frame(fortify(data, ...), mapping, environment = environment)
 }
 
 #' @export
 ggplot.data.frame <- function(data, mapping = aes(), ...,
-                              environment = NULL) {
+                              environment = parent.frame()) {
   if (!missing(mapping) && !inherits(mapping, "uneval")) {
     stop("Mapping should be created with `aes() or `aes_()`.", call. = FALSE)
-  }
-
-  if (!is.null(environment)) {
-    stop(
-      "`environment` is deprecated: environments are now captured by `aes()`",
-      call. = FALSE
-    )
   }
 
   p <- structure(list(
@@ -102,7 +95,7 @@ ggplot.data.frame <- function(data, mapping = aes(), ...,
     theme = list(),
     coordinates = coord_cartesian(default = TRUE),
     facet = facet_null(),
-    plot_env = parent.frame()
+    plot_env = environment
   ), class = c("gg", "ggplot"))
 
   p$labels <- make_labels(mapping)
@@ -113,7 +106,7 @@ ggplot.data.frame <- function(data, mapping = aes(), ...,
 
 #' @export
 ggplot.grouped_df <- function(data, mapping = aes(), ...,
-                               environment = NULL) {
+                               environment = parent.frame()) {
 
   data$.group <- dplyr::group_indices(data)
   mapping$group <- mapping$group %||% quote(.group)

R/scale-type.R
@@ -1,9 +1,9 @@
-find_scale <- function(aes, x) {
+find_scale <- function(aes, x, env) {
   type <- scale_type(x)
   candidates <- paste("scale", aes, type, sep = "_")
 
   for (scale in candidates) {
-    scale_f <- find_global(scale, globalenv(), mode = "function")
+    scale_f <- find_global(scale, env, mode = "function")
     if (!is.null(scale_f))
       return(scale_f())
   }

R/scales-.r
@@ -88,7 +88,7 @@ scales_transform_df <- function(scales, df) {
 
 # @param aesthetics A list of aesthetic-variable mappings. The name of each
 #   item is the aesthetic, and the value of each item is the variable in data.
-scales_add_defaults <- function(scales, data, aesthetics) {
+scales_add_defaults <- function(scales, data, aesthetics, env) {
   if (is.null(aesthetics)) return()
   names(aesthetics) <- unlist(lapply(names(aesthetics), aes_to_scale))
 
@@ -99,7 +99,7 @@ scales_add_defaults <- function(scales, data, aesthetics) {
   datacols <- lapply(aesthetics[new_aesthetics], rlang::eval_tidy, data = data)
 
   for (aes in names(datacols)) {
-    scales$add(find_scale(aes, datacols[[aes]]))
+    scales$add(find_scale(aes, datacols[[aes]], env))
   }
 
 }

man/ggplot.Rd
@@ -4,7 +4,7 @@
 \alias{ggplot}
 \title{Create a new ggplot}
 \usage{
-ggplot(data = NULL, mapping = aes(), ..., environment = NULL)
+ggplot(data = NULL, mapping = aes(), ..., environment = parent.frame())
 }
 \arguments{
 \item{data}{Default dataset to use for plot. If not already a data.frame,

commit e9346f50f86ac92ffbf2cb591b47749e05865d20

In qplot, look up layer in ggplot2 package env
Fixes #2586. Regression introduced in 4c1964bc650028acaa8c606de341dcbd6cfb2e78

R/quick-plot.r
@@ -144,7 +144,7 @@ qplot <- function(x, y, ..., data, facets = NULL, margins = FALSE,
   for (g in geom) {
     # We reevaluate constants once per geom for historical reasons?
     params <- lapply(consts, rlang::eval_tidy)
-    p <- p + do.call(paste0("geom_", g), params, envir = caller_env)
+    p <- p + do.call(paste0("geom_", g), params)
   }
 
   logv <- function(var) var %in% strsplit(log, "")[[1]]

tests/testthat/test-qplot.r
@@ -29,3 +29,12 @@ test_that("qplot() evaluates constants in the right place", {
   })
   expect_identical(layer_data(p)$colour, I("red"))
 })
+
+test_that("qplot() evaluates layers in package environment", {
+  geom_line <- function(...) {
+    stop("!!!")
+  }
+
+  expect_error(p <- qplot(1, 1, geom = "line"), NA)
+
+})