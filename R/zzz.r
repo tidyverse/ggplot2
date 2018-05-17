b0ec7a326c4470a78f1cb6438e4891e99987b9a8
Hadley Wickham <h.wickham@gmail.com>
2018-05-16 17:37:57 -0700

    Overwrite vars with dplyr::vars, if available (#2593)
    Fixes #2585

--- R/zzz.r
+++ R/zzz.r
@@ -16,6 +16,16 @@
   })
 }
 
+.onLoad <- function(...) {
+  backport_unit_methods()
+
+  # To avoid namespace clash with dplyr.
+  # It seems surprising that this hack works
+  if (requireNamespace("dplyr", quietly = TRUE)) {
+    vars <<- dplyr::vars
+  }
+}
+
 release_questions <- function() {
   c(
     "Have you built the book?"
