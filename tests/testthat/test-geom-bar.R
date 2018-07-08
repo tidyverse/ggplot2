550d66bd5b89d1eda89a231310e853b8bb0bfd87
Claus Wilke <wilke@austin.utexas.edu>
2018-07-08 18:58:06 -0500

    faster unit tests

--- tests/testthat/test-geom-bar.R
+++ tests/testthat/test-geom-bar.R
@@ -3,10 +3,10 @@ context("geom_bar")
 test_that("geom_bar removes bars with parts outside the plot limits", {
   dat <- data.frame(x = c("a", "b", "b", "c", "c", "c"))
 
-  render_plot <- function(extra = list()) {
-    withr::with_pdf(NULL, print(ggplot(dat, aes(x)) + geom_bar() + extra))
-  }
+  p <- ggplot(dat, aes(x)) + geom_bar()
 
-  expect_warning(render_plot(ylim(0.5, 4)), "Removed 3 rows containing missing values")
-  expect_warning(render_plot(ylim(0, 2.5)), "Removed 1 rows containing missing values")
+  expect_warning( # warning created at render stage
+    ggplotGrob(p + ylim(0, 2.5)),
+    "Removed 1 rows containing missing values"
+  )
 })
--- tests/testthat/test-geom-col.R
+++ tests/testthat/test-geom-col.R
@@ -3,10 +3,14 @@ context("geom_col")
 test_that("geom_col removes columns with parts outside the plot limits", {
   dat <- data.frame(x = c(1, 2, 3))
 
-  render_plot <- function(extra = list()) {
-    withr::with_pdf(NULL, print(ggplot(dat, aes(x, x)) + geom_col() + extra))
-  }
+  p <- ggplot(dat, aes(x, x)) + geom_col()
 
-  expect_warning(render_plot(ylim(0.5, 4)), "Removed 3 rows containing missing values")
-  expect_warning(render_plot(ylim(0, 2.5)), "Removed 1 rows containing missing values")
+  expect_warning( # warning created at render stage
+    ggplotGrob(p + ylim(0.5, 4)),
+    "Removed 3 rows containing missing values"
+  )
+  expect_warning( # warning created at build stage
+    ggplot_build(p + ylim(0, 2.5)),
+    "Removed 1 rows containing missing values"
+  )
 })
