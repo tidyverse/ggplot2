bdfb2046a028cd32be2cf11f56841d22af904ae7
Dana Seidel <dpseidel@berkeley.edu>
2018-07-17 10:12:00 -0700

    Add tidyeval to benchplot

--- R/bench.r
+++ R/bench.r
@@ -1,15 +1,19 @@
 #' Benchmark plot creation time.
 #' Broken down into construct, build, render and draw times.
 #'
-#' @param x code to create ggplot2 plot
+#' @param x expression to create ggplot2 plot
 #' @export
 #' @keywords internal
 #' @examples
 #' benchplot(ggplot(mtcars, aes(mpg, wt)) + geom_point())
 #' benchplot(ggplot(mtcars, aes(mpg, wt)) + geom_point() + facet_grid(. ~ cyl))
+#'
+#' # Use tidyeval
+#' x <- expr(ggplot(mtcars, aes(mpg, wt)) + geom_point())
+#' benchplot(x)
 benchplot <- function(x) {
 
-  construct <- system.time(force(x))
+  construct <- system.time(x <- rlang::eval_tidy(x))
   stopifnot(inherits(x, "ggplot"))
 
   build <- system.time(data <- ggplot_build(x))
