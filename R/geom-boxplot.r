f8b81d72a7c8d77facfb536c0eb1412b3ee83c8a
Dana Seidel <dpseidel@berkeley.edu>
2018-08-01 10:38:57 -0700

    Clarify geom_boxplot examples
    Closes #2793

--- R/geom-boxplot.r
+++ R/geom-boxplot.r
@@ -60,7 +60,6 @@
 #' @examples
 #' p <- ggplot(mpg, aes(class, hwy))
 #' p + geom_boxplot()
-#' p + geom_boxplot() + geom_jitter(width = 0.2)
 #' p + geom_boxplot() + coord_flip()
 #'
 #' p + geom_boxplot(notch = TRUE)
@@ -69,6 +68,8 @@
 #' # By default, outlier points match the colour of the box. Use
 #' # outlier.colour to override
 #' p + geom_boxplot(outlier.colour = "red", outlier.shape = 1)
+#' # Remove outliers when overlaying boxplot with original data points
+#' p + geom_boxplot(outlier.shape = NA) + geom_jitter(width = 0.2)
 #'
 #' # Boxplots are automatically dodged when any aesthetic is a factor
 #' p + geom_boxplot(aes(colour = drv))
@@ -79,6 +80,7 @@
 #'   geom_boxplot()
 #' ggplot(diamonds, aes(carat, price)) +
 #'   geom_boxplot(aes(group = cut_width(carat, 0.25)))
+#' # Adjust the transparency of outliers using outlier.alpha
 #' ggplot(diamonds, aes(carat, price)) +
 #'   geom_boxplot(aes(group = cut_width(carat, 0.25)), outlier.alpha = 0.1)
 #'
