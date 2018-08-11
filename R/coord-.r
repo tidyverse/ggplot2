5212b5de11198c97074f4e667ee3f47cc12acc0a
Claus Wilke <wilke@austin.utexas.edu>
2018-08-10 21:31:43 -0400

    Update coord documentation

--- R/coord-.r
+++ R/coord-.r
@@ -13,14 +13,23 @@
 #'   - `render_bg`: Renders background elements.
 #'   - `render_axis_h`: Renders the horizontal axes.
 #'   - `render_axis_v`: Renders the vertical axes.
-#'   - `range`: Returns the x and y ranges
+#'   - `backtransform_range(panel_params)`: Extracts the panel range provided
+#'     in `panel_params` (created by `setup_panel_params()`, see below) and
+#'     backtransforms to data coordinates. This back-transformation is needed
+#'     for coords such as `coord_flip()`, `coord_polar()`, `coord_trans()` where
+#'     the range in the transformed coordinates differs from the range in the
+#'     untransformed coordinates.
+#'   - `range`: Deprecated, do not implement. Calls `backtransform_range()`.
 #'   - `transform`: Transforms x and y coordinates.
 #'   - `distance`: Calculates distance.
 #'   - `is_linear`: Returns `TRUE` if the coordinate system is
 #'     linear; `FALSE` otherwise.
 #'   - `is_free`: Returns `TRUE` if the coordinate system supports free
-#'     positional scales.
-#'   - `setup_panel_params(data)`:
+#'     positional scales; `FALSE` otherwise.
+#'   - `setup_panel_params(scale_x, scale_y, params)`: Determines the appropriate
+#'     x and y ranges for each panel, and also calculates anything else needed to
+#'     render the panel and axes, such as tick positions and labels for major
+#'     and minor ticks. Returns all this information in a named list.
 #'   - `setup_data(data, params)`: Allows the coordinate system to
 #'     manipulate the plot data. Should return list of data frames.
 #'   - `setup_layout(layout, params)`: Allows the coordinate
