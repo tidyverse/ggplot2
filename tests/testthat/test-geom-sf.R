3e9121d03cd5da8a44fb20d57cc21f44207edf17
Hiroaki Yutani <yutani.ini@gmail.com>
2018-08-11 12:33:49 +0900

    Add visual tests for geom_sf_label() and geom_sf_text()

--- tests/testthat/test-geom-sf.R
+++ tests/testthat/test-geom-sf.R
@@ -19,3 +19,21 @@ test_that("geom_sf draws correctly", {
     ggplot() + geom_sf(data = pts)
   )
 })
+
+test_that("geom_sf_text() and geom_sf_label() draws correctly", {
+  skip_if_not_installed("sf")
+  if (packageVersion("sf") < "0.5.3") skip("Need sf 0.5.3")
+
+  f <- system.file("gpkg/nc.gpkg", package="sf")
+  nc <- sf::read_sf(f)
+  # In order to avoid warning, trnasform to a projected coordinate system
+  nc_3857 <- sf::st_transform(nc, "+init=epsg:3857")
+
+  expect_doppelganger("Texts for North Carolina",
+    ggplot() + geom_sf_text(data = nc_3857[1:3, ], aes(label = NAME))
+  )
+
+  expect_doppelganger("Labels for North Carolina",
+    ggplot() + geom_sf_label(data = nc_3857[1:3, ], aes(label = NAME))
+  )
+})
