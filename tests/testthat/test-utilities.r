4b6b02f4ea2b34b86003555e6e2587814cb236ce
Kamil Slowikowski <kslowikowski@gmail.com>
2018-09-02 22:03:46 -0400

    move parse_safe() tests to test-utilities.r

--- tests/testthat/test-utilities.r
+++ tests/testthat/test-utilities.r
@@ -43,3 +43,52 @@ test_that("find_args behaves correctly", {
   # Defaults are overwritten
   expect_true(test_fun(arg2 = TRUE)$arg2)
 })
+
+test_that("parse_safe works with simple expressions", {
+  expect_equal(
+    parse_safe(c("", " ", "     ")),
+    expression(NA, NA, NA)
+  )
+
+  expect_equal(
+    parse_safe(c("A", "B", "C")),
+    expression(A, B, C)
+  )
+
+  expect_equal(
+    parse_safe(c("alpha", "", "gamma", " ")),
+    expression(alpha, NA, gamma, NA)
+  )
+
+  expect_equal(
+    parse_safe(c(NA, "a", NA, "alpha")),
+    expression(NA, a, NA, alpha)
+  )
+
+  expect_equal(
+    parse_safe(factor(c("alpha", "beta", ""))),
+    expression(2, 3, 1)
+  )
+})
+
+test_that("parse_safe works with multi expressions", {
+  expect_equal(
+    parse_safe(c(" \n", "\n ", " \n  \n  ")),
+    expression(NA, NA, NA)
+  )
+
+  expect_equal(
+    parse_safe(c("alpha ~ beta", "beta \n gamma", "")),
+    expression(alpha ~ beta, beta, NA)
+  )
+
+  expect_equal(
+    parse_safe(c("alpha ~ beta", " ", "integral(f(x) * dx, a, b)")),
+    expression(alpha ~ beta, NA, integral(f(x) * dx, a, b))
+  )
+
+  expect_equal(
+    parse_safe(c(NA, 1, 2, "a \n b")),
+    expression(NA, 1, 2, a)
+  )
+})
