59f20e2c3739f96c6d5e6f3830213bc89e4f95d6
RichardJActon <richardjacton@gmail.com>
2018-10-31 22:51:07 +0000

    updated expected error message

--- tests/testthat/test-geom-hline-vline-abline.R
+++ tests/testthat/test-geom-hline-vline-abline.R
@@ -60,7 +60,7 @@ test_that("Warning if a supplied mapping is going to be overwritten",{
 
   expect_warning(
     geom_abline(intercept = 3, aes(colour = colour)),
-    "Using both"
+    "Using "
   )
 
   expect_warning(
