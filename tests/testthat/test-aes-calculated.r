1a96032ee6922f77273623b864bb67a4c40dbc6c
Hiroaki Yutani <yutani.ini@gmail.com>
2018-11-11 11:13:23 +0900

    Soften the test for a long expression

--- tests/testthat/test-aes-calculated.r
+++ tests/testthat/test-aes-calculated.r
@@ -32,8 +32,9 @@ test_that("make_labels() deprases mappings properly", {
   # symbol is always deparsed without backticks
   expect_identical(make_labels(aes(x = `a b`)), list(x = "a b"))
   # long expression is abbreviated with ...
-  expect_identical(make_labels(aes(x = 2 * x * exp(`coef 1` * x^2) * 2 * x * exp(`coef 1` * x^2) * 2 * x)),
-                   list(x = "2 * x * exp(`coef 1` * x^2) * 2 * x * exp(`coef 1` * x^2) * 2 * ..."))
+  x_lab <- make_labels(aes(x = 2 * x * exp(`coef 1` * x^2) * 2 * x * exp(`coef 1` * x^2) * 2 * x))$x
+  expect_length(x_lab, 1L)
+  expect_match(x_lab, "...$")
   # if the mapping is a literal or NULL, the aesthetics is used
   expect_identical(make_labels(aes(x = 1)), list(x = "x"))
   expect_identical(make_labels(aes(x = NULL)), list(x = "x"))
