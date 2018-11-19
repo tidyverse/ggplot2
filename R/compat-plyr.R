35b3d1fd754b0af20b3bc833c3076dc97e490b81
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-11-19 14:29:31 +0100

    start porting plyr functions

---
+++ R/compat-plyr.R
@@ -0,0 +1,113 @@
+defaults <- function(x, y) c(x, y[setdiff(names(y), names(x))])
+unrowname <- function(x) {
+  if (is.data.frame(x)) {
+    attr(x, "row.names") <- .set_row_names(.row_names_info(x, 2L))
+  } else if (is.matrix(x)) {
+    dimnames(x)[1] <- list(NULL)
+  } else {
+    stop("Can only remove rownames from data.frame and matrix objects", call. = FALSE)
+  }
+  x
+}
+rename <- function(x, replace) {
+  current_names <- names(x)
+  old_names <- names(replace)
+  missing_names <- setdiff(old_names, current_names)
+  if (length(missing_names) > 0) {
+    replace <- replace[!old_names %in% missing_names]
+    old_names <- names(replace)
+  }
+  names(x)[match(old_names, current_names)] <- as.vector(replace)
+  x
+}
+# Adapted from plyr:::id_vars
+id_var <- function(x, drop = FALSE) {
+  if (length(x) == 0) {
+    id <- integer()
+    n = 0L
+  } else if (!is.null(attr(x, "n")) && !drop) {
+    return(x)
+  } else if (is.factor(x) && !drop) {
+    x <- addNA(x, ifany = TRUE)
+    id <- as.integer(x)
+    n <- length(levels(x))
+  } else {
+    levels <- sort(unique(x), na.last = TRUE)
+    id <- match(x, levels)
+    n <- max(id)
+  }
+  attr(id, "n") <- n
+  id
+}
+# Adapted from plyr::id
+id <- function(.variables, drop = FALSE) {
+  nrows <- NULL
+  if (is.data.frame(.variables)) {
+    nrows <- nrow(.variables)
+    .variables <- unclass(.variables)
+  }
+  lengths <- vapply(.variables, length, integer(1))
+  .variables <- .variables[lengths != 0]
+  if (length(.variables) == 0) {
+    n <- nrows %||% 0L
+    id <- seq_len(n)
+    attr(id, "n") <- n
+    return(id)
+  }
+  if (length(.variables) == 1) {
+    return(id_var(.variables[[1]], drop = drop))
+  }
+  ids <- rev(lapply(.variables, id_var, drop = drop))
+  p <- length(ids)
+  ndistinct <- vapply(ids, attr, "n", FUN.VALUE = numeric(1), USE.NAMES = FALSE)
+  n <- prod(ndistinct)
+  if (n > 2^31) {
+    char_id <- do.call("paste", c(ids, sep = "\r"))
+    res <- match(char_id, unique(char_id))
+  }
+  else {
+    combs <- c(1, cumprod(ndistinct[-p]))
+    mat <- do.call("cbind", ids)
+    res <- c((mat - 1L) %*% combs + 1L)
+  }
+  if (drop) {
+    id_var(res, drop = TRUE)
+  }
+  else {
+    res <- as.integer(res)
+    attr(res, "n") <- n
+    res
+  }
+}
+
+count <- function (df, vars = NULL, wt_var = NULL) {
+  if (is.atomic(df)) {
+    df <- new_data_frame(list(x = df))
+  }
+  if (!is.null(vars)) {
+    df2 <- quickdf(eval.quoted(vars, df))
+  }
+  else {
+    df2 <- df
+  }
+  id <- ninteraction(df2, drop = TRUE)
+  u_id <- !duplicated(id)
+  labels <- df2[u_id, , drop = FALSE]
+  labels <- labels[order(id[u_id]), , drop = FALSE]
+  if (is.null(wt_var) && "freq" %in% names(df)) {
+    message("Using freq as weighting variable")
+    wt_var <- "freq"
+  }
+  if (!is.null(wt_var)) {
+    wt_var <- as.quoted(wt_var)
+    if (length(wt_var) > 1) {
+      stop("wt_var must be a single variable", call. = FALSE)
+    }
+    wt <- eval.quoted(wt_var, df)[[1]]
+    freq <- vaggregate(wt, id, sum, .default = 0)
+  }
+  else {
+    freq <- tabulate(id, attr(id, "n"))
+  }
+  unrowname(data.frame(labels, freq))
+}
