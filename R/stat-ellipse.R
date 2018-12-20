9c787ebf369d903c0831c34aeebd47a04fbf3006
Patrick Toche <contact@patricktoche.com>
2018-12-20 18:16:35 +0800

    Update stat-ellipse.R
    based on the original ``stat-ellipse.R``, with minor changes + annotations

--- R/stat-ellipse.R
+++ R/stat-ellipse.R
@@ -1,6 +1,6 @@
-#' Compute data/confidence ellipses for a bivariate dataset
+#' Compute data ellipses for a bivariate dataset
 #'
-#' The method for calculating the ellipses has been modified from
+#' The method for calculating the ellipses is adapted from
 #' `car::ellipse` (https://CRAN.R-project.org/package=car )
 #'
 #' @references John Fox and Sanford Weisberg (2019). An {R} Companion to
@@ -15,7 +15,6 @@
 #'   `"euclid"` draws a circle with the radius equal to `level`.
 #'   representing the euclidean distance from the center.
 #'   This ellipse probably won't appear circular unless `coord_fixed()` is applied.
-#'   `"confidence" draws a confidence ellipse for a given regression model.
 #' @param segments The number of segments to be used in drawing the ellipse.
 #' @inheritParams layer
 #' @inheritParams geom_point
@@ -37,36 +36,13 @@
 #' ggplot(faithful, aes(waiting, eruptions, color = eruptions > 3)) +
 #'   geom_point() +
 #'   stat_ellipse(type = "norm", linetype = 2) +
-#'   stat_ellipse(type = "conf", color = "red", size = 2)
-#'
-#' ggplot(faithful, aes(waiting, eruptions, color = eruptions > 3)) +
-#'   geom_point() +
-#'   stat_ellipse(type = "norm", linetype = 2) +
 #'   stat_ellipse(type = "euclid", level = 3) +
 #'   coord_fixed()
 #'
 #' ggplot(faithful, aes(waiting, eruptions, fill = eruptions > 3)) +
 #'   stat_ellipse(geom = "polygon")
-#'
-#' TO DO: Still at the debugging stage: confidence ellipse doesn't look right!
-#' TO DO: add a method argument, e.g. method = "lm", method = "glm", etc.
-#' TO DO: allow partial matching so 'type = "normal"' and 'type = "conf"' work
-#' TO DO: allow formula to be passed as string "y ~ x" 
-#' TO DO: make sure methods "t" and "norm" can find "x" and "y" in multivariate dataset
-#' TO DO: allow overruling of degrees of freedom with options dfn and dfd 
-#' TO DO: check that option 'segments' works 
-#' TO DO: should I use stats::qf() or simply qf() ?
-#' TO DO: pass fill and alpha to the ellipse constructor
-#' TO DO: Why not use center <- colMeans(data[,vars]) ?
-#' TO DO: allow overruling of center. 
-#' TO DO: add prediction ellipses as well as confidence ellipses!
-#' 
-#' 
-stat_ellipse <- function(data = NULL, 
-                         mapping = NULL, 
-                         formula = NULL, 
-                         geom = "path", 
-                         position = "identity",
+stat_ellipse <- function(mapping = NULL, data = NULL,
+                         geom = "path", position = "identity",
                          ...,
                          type = "t",
                          level = 0.95,
@@ -74,10 +50,6 @@ stat_ellipse <- function(data = NULL,
                          na.rm = FALSE,
                          show.legend = NA,
                          inherit.aes = TRUE) {
-
-is.formula <- function(x) inherits(x, "formula") 
-if (!is.formula(formula)) stop("argument 'formula' for confidence ellipses must be passed as a formula, e.g. 'formula = y ~ x' without quote marks. If type is set to 'confidence' The function takes the formula and applies the 'linear model' function 'lm(formula)' (ordinary least squares). It cannot currently handle other models. When passing multivariate models, the variable of interest must be passed first, e.g. if 'y ~ x + z', the variables 'y' and 'x' will be used to compute the ellipse.  Degrees of freedom are computed from the model and cannot be overruled.", call. = FALSE)
-
   layer(
     data = data,
     mapping = mapping,
@@ -87,7 +59,6 @@ if (!is.formula(formula)) stop("argument 'formula' for confidence ellipses must 
     show.legend = show.legend,
     inherit.aes = inherit.aes,
     params = list(
-      formula = formula, 
       type = type,
       level = level,
       segments = segments,
@@ -103,15 +74,16 @@ if (!is.formula(formula)) stop("argument 'formula' for confidence ellipses must 
 #' @export
 StatEllipse <- ggproto("StatEllipse", Stat,
   required_aes = c("x", "y"),
-  compute_group = function(data, formula = y ~ x, scales, type = "t", level = 0.95,
+
+  compute_group = function(data, scales, type = "t", level = 0.95,
                            segments = 51, na.rm = FALSE) {
-    calculate_ellipse(data = data, vars = c("x", "y"), formula = formula, type = type,
+    calculate_ellipse(data = data, vars = c("x", "y"), type = type,
                       level = level, segments = segments)
   }
 )
 
-calculate_ellipse <- function(data, vars, formula, type, level, segments){
-  
+calculate_ellipse <- function(data, vars, type, level, segments){
+
   # accept only recognized ellipse types
   if (!type %in% c("t", "norm", "euclid", "confidence")) {
     message("Unrecognized ellipse type")
@@ -125,75 +97,38 @@ calculate_ellipse <- function(data, vars, formula, type, level, segments){
   # compute a unit circle as basis for ellipse (used by all types)
   angles <- (0:segments) * 2 * pi/segments
   unit.circle <- cbind(cos(angles), sin(angles))
+  
+  # compute degrees of freedom for types "norm", "t", and "euclid"
+  dfn <- 2
+  dfd <- nrow(data) - 1
 
-  # compute ellipse for each type 
-  # start with "confidence" ellipse 
-  if (type == "confidence") {
-    print("Just entered 'type == confidence':")  # remove after debugging
-    # compute center of ellipse
-    #center <- colMeans(data[,vars])
-    center <- MASS::cov.trob(data[,vars])$center  # car uses this, why not colMeans?
-    # fit a linear model
-    fit <- lm(formula, data = data) 
-    # compute degrees of freedom
-    dfn <- length(coef(fit))
-    dfd <- df.residual(fit)
-    # compute variance/covariance matrix
-    vcov <- vcov(fit)
-    # extract appropriate coefficients from model 
-      # only handles linear model with intercept
-      # x variable must be first passed
-    which.coef <- c(1, 2) 
-    shape <- vcov[which.coef, which.coef] 
-    # compute rotation of ellipse via Cholesky decomposition
-    # code from "car" package, version 3.0-2 (18 December 2018)
-    Q <- chol(shape, pivot = TRUE) 
-    order <- order(attr(Q, "pivot"))
-    rotation <- t(unit.circle %*% Q[, order]) 
-    # older versions of the "car" package used:
-      # Q <- chol(shape, pivot = FALSE)
-      # rotation <- t(unit.circle %*% Q) 
-    # compute radius of ellipse 
-    radius <- sqrt(stats::qchisq(level, dfn))
-    # center/radius/rotation variables have now been computed for type "confidence"
-  # compute ellipse for types other than "confidence"
-  } else {
-    # compute degrees of freedom for types "norm", "t", and "euclid"
-    dfn <- 2
-    dfd <- nrow(data) - 1
-    # compute variance/covariance matrix 
-    if (type == "t") { 
-      v <- MASS::cov.trob(data[,vars])
-    } else if (type == "norm") {
-      v <- stats::cov.wt(data[,vars])
-    } else if (type == "euclid") {
-      v <- stats::cov.wt(data[,vars]) 
-      v$cov <- diag(rep(min(diag(v$cov)), 2))
-    }
-    # compute center of ellipse
-    center <- v$center
-    # compute rotation of ellipse via Cholesky decomposition
-    shape <- v$cov
-    Q <- chol(shape, pivot = TRUE) 
-    order <- order(attr(Q, "pivot"))
-    rotation <- t(unit.circle %*% Q[, order]) 
-    # compute radius of ellipse
-    if (type == "euclid") {
-      radius <- level/max(Q) 
-    } else {
-      radius <- sqrt(dfn * stats::qf(level, dfn, dfd)) 
-    }
+  # compute variance/covariance matrix 
+  if (type == "euclid") {
+    v <- stats::cov.wt(data[,vars]) 
+    v$cov <- diag(rep(min(diag(v$cov)), 2))
+  } else if (type == "t") { 
+    v <- MASS::cov.trob(data[,vars])
+  } else  { # type == "norm" is the third and last option
+    v <- stats::cov.wt(data[,vars])
   }
-  # center/radius/rotation variables have now been computed for all types
+
+  # compute center of ellipse
+  center <- v$center
+
+  # compute rotation of ellipse via Cholesky decomposition
+  shape <- v$cov
+  Q <- chol(shape, pivot = TRUE) 
+  order <- order(attr(Q, "pivot"))
+  rotation <- t(unit.circle %*% Q[, order]) 
+  # compute radius of ellipse
+  if (type == "euclid") {
+    radius <- level/max(Q) 
+  } else {
+    radius <- sqrt(dfn * stats::qf(level, dfn, dfd)) 
+  }
+
   # compute the ellipse from the center/radius/rotation variables
   ellipse <- t(center + radius * rotation)  
   colnames(ellipse) <- vars 
   mat_2_df(ellipse) 
-  
-  # show data in console for debugging purposes
-  ellipse <- mat_2_df(ellipse) 
-  message("Debug: sorry for filling your console with data!")
-  print(ellipse) 
-  return(ellipse)
-
 }
