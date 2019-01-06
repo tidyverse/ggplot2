d0c435a876a1a59f49a3b699940a356cbb1b7068
Hiroaki Yutani <yutani.ini@gmail.com>
2019-01-06 22:40:40 +0900

    Do not check vdiffr's version in tests

--- tests/testthat/helper-vdiffr.R
+++ tests/testthat/helper-vdiffr.R
@@ -2,10 +2,4 @@
 #   - VDIFFR_RUN_TESTS is "false" (on Travis CI with older versions and dev version of R)
 #   - CI is not set (on CRAN)
 
-# disable vdiffr if version is old
-if (!requireNamespace("vdiffr", quietly = TRUE) ||
-  utils::packageVersion("vdiffr") < "0.2.3.9001") {
-  Sys.setenv(VDIFFR_RUN_TESTS = "false")
-}
-
 expect_doppelganger <- vdiffr::expect_doppelganger
