ddf99b8b4433a2298805cc760a5e1ecc2c974206
Claus Wilke <wilke@austin.utexas.edu>
2019-01-29 23:26:09 -0600

    separate out stat_sf() code

--- R/sf.R
+++ R/sf.R
@@ -97,49 +97,6 @@ is_sf <- function(data) {
   inherits(data, "sf")
 }
 
-# stat --------------------------------------------------------------------
-
-#' @export
-#' @rdname ggsf
-#' @usage NULL
-#' @format NULL
-StatSf <- ggproto("StatSf", Stat,
-  compute_group = function(data, scales) {
-    bbox <- sf::st_bbox(data[[ geom_column(data) ]])
-    data$xmin <- bbox[["xmin"]]
-    data$xmax <- bbox[["xmax"]]
-    data$ymin <- bbox[["ymin"]]
-    data$ymax <- bbox[["ymax"]]
-
-    data
-  },
-
-  required_aes = c("geometry")
-)
-
-#' @export
-#' @rdname ggsf
-#' @inheritParams stat_identity
-stat_sf <- function(mapping = NULL, data = NULL, geom = "rect",
-                    position = "identity", na.rm = FALSE, show.legend = NA,
-                    inherit.aes = TRUE, ...) {
-  layer(
-    stat = StatSf,
-    data = data,
-    mapping = mapping,
-    geom = geom,
-    position = position,
-    show.legend = if (is.character(show.legend)) TRUE else show.legend,
-    inherit.aes = inherit.aes,
-    params = list(
-      na.rm = na.rm,
-      legend = if (is.character(show.legend)) show.legend else "polygon",
-      ...
-    ),
-    layer_class = LayerSf
-  )
-}
-
 
 # A special sf layer that auto-maps geometry data to the `geometry` aesthetic
 
