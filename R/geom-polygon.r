5f6925f838b9cc1a850fc810d00fe653d8c4ee2e
Thomas Lin Pedersen <thomasp85@gmail.com>
2019-02-12 15:24:10 +0100

    make last polygon example conditional on grid version

--- R/geom-polygon.r
+++ R/geom-polygon.r
@@ -60,21 +60,24 @@
 #' # And if the positions are in longitude and latitude, you can use
 #' # coord_map to produce different map projections.
 #'
-#' # As of R version 3.6 geom_polygon() supports polygons with holes
-#' # Use the subgroup aesthetic to differentiate holes from the main polygon
+#' if (packageVersion("grid") >= "3.6") {
+#'   # As of R version 3.6 geom_polygon() supports polygons with holes
+#'   # Use the subgroup aesthetic to differentiate holes from the main polygon
 #'
-#' holes <- do.call(rbind, lapply(split(datapoly, datapoly$id), function(df) {
-#'   df$x <- df$x + 0.5 * (mean(df$x) - df$x)
-#'   df$y <- df$y + 0.5 * (mean(df$y) - df$y)
-#'   df
-#' }))
-#' datapoly$subid <- 1L
-#' holes$subid <- 2L
-#' datapoly <- rbind(datapoly, holes)
+#'   holes <- do.call(rbind, lapply(split(datapoly, datapoly$id), function(df) {
+#'     df$x <- df$x + 0.5 * (mean(df$x) - df$x)
+#'     df$y <- df$y + 0.5 * (mean(df$y) - df$y)
+#'     df
+#'   }))
+#'   datapoly$subid <- 1L
+#'   holes$subid <- 2L
+#'   datapoly <- rbind(datapoly, holes)
 #'
-#' p <- ggplot(datapoly, aes(x = x, y = y)) +
-#'   geom_polygon(aes(fill = value, group = id, subgroup = subid))
-#' p
+#'   p <- ggplot(datapoly, aes(x = x, y = y)) +
+#'     geom_polygon(aes(fill = value, group = id, subgroup = subid))
+#'   p
+#' }
+#'
 geom_polygon <- function(mapping = NULL, data = NULL,
                          stat = "identity", position = "identity",
                          rule = "evenodd",
