bb96c58dee2a6b5744610f8d3beee357573fb7a1
jrnold <jeffrey.arnold@gmail.com>
2019-02-15 20:23:13 -0800

    Fix order of diamond color factor.
    The factor order of the diamonds dataset is currently `D < ... < J`, but since "D" is the best grade, it should be `J < ... < D`.
    
    https://en.wikipedia.org/wiki/Diamond_color#Grading_fancy_color_diamonds

--- data-raw/diamonds.R
+++ data-raw/diamonds.R
@@ -2,7 +2,7 @@ library(readr)
 diamonds <- read_csv("data-raw/diamonds.csv", col_types =
   list(
     cut = col_factor(c("Fair", "Good", "Very Good", "Premium", "Ideal"), TRUE),
-    color = col_factor(c("D", "E", "F", "G", "H", "I", "J"), TRUE),
+    color = col_factor(c("J", "I", "H", "G", "F", "E", "D"), TRUE),
     clarity = col_factor(c("I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"), TRUE)
   )
 )
