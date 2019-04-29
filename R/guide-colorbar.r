7582d608611965eb16d19db161881d6a64e840f4
Claus Wilke <wilke@austin.utexas.edu>
2019-04-29 09:38:49 -0500

    fix bad default for legend spacing variables. closes #2943. (#3276)

--- R/guide-colorbar.r
+++ R/guide-colorbar.r
@@ -342,7 +342,8 @@ guide_gengrob.colorbar <- function(guide, theme) {
 
   title_width <- width_cm(grob.title)
   title_height <- height_cm(grob.title)
-  title_fontsize <- title.theme$size %||% calc_element("legend.title", theme)$size %||% 0
+  title_fontsize <- title.theme$size %||% calc_element("legend.title", theme)$size %||%
+    calc_element("text", theme)$size %||% 11
 
   # gap between keys etc
   # the default horizontal and vertical gap need to be the same to avoid strange
