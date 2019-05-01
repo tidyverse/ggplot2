18132314182cef2457353f43e0fa9af20af372a0
Dewey Dunnington <dewey@fishandwhistle.net>
2019-05-01 12:48:03 -0300

    redo warnings for overwritten arguments in geom_abline(), geom_hline(), and geom_vline()

--- R/geom-hline.r
+++ R/geom-hline.r
@@ -11,14 +11,14 @@ geom_hline <- function(mapping = NULL, data = NULL,
 
   # Act like an annotation
   if (!missing(yintercept)) {
-    # Warn if supplied mapping is going to be overwritten
-    if (!missing(mapping)) {
-      warning(paste0("Using both `yintercept` and `mapping` may not have the",
-                     " desired result as mapping is overwritten if",
-                     " `yintercept` is specified\n"
-              )
-      )
+    # Warn if supplied mapping and/or data is going to be overwritten
+    if (!is.null(mapping)) {
+      warn_overwritten_args("geom_hline()", "mapping", "yintercept")
     }
+    if (!is.null(data)) {
+      warn_overwritten_args("geom_hline()", "data", "yintercept")
+    }
+
     data <- new_data_frame(list(yintercept = yintercept))
     mapping <- aes(yintercept = yintercept)
     show.legend <- FALSE
