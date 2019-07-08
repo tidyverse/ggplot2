f0986ada1ae05d43c7802a918e6d144e2453207b
Sina Rueeger <sina.rueeger@gmail.com>
2019-07-08 11:32:24 +0200

    Adding explicit bins, binwidth + breaks arguments, and passing them onto the parameter list params.

--- R/stat-contour.r
+++ R/stat-contour.r
@@ -11,6 +11,9 @@
 stat_contour <- function(mapping = NULL, data = NULL,
                          geom = "contour", position = "identity",
                          ...,
+                         bins = NULL,
+                         binwidth = NULL,
+                         breaks = NULL,
                          na.rm = FALSE,
                          show.legend = NA,
                          inherit.aes = TRUE) {
@@ -23,6 +26,9 @@ stat_contour <- function(mapping = NULL, data = NULL,
     show.legend = show.legend,
     inherit.aes = inherit.aes,
     params = list(
+      bins = bins,
+      binwidth = binwidth,
+      breaks = breaks,
       na.rm = na.rm,
       ...
     )
