e39c0241ce50a0c88da098a5365ff1dbf848fd8c
Elesday <joris.falip@gmail.com>
2019-07-08 16:00:30 +0200

    Added link to `coord_cartesian()`

--- R/scale-.r
+++ R/scale-.r
@@ -40,7 +40,7 @@
 #'     new limits
 #'
 #'  Note that setting limits on positional scales will **remove** data outside of the limits.
-#'  If the purpose is to zoom, use the limit argument in the coordinate system.
+#'  If the purpose is to zoom, use the limit argument in the coordinate system (see [coord_cartesian()]).
 #' @param rescaler A function used to scale the input values to the
 #'   range \[0, 1]. This is always [scales::rescale()], except for
 #'   diverging and n colour gradients (i.e., [scale_colour_gradient2()],
