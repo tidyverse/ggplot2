fd3fa77a49210cfbfe61033c11b53163261743d9
Thomas Lin Pedersen <thomasp85@gmail.com>
2019-08-28 14:57:15 +0200

    Add errorbar

--- R/geom-errorbar.r
+++ R/geom-errorbar.r
@@ -26,26 +26,46 @@ geom_errorbar <- function(mapping = NULL, data = NULL,
 #' @usage NULL
 #' @export
 GeomErrorbar <- ggproto("GeomErrorbar", Geom,
-  default_aes = aes(colour = "black", size = 0.5, linetype = 1, width = 0.5,
+  default_aes = aes(x = NULL, y = NULL, xmin = NULL, xmax = NULL, ymin = NULL,
+    ymax = NULL, colour = "black", size = 0.5, linetype = 1, width = 0.5,
     alpha = NA),
 
   draw_key = draw_key_path,
 
-  required_aes = c("x", "ymin", "ymax"),
-
   setup_data = function(data, params) {
+    if (all(c("y", "xmin", "xmax") %in% names(data))) {
+      main_aes <- "y"
+    } else if (all(c("x", "ymin", "ymax") %in% names(data))) {
+      main_aes <- "x"
+    } else {
+      stop("Either, `x`, `ymin`, and `ymax` or `y`, `xmin`, and `xmax` must be supplied", call. = FALSE)
+    }
+    data$main_aes <- main_aes
     data$width <- data$width %||%
-      params$width %||% (resolution(data$x, FALSE) * 0.9)
+      params$width %||% (resolution(data[[main_aes]], FALSE) * 0.9)
 
-    transform(data,
-      xmin = x - width / 2, xmax = x + width / 2, width = NULL
+    switch(main_aes,
+      x = transform(data,
+        xmin = x - width / 2, xmax = x + width / 2, width = NULL
+      ),
+      y = transform(data,
+        ymin = y - width / 2, ymax = y + width / 2, width = NULL
+      )
     )
+
   },
 
   draw_panel = function(data, panel_params, coord, width = NULL) {
+    if (data$main_aes[1] == "x") {
+      x <- as.vector(rbind(data$xmin, data$xmax, NA, data$x,    data$x,    NA, data$xmin, data$xmax))
+      y <- as.vector(rbind(data$ymax, data$ymax, NA, data$ymax, data$ymin, NA, data$ymin, data$ymin))
+    } else {
+      x = as.vector(rbind(data$xmax, data$xmax, NA, data$xmax, data$xmin, NA, data$xmin, data$xmin))
+      y = as.vector(rbind(data$ymin, data$ymax, NA, data$y,    data$y,    NA, data$ymin, data$ymax))
+    }
     GeomPath$draw_panel(new_data_frame(list(
-      x = as.vector(rbind(data$xmin, data$xmax, NA, data$x,    data$x,    NA, data$xmin, data$xmax)),
-      y = as.vector(rbind(data$ymax, data$ymax, NA, data$ymax, data$ymin, NA, data$ymin, data$ymin)),
+      x = x,
+      y = y,
       colour = rep(data$colour, each = 8),
       alpha = rep(data$alpha, each = 8),
       size = rep(data$size, each = 8),
