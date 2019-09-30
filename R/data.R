2eee253dc7164416766fcff4a4d16a528dfde17b
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2019-09-30 13:28:46 +0100

    Clarify nrow in function title

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' Prices of 50,000 round cut diamonds
+#' Prices of over 50,000 round cut diamonds
 #'
 #' A dataset containing the prices and other attributes of almost 54,000
 #'  diamonds. The variables are as follows:
