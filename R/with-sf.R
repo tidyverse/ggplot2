80604091c3bce6e74e8924156f63111458d793b1
Claus Wilke <wilke@austin.utexas.edu>
2019-12-04 14:59:08 -0600

    progress

--- R/with-sf.R
+++ R/with-sf.R
@@ -1,34 +1,64 @@
 #' Turn regular layers into sf layers
 #'
 #' @param ... Layer
+#' @param crs Coordinate reference system of the origin layer. Defaults to
+#'   longitude and latitude in WGS84 (EPSG:4326), the World Geodetic System
+#'   from 1984.
 #' @export
-with_sf <- function(...) {
-  l <- list(...)[[1]] # can handle only one layer for now
-  parent_geom <- l$geom
-  ggproto(NULL, l,
-    geom = ggproto('SfedGeom', parent_geom,
+with_sf <- function(..., crs = 4326) {
+  x <- list(...)
+  with_sf_impl(x, crs)
+}
+
+with_sf_impl <- function(x, crs, ...) {
+  UseMethod("with_sf_impl")
+}
+
+with_sf_impl.default <- function(x, crs, ...) {
+  stop(
+    "Can't convert object of class `", class(x), "` to an sf layer.\n",
+    call. = FALSE
+  )
+}
+
+with_sf_impl.list <- function(x, crs, ...) {
+  l <- list()
+  # for some reason lapply() version of this doesn't work
+  for (i in seq_along(x)) {
+    l[[i]] <- with_sf_impl(x[[i]], crs)
+  }
+  if (length(l) == 1) return(l[[1]])
+  l
+}
+
+with_sf_impl.Layer <- function(x, crs, ...) {
+  parent_geom <- x$geom
+  ggproto(NULL, x,
+    geom = ggproto('SfifiedGeom', parent_geom,
       draw_panel = function(data, panel_params, coord, na.rm = FALSE) {
-        sfed_coord <- ggproto('SfedCoord', coord,
+        sfified_coord <- ggproto('SfifiedCoord', coord,
           transform = function(data, range) {
-            data <- sfed_transform(data, crs = coord$crs)
+            data <- sfified_transform(data, crs = coord$crs, source_crs = crs)
             coord$transform(data, range)
           }
+          ## TODO:
+          ## need to implement backtransform_range()
         )
-        parent_geom$draw_panel(data, panel_params, sfed_coord, na.rm)
+        parent_geom$draw_panel(data, panel_params, sfified_coord, na.rm)
       }
     )
   )
 }
 
 
-sfed_transform <- function(data, crs = NULL) {
+sfified_transform <- function(data, crs = NULL, source_crs = 4326) {
   if (is.null(crs)) {
     return(data)
   }
 
   sf_data <- sf::st_sfc(
     sf::st_multipoint(cbind(data$x, data$y)),
-    crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+    crs = source_crs
   )
   sf_data_trans <- sf::st_transform(sf_data, crs)[[1]]
   data$x <- sf_data_trans[, 1]
