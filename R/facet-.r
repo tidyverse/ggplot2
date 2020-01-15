9d48e3d34ec3c3deddbeece318938d2236873fc3
Hiroaki Yutani <yutani.ini@gmail.com>
2020-01-15 23:37:11 +0900

    Remove a shortcut when facet is a quosure of a symbol

--- R/facet-.r
+++ R/facet-.r
@@ -423,17 +423,6 @@ eval_facets <- function(facets, data, possible_columns = NULL) {
   new_data_frame(tibble::as_tibble(vars))
 }
 eval_facet <- function(facet, data, possible_columns = NULL) {
-  if (quo_is_symbol(facet)) {
-    facet <- as.character(quo_get_expr(facet))
-
-    if (facet %in% names(data)) {
-      out <- data[[facet]]
-    } else {
-      out <- NULL
-    }
-    return(out)
-  }
-
   # clone the env in order to prevent side effects (hopefully)
   env <- env_clone(quo_get_env(facet))
 
