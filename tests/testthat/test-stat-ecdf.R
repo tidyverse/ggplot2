a8ba9bd873fa6a3026ac21c37a958bef1b245b8f
Jeremias Blendin <303581+jgjl@users.noreply.github.com>
2020-05-16 20:32:48 -0700

    Update stat_ecdf to work either on the x or the y aesthetic, whatever is provided.

---
+++ tests/testthat/test-stat-ecdf.R
@@ -0,0 +1,16 @@
+context("stat_ecdf")
+
+test_that("stat_ecdf works in both directions", {
+  p <- ggplot(mpg, aes(hwy)) + stat_ecdf()
+  x <- layer_data(p)
+  expect_false(x$flipped_aes[1])
+
+  p <- ggplot(mpg, aes(y = hwy)) + stat_ecdf()
+  y <- layer_data(p)
+  expect_true(y$flipped_aes[1])
+
+  x$flipped_aes <- NULL
+  y$flipped_aes <- NULL
+  expect_identical(x, flip_data(y, TRUE)[,names(x)])
+})
+
