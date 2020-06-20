db8f9d9b731708b8aaafdc76bdc4debcb5f1be9b
Hiroaki Yutani <yutani.ini@gmail.com>
2020-06-20 17:13:12 +0900

    Remove @importFrom

--- R/utilities-tidy-eval.R
+++ R/utilities-tidy-eval.R
@@ -41,7 +41,6 @@
 #' @md
 #' @name tidyeval
 #' @keywords internal
-#' @importFrom rlang expr enquo enquos sym syms .data := as_name as_label
 #' @aliases expr enquo enquos sym syms .data := as_name as_label
 #' @export expr enquo enquos sym syms .data := as_name as_label
 NULL
@@ -50,6 +49,5 @@ NULL
 # superseded by `as_label()` and `as_name()`.
 
 #' @name tidyeval
-#' @importFrom rlang quo_name
 #' @export quo_name
 NULL
