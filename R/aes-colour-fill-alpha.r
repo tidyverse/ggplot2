03fcd7056c82a4cc2c2ad833f83ab8f7fd6890cf
rjake <rjake@sas.upenn.edu>
2020-06-26 14:55:04 -0400

    Style aes_colour_fill_alpha

--- R/aes-colour-fill-alpha.r
+++ R/aes-colour-fill-alpha.r
@@ -92,8 +92,11 @@
 #' p <- ggplot(economics, aes(x = date, y = unemploy)) + geom_line()
 #' p
 #' yrng <- range(economics$unemploy)
-#' p <- p + geom_rect(aes(NULL, NULL, xmin = start, xmax = end, fill = party),
-#' ymin = yrng[1], ymax = yrng[2], data = presidential)
+#' p <- p +
+#'   geom_rect(
+#'     aes(NULL, NULL, xmin = start, xmax = end, fill = party),
+#'     ymin = yrng[1], ymax = yrng[2], data = presidential
+#'   )
 #' p
 #' p + scale_fill_manual(values = alpha(c("blue", "red"), .3))
 #' }
