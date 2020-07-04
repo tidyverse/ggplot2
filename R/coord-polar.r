31572dcbadc40b51e31d28fef85f7299f589cea1
Elio Campitelli <eliocampitelli@gmail.com>
2020-07-04 01:07:22 -0300

    Doesn't try to combine labels if labels are NULL (#4115)
    * Doesn't try to combine labels if labels are NULL
    
    * Changes & to &&

--- R/coord-polar.r
+++ R/coord-polar.r
@@ -281,7 +281,7 @@ CoordPolar <- ggproto("CoordPolar", Coord,
     # Combine the two ends of the scale if they are close
     theta <- theta[!is.na(theta)]
     ends_apart <- (theta[length(theta)] - theta[1]) %% (2*pi)
-    if (length(theta) > 0 && ends_apart < 0.05) {
+    if (length(theta) > 0 && ends_apart < 0.05 && !is.null(labels)) {
       n <- length(labels)
       if (is.expression(labels)) {
         combined <- substitute(paste(a, "/", b),
