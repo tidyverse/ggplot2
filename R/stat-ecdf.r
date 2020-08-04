a3760560f69c835d617b6cc2ec50ba243ce70386
Jeremias Blendin <303581+jgjl@users.noreply.github.com>
2020-08-03 21:19:49 -0700

    Fix typo in comments.

--- R/stat-ecdf.r
+++ R/stat-ecdf.r
@@ -9,7 +9,7 @@
 #'
 #' The statistic relies on the aesthetics assignment to guess which variable as
 #' input and as output. Either x or y must be provided and one of them must be
-#' unused. The ECDF will be caculated on the given aesthetic and will be output
+#' unused. The ECDF will be calculated on the given aesthetic and will be output
 #' on the unused one.
 #'
 #' @inheritParams layer
