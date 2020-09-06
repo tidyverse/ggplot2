a7ecb1a22711cad41cb08321e388044a1f5d08d0
Hiroaki Yutani <yutani.ini@gmail.com>
2020-09-06 16:02:42 +0900

    Skip visual tests when vdiffr is not available

--- tests/testthat/helper-vdiffr.R
+++ tests/testthat/helper-vdiffr.R
@@ -2,4 +2,9 @@
 #   - VDIFFR_RUN_TESTS is "false" (on Travis CI with older versions and dev version of R)
 #   - CI is not set (on CRAN)
 
-expect_doppelganger <- vdiffr::expect_doppelganger
+if (requireNamespace("vdiffr", quietly = TRUE)) {
+  expect_doppelganger <- vdiffr::expect_doppelganger
+} else {
+  # If vdiffr is not available, assign a dummy function
+  expect_doppelganger <- function(...) skip("vdiffr is not installed.")
+}
