b178d22de3c7e85b7e4e6d144969ba4c6ea30e22
Hiroaki Yutani <yutani.ini@gmail.com>
2021-04-26 21:22:54 +0900

    Fix tests/testthat/test-prohibited-functions.R (#4446)
    * Do not raise error for `base::data.frame()`
    
    * Add a failing test
    
    * Add a path for R CMD check

--- tests/testthat/test-prohibited-functions.R
+++ tests/testthat/test-prohibited-functions.R
@@ -12,7 +12,14 @@ get_n_warning <- function(f) {
 
 get_n_data.frame <- function(f) {
   d <- getParseData(parse(f, keep.source = TRUE))
-  sum(d$token == "SYMBOL_FUNCTION_CALL" & d$text == "data.frame")
+
+  idx_base <- d$token == "SYMBOL_PACKAGE" & d$text == "base"
+  idx_colons <- d$token == "NS_GET" & d$text == "::"
+  # exclude the case when the `data.frame` is prefixed with `base::`
+  idx_base_prefixed <- c(FALSE, FALSE, idx_base[1:(nrow(d) - 2)]) & c(FALSE, idx_colons[1:(nrow(d) - 1)])
+
+  idx_data.frame <- d$token == "SYMBOL_FUNCTION_CALL" & d$text == "data.frame"
+  sum(idx_data.frame & !idx_base_prefixed)
 }
 
 test_that("`get_n_*() detects number of calls properly", {
@@ -21,7 +28,8 @@ test_that("`get_n_*() detects number of calls properly", {
     c(
       'stop("foo!")',
       'warning("bar!")',
-      "data.frame(x = 1)"
+      "data.frame(x = 1)",
+      "base::data.frame(x = 1)"  # this is not counted
     ),
     "tmp.R"
   )
@@ -32,7 +40,15 @@ test_that("`get_n_*() detects number of calls properly", {
 })
 
 # Pattern is needed filter out files such as ggplot2.rdb, which is created when running covr::package_coverage()
-R_files <- list.files("../../R", pattern = ".*\\.(R|r)$", full.names = TRUE)
+R_paths <- c(
+  "../../R",                     # in the case of devtools::test()
+  "../../00_pkg_src/ggplot2/R"   # in the case of R CMD check
+)
+R_files <- list.files(R_paths, pattern = ".*\\.(R|r)$", full.names = TRUE)
+
+test_that("list up R files properly", {
+  expect_true(length(R_files) > 0)
+})
 
 test_that("do not use stop()", {
   stops <- vapply(R_files, get_n_stop, integer(1))
@@ -44,7 +60,7 @@ test_that("do not use warning()", {
   expect_equal(sum(warnings), 0)
 })
 
-test_that("do not use data.frame(), use `data_frame()` or `new_data_frame()`", {
+test_that("do not use data.frame(), use `data_frame()` or `new_data_frame()`, or add `base::` prefix", {
   data.frames <- vapply(R_files, get_n_data.frame, integer(1))
   expect_equal(sum(data.frames), 0)
 })
