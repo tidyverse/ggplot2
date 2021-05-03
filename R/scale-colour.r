d6de9b5d6b4c50ec6304eff4e789ff69578c0b2f
Claus Wilke <wilke@austin.utexas.edu>
2021-05-03 10:01:24 -0500

    add scale check

--- R/scale-colour.r
+++ R/scale-colour.r
@@ -78,7 +78,7 @@ scale_colour_continuous <- function(...,
   type <- type %||% "gradient"
 
   if (is.function(type)) {
-    type(...)
+    check_scale_type(type(...), "scale_colour_continuous", "colour")
   } else if (identical(type, "gradient")) {
     scale_colour_gradient(...)
   } else if (identical(type, "viridis")) {
@@ -95,7 +95,7 @@ scale_fill_continuous <- function(...,
   type <- type %||% "gradient"
 
   if (is.function(type)) {
-    type(...)
+    check_scale_type(type(...), "scale_fill_continuous", "fill")
   } else if (identical(type, "gradient")) {
     scale_fill_gradient(...)
   } else if (identical(type, "viridis")) {
@@ -110,7 +110,7 @@ scale_fill_continuous <- function(...,
 scale_colour_binned <- function(...,
                                 type = getOption("ggplot2.binned.colour")) {
   if (is.function(type)) {
-    type(...)
+    check_scale_type(type(...), "scale_colour_binned", "colour")
   } else {
     type_fallback <- getOption("ggplot2.continuous.colour", default = "gradient")
     # don't use fallback from scale_colour_continuous() if it is
@@ -136,7 +136,7 @@ scale_colour_binned <- function(...,
 scale_fill_binned <- function(...,
                               type = getOption("ggplot2.binned.fill")) {
   if (is.function(type)) {
-    type(...)
+    check_scale_type(type(...), "scale_fill_binned", "fill")
   } else {
     type_fallback <- getOption("ggplot2.continuous.fill", default = "gradient")
     # don't use fallback from scale_colour_continuous() if it is
@@ -155,4 +155,18 @@ scale_fill_binned <- function(...,
       abort("Unknown scale type")
     }
   }
+}
+
+
+# helper function to make sure that the provided scale is of the correct
+# type (i.e., is continuous and works with the provided aesthetic)
+check_scale_type <- function(scale, name, aesthetic) {
+  if (!isTRUE(aesthetic %in% scale$aesthetics)) {
+    abort(glue("The `type` argument of `{name}()` must return a continuous scale for the {aesthetic} aesthetic. The provided scale works with the following aesthetics: {glue_collapse(scale$aesthetics, sep = ', ')}"))
+  }
+  if (isTRUE(scale$is_discrete())) {
+    abort(glue("The `type` argument of `{name}()` must return a continuous scale for the {aesthetic} aesthetic, but the provided scale is discrete."))
+  }
+
+  scale
 }
