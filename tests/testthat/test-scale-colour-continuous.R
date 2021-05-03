84cf4fef1fcfe46028b08e44dfae4716a781a709
Claus Wilke <wilke@austin.utexas.edu>
2021-05-03 10:38:16 -0500

    unit tests for errors

---
+++ tests/testthat/test-scale-colour-continuous.R
@@ -0,0 +1,20 @@
+context("test-scale-colour-continuous.R")
+
+test_that("type argument is checked for proper input", {
+  expect_error(
+    scale_colour_continuous(type = function() "abc"),
+    "is not a scale function"
+  )
+  expect_error(
+    scale_fill_continuous(type = geom_point),
+    "is not a scale function"
+  )
+  expect_error(
+    scale_colour_binned(type = function(...) scale_colour_binned(aesthetics = c("fill", "point_colour"))),
+    "works with the following aesthetics: fill, point_colour"
+  )
+  expect_error(
+    scale_fill_binned(type = scale_fill_brewer),
+    "provided scale is discrete"
+  )
+})
