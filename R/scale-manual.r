fe5953e5b724b991c9172ae5bf61719de9d52eba
Teun van den Brand <49372158+teunbrand@users.noreply.github.com>
2021-09-15 22:25:20 +0200

    manual scale limits default to present names of values

--- R/scale-manual.r
+++ R/scale-manual.r
@@ -132,8 +132,9 @@ manual_scale <- function(aesthetic, values = NULL, breaks = waiver(), ..., limit
     force(values)
   }
 
-  if (is.null(limits)) {
-    limits <- names(values)
+  if (is.null(limits) && !is.null(names(values))) {
+    force(values)
+    limits <- function(x) {intersect(x, names(values))}
   }
 
   # order values according to breaks
