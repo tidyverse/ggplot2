f92da860b64793a195c3d2dbc605627b2f4776b0
Thomas Lin Pedersen <thomasp85@gmail.com>
2021-10-29 15:30:39 +0200

    update to the fact that maps no longer is available for R < 3.5

--- tests/testthat/test-coord-map.R
+++ tests/testthat/test-coord-map.R
@@ -1,9 +1,9 @@
 context("coord_map")
 
-us_map <- map_data("usa")
-p_us <- ggplot(us_map, aes(x = long, y = lat, group = group))
-
 test_that("USA state map drawn", {
+  skip_if(packageVersion("base") < "3.5.0")
+  us_map <- map_data("usa")
+  p_us <- ggplot(us_map, aes(x = long, y = lat, group = group))
   expect_doppelganger(
     "USA mercator",
     p_us +
@@ -13,6 +13,9 @@ test_that("USA state map drawn", {
 })
 
 test_that("coord_map scale position can be switched", {
+  skip_if(packageVersion("base") < "3.5.0")
+  us_map <- map_data("usa")
+  p_us <- ggplot(us_map, aes(x = long, y = lat, group = group))
   expect_doppelganger(
     "coord_map switched scale position",
     p_us +
