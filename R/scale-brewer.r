c856b7f2b69ac7cba421a6f82099d14785d57d5b
Hiroaki Yutani <yutani.ini@gmail.com>
2021-11-03 08:32:27 +0900

    set.seed() before sample() (#4502)

--- R/scale-brewer.r
+++ R/scale-brewer.r
@@ -40,6 +40,7 @@
 #' @rdname scale_brewer
 #' @export
 #' @examples
+#' set.seed(596)
 #' dsamp <- diamonds[sample(nrow(diamonds), 1000), ]
 #' (d <- ggplot(dsamp, aes(carat, price)) +
 #'   geom_point(aes(colour = clarity)))
