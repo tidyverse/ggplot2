631b1c1b502fe92cf5e839eda332e9499cef8e00
Benson Muite <bkmgit@users.noreply.github.com>
2021-11-27 11:41:27 +0300

    License information
    There is no original source for the data. OpenML indicates it is in the public domain,https://www.openml.org/d/42225

--- R/data.R
+++ R/data.R
@@ -1,7 +1,8 @@
 #' Prices of over 50,000 round cut diamonds
 #'
 #' A dataset containing the prices and other attributes of almost 54,000
-#'  diamonds. The variables are as follows:
+#' diamonds. The dataset is in the public domain. The variables are as 
+#' follows:
 #'
 #' @format A data frame with 53940 rows and 10 variables:
 #' \describe{
