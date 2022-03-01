5edc94170cb3f46457ded546395c65e19e831cfd
Thomas Lin Pedersen <thomasp85@gmail.com>
2022-03-01 13:26:24 +0100

    standardise package doc and imports

--- R/ggplot2-package.R
+++ R/ggplot2-package.R
@@ -1,7 +1,9 @@
 #' @keywords internal
 "_PACKAGE"
 
+## usethis namespace: start
 #' @import scales grid gtable rlang
 #' @importFrom stats setNames
 #' @importFrom glue glue glue_collapse
+## usethis namespace: end
 NULL
