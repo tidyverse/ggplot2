fae01e31d0e9ebd76d3c66b8710272bda1707f0a
Matthew Kay <matthew.kay@gmail.com>
2022-03-15 19:50:57 -0500

    condense orientation-aware key glyph visual tests

--- tests/testthat/test-draw-key.R
+++ tests/testthat/test-draw-key.R
@@ -20,61 +20,34 @@ test_that("alternative key glyphs work", {
 
 # Orientation-aware key glyphs --------------------------------------------
 
-test_that("orientation-aware key glyphs work", {
+test_that("horizontal key glyphs work", {
   df <- data.frame(
     middle = 1:2,
     lower = 0:1,
     upper = 2:3,
     min = -1:0,
     max = 3:4,
-    group = c("a","b")
+    group1 = c("a","b"),
+    group2 = c("c","d")
   )
 
-  p_horizontal <- ggplot(df, aes(
+  p <- ggplot(df, aes(
     x = middle,
-    y = group,
     xmiddle = middle,
     xlower = lower,
     xupper = upper,
     xmin = min,
-    xmax = max,
-    color = group
+    xmax = max
   ))
 
-  p_vertical <- ggplot(df, aes(
-    x = group,
-    y = middle,
-    middle = middle,
-    lower = lower,
-    upper = upper,
-    ymin = min,
-    ymax = max,
-    color = group
-  ))
-
-  expect_doppelganger("vertical boxplot",
-    p_vertical + geom_boxplot(stat = "identity")
+  expect_doppelganger("horizontal boxplot and crossbar",
+    p +
+      geom_boxplot(aes(y = group1, color = group1), stat = "identity") +
+      geom_crossbar(aes(y = group2, fill = group2))
   )
-  expect_doppelganger("vertical crossbar",
-    p_vertical + geom_crossbar()
-  )
-  expect_doppelganger("vertical pointrange",
-    p_vertical + geom_pointrange()
-  )
-  expect_doppelganger("vertical linerange",
-    p_vertical + geom_linerange()
-  )
-
-  expect_doppelganger("horizontal boxplot",
-    p_horizontal + geom_boxplot(stat = "identity")
-  )
-  expect_doppelganger("horizontal crossbar",
-    p_horizontal + geom_crossbar()
-  )
-  expect_doppelganger("horizontal pointrange",
-    p_horizontal + geom_pointrange()
-  )
-  expect_doppelganger("horizontal linerange",
-    p_horizontal + geom_linerange()
+  expect_doppelganger("horizontal linerange and pointrange",
+    p +
+      geom_linerange(aes(y = group1, color = group1)) +
+      geom_pointrange(aes(y = group2, shape = group2))
   )
 })
