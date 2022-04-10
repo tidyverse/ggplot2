9ba9a55c6be608721ba1f7fae4d132506d585450
Kirill Müller <krlmlr+r@mailbox.org>
2022-04-10 08:21:53 +0200

    find_scale() works with scales from packages that are loaded but not attached

--- R/scale-type.R
+++ R/scale-type.R
@@ -7,10 +7,23 @@ find_scale <- function(aes, x, env = parent.frame()) {
   }
 
   type <- scale_type(x)
+  extra_env <- attr(type, "env")
   candidates <- paste("scale", aes, type, sep = "_")
 
+  get_extra_env <- function() {
+    if (!is.null(extra_env)) {
+      return(extra_env)
+    }
+    # Call only once
+    extra_env <<- all_namespaces()
+    return(extra_env)
+  }
+
   for (scale in candidates) {
-    scale_f <- find_global(scale, env, mode = "function")
+    scale_f <- find_global(
+      scale, env, mode = "function",
+      get_extra_env()
+    )
     if (!is.null(scale_f))
       return(scale_f())
   }
@@ -22,17 +35,36 @@ find_scale <- function(aes, x, env = parent.frame()) {
   return(NULL)
 }
 
+all_namespaces <- function() {
+  lapply(loadedNamespaces(), asNamespace)
+}
+
 # Look for object first in parent environment and if not found, then in
 # ggplot2 namespace environment.  This makes it possible to override default
 # scales by setting them in the parent environment.
-find_global <- function(name, env, mode = "any") {
-  if (exists(name, envir = env, mode = mode)) {
-    return(get(name, envir = env, mode = mode))
+find_global <- function(name, env, mode = "any", extra_env_list = NULL) {
+  out <- get0(name, envir = env, mode = mode)
+  if (!is.null(out)) {
+    return(out)
+  }
+
+  if (!is.null(extra_env_list)) {
+    if (!is.list(extra_env_list)) {
+      extra_env_list <- list(extra_env_list)
+    }
+
+    for (extra_env in extra_env_list) {
+      out <- get0(name, envir = extra_env, mode = mode)
+      if (!is.null(out)) {
+        return(out)
+      }
+    }
   }
 
   nsenv <- asNamespace("ggplot2")
-  if (exists(name, envir = nsenv, mode = mode)) {
-    return(get(name, envir = nsenv, mode = mode))
+  out <- get0(name, envir = nsenv, mode = mode)
+  if (!is.null(out)) {
+    return(out)
   }
 
   NULL
@@ -44,18 +76,29 @@ find_global <- function(name, env, mode = "any") {
 #' ggplot2 to handle new types of data. If you simply want to pass the vector
 #' through as an additional aesthetic, return `"identity"`.
 #'
+#' Scale functions defined in other packages require special handling.
+#' For compatibility, since ggplot2 3.3.6, the default is to search for
+#' scale functions in all loaded namespaces. This behavior changes
+#' if the return value has an attribute named `"env"`.
+#'
 #' @param x A vector
 #' @return A character vector of scale types. These will be tried in turn
 #'   to find a default scale. For example, if `scale_type()` returns
 #'   `c("foo", "bar")` and the vector is used with the colour aesthetic,
 #'   ggplot2 will first look for `scale_colour_foo` then
 #'   `scale_colour_bar`.
+#'
+#'   If the returned character vector has an attribute named `"env"`,
+#'   it is expected to be an environment or a list of environments.
+#'   If it is provided, these environments are searched for scale functions,
+#'   instead of the default of all loaded environments.
 #' @export
 #' @keywords internal
 #' @examples
 #' scale_type(1:5)
 #' scale_type("test")
 #' scale_type(Sys.Date())
+#' scale_type(tibble::num(1:3))
 scale_type <- function(x) UseMethod("scale_type")
 
 #' @export
