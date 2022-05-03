30ef11b1e8436b01b2732b1b9ec0dba9642037ff
Hiroaki Yutani <yutani.ini@gmail.com>
2022-05-03 10:01:36 +0900

    Remove examples

--- R/aes.r
+++ R/aes.r
@@ -265,23 +265,6 @@ is_position_aes <- function(vars) {
 #' @keywords internal
 #'
 #' @export
-#' @examples
-#' # Three ways of generating the same aesthetics
-#' aes(mpg, wt, col = cyl)
-#' aes_(quote(mpg), quote(wt), col = quote(cyl))
-#' aes_(~mpg, ~wt, col = ~cyl)
-#' aes_string("mpg", "wt", col = "cyl")
-#'
-#' # You can't easily mimic these calls with aes_string
-#' aes(`$100`, colour = "smooth")
-#' aes_(~ `$100`, colour = "smooth")
-#' # Ok, you can, but it requires a _lot_ of quotes
-#' aes_string("`$100`", colour = '"smooth"')
-#'
-#' # Convert strings to names with as.name
-#' var <- "cyl"
-#' aes(col = x)
-#' aes_(col = as.name(var))
 aes_ <- function(x, y, ...) {
   lifecycle::deprecate_soft(
     "3.0.0",
