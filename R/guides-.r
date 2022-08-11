9c27d3e9162200edf75caecfc3141c01af8eaa0d
Teun van den Brand <49372158+teunbrand@users.noreply.github.com>
2022-08-11 21:02:33 +0200

    Better warning when guide function not found

--- R/guides-.r
+++ R/guides-.r
@@ -177,9 +177,13 @@ resolve_guide <- function(aesthetic, scale, guides, default = "none", null = "no
 validate_guide <- function(guide) {
   # if guide is specified by character, then find the corresponding guide
   if (is.character(guide)) {
-    find_global(paste0("guide_", guide), env = global_env(),
-                mode = "function")()
-  } else if (inherits(guide, "guide")) {
+    guide <- find_global(paste0("guide_", guide), env = global_env(),
+                         mode = "function")
+    if (is.function(guide)) {
+      return(guide())
+    }
+  }
+  if (inherits(guide, "guide")) {
     guide
   } else if (inherits(guide, "Guide")) {
     # ggproto guides need to be cloned because they may have been defined
