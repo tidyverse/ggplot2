62a826d9f6dc4c301fd3736888a79e44ba8bb879
Thomas Lin Pedersen <thomasp85@gmail.com>
2022-10-07 13:13:24 +0200

    provide correct versioning

--- R/quick-plot.r
+++ R/quick-plot.r
@@ -66,8 +66,8 @@ qplot <- function(x, y, ..., data, facets = NULL, margins = FALSE,
 
   caller_env <- parent.frame()
 
-  if (lifecycle::is_present(stat)) lifecycle::deprecate_stop("3.4.0", "qplot(stat)")
-  if (lifecycle::is_present(position)) lifecycle::deprecate_stop("3.4.0", "qplot(position)")
+  if (lifecycle::is_present(stat)) lifecycle::deprecate_stop("2.0.0", "qplot(stat)")
+  if (lifecycle::is_present(position)) lifecycle::deprecate_stop("2.0.0", "qplot(position)")
   if (!is.character(geom)) {
     cli::cli_abort("{.arg geom} must be a character vector")
   }
