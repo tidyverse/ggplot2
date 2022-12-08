3433f9aec6329587dab1ae4eaf0d8968dc9fd341
Teun van den Brand <49372158+teunbrand@users.noreply.github.com>
2022-12-08 22:05:12 +0100

    Add unit test

--- tests/testthat/test-geom-.R
+++ tests/testthat/test-geom-.R
@@ -5,3 +5,19 @@ test_that("aesthetic checking in geom throws correct errors", {
   aes <- list(a = 1:4, b = letters[1:4], c = TRUE, d = 1:2, e = 1:5)
   expect_snapshot_error(check_aesthetics(aes, 4))
 })
+
+test_that("geom defaults can be set and reset", {
+  l <- geom_point()
+  test <- l$geom$use_defaults(data_frame0())
+  expect_equal(test$colour, "black")
+
+  inv <- update_geom_defaults("point", list(colour = "red"))
+  test <- l$geom$use_defaults(data_frame0())
+  expect_equal(test$colour, "red")
+  expect_equal(inv$colour, "black")
+
+  inv <- update_geom_defaults("point", NULL)
+  test <- l$geom$use_defaults(data_frame0())
+  expect_equal(test$colour, "black")
+  expect_equal(inv$colour, "red")
+})
