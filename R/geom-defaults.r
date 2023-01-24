d526484866cf7ab8a36c89573edea30c34770e96
David Kahle <david@kahle.io>
2023-01-24 11:20:37 -0600

    allow better stat defaults updating with examples, add tests

--- R/geom-defaults.r
+++ R/geom-defaults.r
@@ -7,16 +7,42 @@
 #' @keywords internal
 #' @export
 #' @examples
+#'
+#' # updating a geom's default aesthetic settings
+#' # example: change geom_point()'s default color
 #' GeomPoint$default_aes
 #' update_geom_defaults("point", list(color = "red"))
 #' GeomPoint$default_aes
 #' ggplot(mtcars, aes(mpg, wt)) + geom_point()
+#'
+#' # reset default
 #' update_geom_defaults("point", list(colour = "black"))
+#'
+#' # note: adding default aesthetics is possible
+#' # update_geom_defaults("point", list(foo = "bar"))
+#' # GeomPoint$default_aes
+#'
+#'
+#' # updating a stat's default aesthetic settings
+#' # example: change stat_bin()'s default y-axis to the density scale
+#' StatBin$default_aes
+#' update_stat_defaults("bin", list(y = expr(after_stat(density))))
+#' StatBin$default_aes
+#' ggplot(data.frame(x = rnorm(1e3)), aes(x)) +
+#'   geom_histogram() +
+#'   geom_function(fun = dnorm, color = "red")
+#'
+#' # reset default
+#' update_stat_defaults("bin", list(y = expr(after_stat(density))))
+#'
 #' @rdname update_defaults
 update_geom_defaults <- function(geom, new) {
   g <- check_subclass(geom, "Geom", env = parent.frame())
   old <- g$default_aes
-  g$default_aes[] <- defaults(rename_aes(new), old)[names(old)]
+  new <- rename_aes(new)
+  new_names_order <- unique(c(names(old), names(new)))
+  new <- defaults(new, old)[new_names_order]
+  g$default_aes[names(new)] <- new
   invisible()
 }
 
@@ -25,6 +51,9 @@ update_geom_defaults <- function(geom, new) {
 update_stat_defaults <- function(stat, new) {
   g <- check_subclass(stat, "Stat", env = parent.frame())
   old <- g$default_aes
-  g$default_aes[] <- defaults(rename_aes(new), old)[names(old)]
+  new <- rename_aes(new)
+  new_names_order <- unique(c(names(old), names(new)))
+  new <- defaults(new, old)[new_names_order]
+  g$default_aes[names(new)] <- new
   invisible()
 }
