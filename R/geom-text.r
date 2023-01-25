15cb2fbc1de1c906fe5264d3cef85c2a9338f375
Prof. Job N Nmadu <job.nmadu@gmail.com>
2023-01-25 20:33:17 +0100

    Placement of text elements on the plot (#5007)
    The placement of the text in the plot is slightly adjusted.

--- R/geom-text.r
+++ R/geom-text.r
@@ -145,7 +145,7 @@
 #' df <- data.frame(
 #'   x = c(1, 1, 2, 2, 1.5),
 #'   y = c(1, 2, 1, 2, 1.5),
-#'   text = c("bottom-left", "bottom-right", "top-left", "top-right", "center")
+#'   text = c("bottom-left", "top-left", "bottom-right", "top-right", "center")
 #' )
 #' ggplot(df, aes(x, y)) +
 #'   geom_text(aes(label = text))
