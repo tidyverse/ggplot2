67e6348505155ab80e6b98d562825834b1683cf8
Teun van den Brand <49372158+teunbrand@users.noreply.github.com>
2023-02-20 18:24:00 +0100

    Deprecate `facets`

--- R/facet-grid-.r
+++ R/facet-grid-.r
@@ -112,11 +112,13 @@ facet_grid <- function(rows = NULL, cols = NULL, scales = "fixed",
                        space = "fixed", shrink = TRUE,
                        labeller = "label_value", as.table = TRUE,
                        switch = NULL, drop = TRUE, margins = FALSE,
-                       facets = NULL) {
-  # `facets` is soft-deprecated and renamed to `rows`
-  if (!is.null(facets)) {
-    rows <- facets
+                       facets = deprecated()) {
+  # `facets` is deprecated and renamed to `rows`
+  if (lifecycle::is_present(facets)) {
+    deprecate_warn0("2.2.0", "facet_grid(facets)", "facet_grid(rows)")
+    row <- facets
   }
+
   # Should become a warning in a future release
   if (is.logical(cols)) {
     margins <- cols
