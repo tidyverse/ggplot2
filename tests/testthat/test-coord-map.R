162dd07dc3bfadfe2b9000c697223037cfaf51f2
Michael Chirico <chiricom@google.com>
2023-02-21 15:37:11 -0800

    Update test-coord-map.R

--- tests/testthat/test-coord-map.R
+++ tests/testthat/test-coord-map.R
@@ -1,4 +1,5 @@
 test_that("USA state map drawn", {
+  skip_if_not_installed("maps") # required for map_data()
   skip_if(packageVersion("base") < "3.5.0")
   us_map <- map_data("usa")
   p_us <- ggplot(us_map, aes(x = long, y = lat, group = group))
@@ -11,6 +12,7 @@ test_that("USA state map drawn", {
 })
 
 test_that("coord_map scale position can be switched", {
+  skip_if_not_installed("maps") # required for map_data()
   skip_if(packageVersion("base") < "3.5.0")
   us_map <- map_data("usa")
   p_us <- ggplot(us_map, aes(x = long, y = lat, group = group))
@@ -25,6 +27,7 @@ test_that("coord_map scale position can be switched", {
 })
 
 test_that("Inf is squished to range", {
+  skip_if_not_installed("maps") # required for mproject()
   skip_if(packageVersion("base") < "3.5.0")
   d <- cdata(
     ggplot(data_frame(x = 0, y = 0)) +
