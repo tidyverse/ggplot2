ca1c09b1b71151fc68f0c87ae7870cd2850bb11c
Thomas Lin Pedersen <thomasp85@gmail.com>
2023-09-06 09:38:38 +0200

    Allow unnamed `start` mapping

--- R/aes-evaluation.R
+++ R/aes-evaluation.R
@@ -313,7 +313,7 @@ strip_stage <- function(expr) {
   } else if (is_call(uq_expr, "stage")) {
     # Prefer stat mapping if present, otherwise original mapping (fallback to
     # scale mapping) but there should always be two arguments to stage()
-    uq_expr$after_stat %||% uq_expr$start %||% (if (is.null(uq_expr$after_scale)) uq_expr[[3]]) %||% uq_expr[[2]]
+    uq_expr$after_stat %||% uq_expr$start %||% uq_expr[[1]] %||% (if (is.null(uq_expr$after_scale)) uq_expr[[3]]) %||% uq_expr[[2]]
   } else {
     expr
   }
