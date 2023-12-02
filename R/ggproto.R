71ca0cd17898f5d3448f0a34cea607d9ee692d2c
Olivier Roy <olivierroy71@hotmail.com>
2023-12-01 21:35:46 -0500

    error msg

--- R/ggproto.R
+++ R/ggproto.R
@@ -118,7 +118,7 @@ fetch_ggproto <- function(x, name) {
       res <- fetch_ggproto(super(), name)
     } else {
       cli::cli_abort(c(
-        "{.cls {x}} was built with an incompatible version of ggproto.",
+        "{.obj_type_friendly {x}} was built with an incompatible version of ggproto.",
         "i" = "Please reinstall the package that provides this extension."
       ))
     }
