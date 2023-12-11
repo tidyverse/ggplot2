b16a1bb69ea44e410ff35f54e95e6ec598334342
Teun van den Brand <tahvdbrand@gmail.com>
2023-12-11 16:44:18 +0100

    rename argument

--- R/utilities.R
+++ R/utilities.R
@@ -775,21 +775,21 @@ vec_rbind0 <- function(..., .error_call = current_env(), .call = caller_env()) {
 
 # This function is used to vectorise the following pattern:
 #
-# list$name1 <- list$name1 %||% value
-# list$name2 <- list$name2 %||% value
+# obj$name1 <- obj$name1 %||% value
+# obj$name2 <- obj$name2 %||% value
 #
 # and express this pattern as:
 #
-# replace_null(list, name1 = value, name2 = value)
-replace_null <- function(list, ..., env = caller_env()) {
+# replace_null(obj, name1 = value, name2 = value)
+replace_null <- function(obj, ..., env = caller_env()) {
   # Collect dots without evaluating
   dots <- enexprs(...)
-  # Select arguments that are null in `list`
+  # Select arguments that are null in `obj`
   nms  <- names(dots)
-  nms  <- nms[vapply(list[nms], is.null, logical(1))]
+  nms  <- nms[vapply(obj[nms], is.null, logical(1))]
   # Replace those with the evaluated dots
-  list[nms] <- inject(list(!!!dots[nms]), env = env)
-  list
+  obj[nms] <- inject(list(!!!dots[nms]), env = env)
+  obj
 }
 
 attach_plot_env <- function(env) {
