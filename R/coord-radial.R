313bd89f3cc47e95d258e611c9074825c533c3b3
Teun van den Brand <tahvdbrand@gmail.com>
2023-12-12 10:49:19 +0100

    Funnel radial r-axis through CoordCartesian

--- R/coord-radial.R
+++ R/coord-radial.R
@@ -241,20 +241,14 @@ CoordRadial <- ggproto("CoordRadial", Coord,
     if (self$r_axis_inside) {
       return(list(left = zeroGrob(), right = zeroGrob()))
     }
-    list(
-      left  = panel_guides_grob(panel_params$guides, position = "left",  theme = theme),
-      right = panel_guides_grob(panel_params$guides, position = "right", theme = theme)
-    )
+    CoordCartesian$render_axis_v(panel_params, theme)
   },
 
   render_axis_h = function(self, panel_params, theme) {
     if (self$r_axis_inside) {
       return(list(top = zeroGrob(), bottom = zeroGrob()))
     }
-    list(
-      top    = panel_guides_grob(panel_params$guides, position = "top",    theme = theme),
-      bottom = panel_guides_grob(panel_params$guides, position = "bottom", theme = theme)
-    )
+    CoordCartesian$render_axis_h(panel_params, theme)
   },
 
   render_bg = function(self, panel_params, theme) {
