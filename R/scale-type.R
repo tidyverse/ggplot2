40cfe30fc66ef85be9f8dcccfa94a2ae609b7527
Teun van den Brand <tahvdbrand@gmail.com>
2023-12-18 14:42:55 +0100

    skip AsIs scale type

--- R/scale-type.R
+++ R/scale-type.R
@@ -2,7 +2,7 @@ find_scale <- function(aes, x, env = parent.frame()) {
   # Inf is ambiguous; it can be used either with continuous scales or with
   # discrete scales, so just skip in the hope that we will have a better guess
   # with the other layers
-  if (is.null(x) || (is_atomic(x) && all(is.infinite(x)))) {
+  if (is.null(x) || (is_atomic(x) && all(is.infinite(x))) || inherits(x, "AsIs")) {
     return(NULL)
   }
 
