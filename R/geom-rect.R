71923cc412047016d88aeacf2478dad9645fbfca
Teun van den Brand <tahvdbrand@gmail.com>
2024-04-24 13:43:19 +0200

    fill missing aes

--- R/geom-rect.R
+++ R/geom-rect.R
@@ -34,6 +34,20 @@ GeomRect <- ggproto("GeomRect", Geom,
   optional_aes = c("x", "width", "xmin", "xmax", "y", "height", "ymin", "ymax"),
   non_missing_aes = c("xmin", "xmax", "ymin", "ymax"),
 
+  setup_data = function(self, data, params) {
+    if (is.null(data$xmin) || is.null(data$xmax)) {
+      x <- resolve_rect(data$xmin, data$xmax, data$x, data$width %||% params$width)
+      i <- lengths(x) > 1
+      data[c("xmin", "xmax")[i]] <- x[i]
+    }
+    if (is.null(data$ymin) || is.null(data$ymax)) {
+      y <- resolve_rect(data$ymin, data$ymax, data$y, data$height %||% params$height)
+      i <- lengths(y) > 1
+      data[c("ymin", "ymax")[i]] <- y[i]
+    }
+    data
+  },
+
   draw_panel = function(self, data, panel_params, coord, lineend = "butt", linejoin = "mitre") {
     data <- check_linewidth(data, snake_class(self))
     if (!coord$is_linear()) {
@@ -74,3 +88,25 @@ GeomRect <- ggproto("GeomRect", Geom,
 
   rename_size = TRUE
 )
+
+resolve_rect <- function(min = NULL, max = NULL, center = NULL, length = NULL) {
+  if (is.null(min) && is.null(max)) {
+    min <- center - 0.5 * length
+    max <- center + 0.5 * length
+    return(list(min = min, max = max))
+  }
+  if (is.null(min)) {
+    if (is.null(center)) {
+      min <- max - length
+    } else {
+      min <- max - 2 * (max - center)
+    }
+  }
+  if (is.null(max)) {
+    if (is.null(center)) {
+      max <- min + length
+    } else {
+      max <- min + 2 * (center - min)
+    }
+  }
+  list(min = min, max = max)
