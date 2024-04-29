6c2e102065fca4f600768d48eae736a8f6fd23b5
Teun van den Brand <49372158+teunbrand@users.noreply.github.com>
2024-04-29 10:34:23 +0200

    Interpreter/wrapper for `gpar()` (#5867)
    * write wrapper
    
    * use wrapper
    
    * add news bullet
    
    * remove defunct `len0_null()`

--- R/utilities-grid.R
+++ R/utilities-grid.R
@@ -13,6 +13,38 @@ ggname <- function(prefix, grob) {
   grob
 }
 
+#' Interpreter for graphical parameters
+#'
+#' This is a wrapper for [`grid::gpar()`] that applies ggplot2's interpretation
+#' of graphical parameters.
+#'
+#' @param ... Named arguments passed on to `gpar()`.
+#' @param stroke Linewidth for points. Populates the `lwd` grid parameter.
+#' @param pointsize Size for points. Populates the `fontsize` grid parameter.
+#'
+#' @return An object of class 'gpar'.
+#' @keywords internal
+#' @export
+ggpar <- function(..., stroke = NULL, pointsize = NULL) {
+  args <- list2(...)
+  args <- args[lengths(args) > 0]
+
+  if (!is.null(args$lwd)) {
+    args$lwd <- args$lwd * .pt
+  }
+  if (!is.null(stroke)) {
+    args$lwd <- stroke * .stroke / 2
+  }
+  if (!is.null(pointsize)) {
+    # Stroke is added around the outside of the point
+    stroke <- stroke %||% 0
+    stroke[is.na(stroke)] <- 0
+    args$fontsize <- pointsize * .pt + stroke * .stroke / 2
+  }
+
+  inject(gpar(!!!args))
+}
+
 width_cm <- function(x) {
   if (is.grob(x)) {
     convertWidth(grobWidth(x), "cm", TRUE)
