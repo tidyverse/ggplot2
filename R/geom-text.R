e2966ac96efccd18ac2f3bc1ece1deb534895065
Teun van den Brand <tahvdbrand@gmail.com>
2024-05-23 14:14:43 +0200

    rename to `gg_par()`

--- R/geom-text.R
+++ R/geom-text.R
@@ -239,7 +239,7 @@ GeomText <- ggproto("GeomText", Geom,
       data$x, data$y, default.units = "native",
       hjust = data$hjust, vjust = data$vjust,
       rot = data$angle,
-      gp = gparam(
+      gp = gg_par(
         col = alpha(data$colour, data$alpha),
         fontsize = data$size * size.unit,
         fontfamily = data$family,
