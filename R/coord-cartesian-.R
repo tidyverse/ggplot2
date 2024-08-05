b853c8cf65f79c72c8807e7040d83f0dd5d241f9
Teun van den Brand <tahvdbrand@gmail.com>
2024-08-05 11:52:46 +0200

    Redocument parameter

--- R/coord-cartesian-.R
+++ R/coord-cartesian-.R
@@ -9,6 +9,10 @@
 #' @param expand If `TRUE`, the default, adds a small expansion factor to
 #'   the limits to ensure that data and axes don't overlap. If `FALSE`,
 #'   limits are taken exactly from the data or `xlim`/`ylim`.
+#'   Giving a logical vector will separately control the expansion for the four
+#'   directions (top, left, bottom and right). The `expand` argument will be
+#'   recycled to length 4 if necessary. Alternatively, can be a named logical
+#'   vector to control a single direction, e.g. `expand = c(bottom = FALSE)`.
 #' @param default Is this the default coordinate system? If `FALSE` (the default),
 #'   then replacing this coordinate system with another one creates a message alerting
 #'   the user that the coordinate system is being replaced. If `TRUE`, that warning
