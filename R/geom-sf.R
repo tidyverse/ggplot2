3c8c4951fd22a99e10f0dee82db520ac0c94613f
Teun van den Brand <tahvdbrand@gmail.com>
2024-11-19 09:38:32 +0100

    defend against potentially missing `stroke` default

--- R/geom-sf.R
+++ R/geom-sf.R
@@ -231,7 +231,7 @@ GeomSf <- ggproto("GeomSf", Geom,
     point_size[!(is_point | is_collection)] <-
       data$linewidth[!(is_point | is_collection)]
 
-    stroke <- data$stroke * .stroke / 2
+    stroke <- (data$stroke %||% rep(0.5, nrow(data))) * .stroke / 2
     font_size <- point_size * .pt + stroke
 
     linewidth <- data$linewidth * .pt
