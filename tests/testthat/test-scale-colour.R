532d501b2778006898d312f18fa38acc88748c91
Teun van den Brand <tahvdbrand@gmail.com>
2024-12-04 16:28:34 +0100

    add tests

--- tests/testthat/test-scale-colour.R
+++ tests/testthat/test-scale-colour.R
@@ -18,3 +18,34 @@ test_that("type argument is checked for proper input", {
     scale_colour_continuous(type = "abc")
   )
 })
+
+test_that("palette arguments can take alternative input", {
+
+  cols <- c("red", "gold", "green", "cyan", "blue", "magenta")
+  hex  <- alpha(cols, 1)
+
+  sc <- scale_colour_continuous(palette = cols)
+  test <- sc$palette(seq(0, 1, length.out = length(cols)))
+  expect_equal(alpha(test, 1), hex)
+
+  sc <- scale_fill_continuous(palette = cols)
+  test <- sc$palette(seq(0, 1, length.out = length(cols)))
+  expect_equal(alpha(test, 1), hex)
+
+  sc <- scale_colour_binned(palette = cols)
+  test <- sc$palette(seq_along(cols))
+  expect_equal(alpha(test, 1), hex)
+
+  sc <- scale_fill_binned(palette = cols)
+  test <- sc$palette(seq_along(cols))
+  expect_equal(alpha(test, 1), hex)
+
+  sc <- scale_colour_discrete(palette = cols)
+  test <- sc$palette(length(cols))
+  expect_equal(alpha(test, 1), hex)
+
+  sc <- scale_fill_discrete(palette = cols)
+  test <- sc$palette(length(cols))
+  expect_equal(alpha(test, 1), hex)
+
+})
