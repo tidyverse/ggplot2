5645702cb16b57b95212c68762b2c6479ce14ef3
Teun van den Brand <tahvdbrand@gmail.com>
2025-01-21 15:08:21 +0100

    add test

--- tests/testthat/test-scales.R
+++ tests/testthat/test-scales.R
@@ -782,3 +782,30 @@ test_that("discrete scales work with NAs in arbitrary positions", {
   expect_equal(test, output)
 
 })
+
+test_that("ViewScales can be frozen", {
+
+  p1 <- ggplot(mpg, aes(drv, displ)) +
+    geom_boxplot() +
+    annotate("point", x = 5, y = 10) +
+    scale_x_discrete(labels = c("four-wheel", "forward", "reverse"))
+
+  b1 <- ggplot_build(p1)$layout$panel_params[[1]]
+
+  # We build a second plot with the first plot's scales
+  p2 <- ggplot(mpg, aes(drv, cyl)) +
+    geom_violin() +
+    annotate("point", x = 15, y = 100) +
+    b1$x$freeze() +
+    b1$y$freeze()
+  b2 <- ggplot_build(p2)
+
+  # Breaks and labels should respect p1's limits
+  x <- get_guide_data(b2, "x")
+  expect_equal(x$x, 0.6:2.6 / diff(b1$x.range))
+  expect_equal(x$.label, c("four-wheel", "forward", "reverse"))
+
+  y <- get_guide_data(b2, "y")
+  expect_equal(y$y, rescale(seq(2.5, 10, by = 2.5), from = b1$y.range))
+
+})
