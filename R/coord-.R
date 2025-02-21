f552634aca82407a6edc26c6fa51745f8ed59ac7
Teun van den Brand <tahvdbrand@gmail.com>
2025-02-21 13:47:07 +0100

    put `get_layer_key()` before `transform()`

--- R/coord-.R
+++ R/coord-.R
@@ -163,8 +163,8 @@ Coord <- ggproto("Coord",
     guide_params[!empty] <- Map(
       function(guide, guide_param, scale) {
         guide_param <- guide$train(guide_param, scale)
+        guide_param <- guide$get_layer_key(guide_param, layers, data)
         guide_param <- guide$transform(guide_param, self, panel_params)
-        guide_param <- guide$get_layer_key(guide_param, layers)
         guide_param
       },
       guide = guides[!empty],
