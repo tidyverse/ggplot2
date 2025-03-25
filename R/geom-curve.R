453cf6b5389b454f28e36ef1ce5eb91bd06efc75
Teun van den Brand <tahvdbrand@gmail.com>
2025-03-25 09:36:09 +0100

    avoid arbitrary whitespace changes

--- R/geom-curve.R
+++ R/geom-curve.R
@@ -3,45 +3,46 @@
 #' @format NULL
 #' @usage NULL
 #' @export
-GeomCurve <- ggproto("GeomCurve", GeomSegment,
+GeomCurve <- ggproto(
+  "GeomCurve", GeomSegment,
 
-                     default_aes = aes(
-                       colour = from_theme(ink),
-                       linewidth = from_theme(linewidth),
-                       linetype = from_theme(linetype),
-                       alpha = NA
-                     ),
+  default_aes = aes(
+    colour = from_theme(ink),
+    linewidth = from_theme(linewidth),
+    linetype = from_theme(linetype),
+    alpha = NA
+  ),
 
-                     draw_panel = function(data, panel_params, coord, curvature = 0.5, angle = 90,
-                                           ncp = 5, arrow = NULL, arrow.fill = NULL, lineend = "butt", na.rm = FALSE) {
+  draw_panel = function(data, panel_params, coord, curvature = 0.5, angle = 90,
+                        ncp = 5, arrow = NULL, arrow.fill = NULL, lineend = "butt", na.rm = FALSE) {
 
-                       if (!coord$is_linear()) {
-                         cli::cli_warn("{.fn geom_curve} is not implemented for non-linear coordinates")
-                       }
-                       data <- remove_missing(
-                         data, na.rm = na.rm,
-                         c("x", "y", "xend", "yend", "linetype", "linewidth"),
-                         name = "geom_curve"
-                       )
+    if (!coord$is_linear()) {
+      cli::cli_warn("{.fn geom_curve} is not implemented for non-linear coordinates")
+    }
+    data <- remove_missing(
+      data, na.rm = na.rm,
+      c("x", "y", "xend", "yend", "linetype", "linewidth"),
+      name = "geom_curve"
+    )
 
-                       trans <- coord$transform(data, panel_params)
+    trans <- coord$transform(data, panel_params)
 
-                       arrow.fill <- arrow.fill %||% trans$colour
+    arrow.fill <- arrow.fill %||% trans$colour
 
-                       curveGrob(
-                         trans$x, trans$y, trans$xend, trans$yend,
-                         default.units = "native",
-                         curvature = curvature, angle = angle, ncp = ncp,
-                         square = FALSE, squareShape = 1, inflect = FALSE, open = TRUE,
-                         gp = gg_par(
-                           col = alpha(trans$colour, trans$alpha),
-                           fill = alpha(arrow.fill, trans$alpha),
-                           lwd = trans$linewidth,
-                           lty = trans$linetype,
-                           lineend = lineend),
-                         arrow = arrow
-                       )
-                     }
+    curveGrob(
+      trans$x, trans$y, trans$xend, trans$yend,
+      default.units = "native",
+      curvature = curvature, angle = angle, ncp = ncp,
+      square = FALSE, squareShape = 1, inflect = FALSE, open = TRUE,
+      gp = gg_par(
+        col = alpha(trans$colour, trans$alpha),
+        fill = alpha(arrow.fill, trans$alpha),
+        lwd = trans$linewidth,
+        lty = trans$linetype,
+        lineend = lineend),
+      arrow = arrow
+    )
+  }
 )
 
 #' @inheritParams grid::curveGrob
