1f49ade0e8ac5455dc942b4874fa1713f72c628f
Teun van den Brand <tahvdbrand@gmail.com>
2025-04-07 12:32:12 +0200

    remove unrelated example

--- R/annotation-borders.R
+++ R/annotation-borders.R
@@ -15,19 +15,6 @@
 #' @export
 #' @examples
 #' if (require("maps")) {
-#'
-#' ia <- map_data("county", "iowa")
-#' mid_range <- function(x) mean(range(x))
-#' seats <- do.call(rbind, lapply(split(ia, ia$subregion), function(d) {
-#'   data.frame(lat = mid_range(d$lat), long = mid_range(d$long), subregion = unique(d$subregion))
-#' }))
-#'
-#' ggplot(ia, aes(long, lat)) +
-#'   geom_polygon(aes(group = group), fill = NA, colour = "grey60") +
-#'   geom_text(aes(label = subregion), data = seats, size = 2, angle = 45)
-#' }
-#'
-#' if (require("maps")) {
 #' data(us.cities)
 #' capitals <- subset(us.cities, capital == 2)
 #' ggplot(capitals, aes(long, lat)) +
