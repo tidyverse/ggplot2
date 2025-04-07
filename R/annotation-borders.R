fca20612ae572941e25984de66da446beeef2268
Teun van den Brand <tahvdbrand@gmail.com>
2025-04-07 12:10:43 +0200

    rename `borders()` -> `annotation_borders()`

--- R/annotation-borders.R
+++ R/annotation-borders.R
@@ -45,8 +45,8 @@
 #'   scale_size_area() +
 #'   coord_quickmap()
 #' }
-borders <- function(database = "world", regions = ".", fill = NA,
-                    colour = "grey50", xlim = NULL, ylim = NULL, ...) {
+annotation_borders <- function(database = "world", regions = ".", fill = NA,
+                               colour = "grey50", xlim = NULL, ylim = NULL, ...) {
   df <- map_data(database, regions, xlim = xlim, ylim = ylim)
   geom_polygon(aes(.data$long, .data$lat, group = .data$group), data = df,
                fill = fill, colour = colour, ..., inherit.aes = FALSE)
