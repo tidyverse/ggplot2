08d8932814bb167ec8c5268f8360d2aa5c559a7b
Yunuuuu <yunyunpp96@outlook.com>
2025-04-15 01:16:14 +0800

    simplify the test

--- tests/testthat/test-guide-axis.R
+++ tests/testthat/test-guide-axis.R
@@ -385,9 +385,8 @@ test_that("guide_axis_theta sets relative angle", {
 test_that("guide_axis_theta with only one axis key", {
   p <- ggplot(data.frame(x = "a", y = 1:3), aes(x, y)) +
     geom_point() +
-    coord_radial(start = 0, end = pi / 2, inner.radius = 0.5) +
-    guides(theta = guide_axis_theta(angle = 0))
-  expect_doppelganger("guide_axis_theta with only one axis key", p)
+    coord_radial()
+  expect_identical(get_guide_data(p, "theta")$.label, "a")
 })
 
 test_that("guide_axis_theta can be used in cartesian coordinates", {
