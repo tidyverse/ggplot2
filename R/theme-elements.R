a7756357e9d46ff6627d90a5157a48e1d0a1cce1
Teun van den Brand <tahvdbrand@gmail.com>
2025-04-16 12:58:50 +0200

    use classic extractors and subassignment

--- R/theme-elements.R
+++ R/theme-elements.R
@@ -330,6 +330,45 @@ rel <- function(x) {
 }
 
 #' @export
+`$.ggplot2::element` <- function(x, i) {
+  # deprecate_soft0("4.0.0", I("`<ggplot2::element>$i`"), I("`<ggplot2::element>@i`"))
+  `[[`(S7::props(x), i)
+}
+
+#' @export
+`[.ggplot2::element` <- function(x, i) {
+  # deprecate_soft0("4.0.0", I("`<gglot2::element>[i]`"), I("`S7::props(<ggplot2::element>, i)`"))
+  `[`(S7::props(x), i)
+}
+
+#' @export
+`[[.ggplot2::element` <- function(x, i) {
+  # deprecate_soft0("4.0.0", I("`<ggplot2::element>[[i]]`"), I("`S7::prop(<ggplot2::element>, i)`"))
+  `[[`(S7::props(x), i)
+}
+
+#' @export
+`$<-.ggplot2::element` <- function(x, i, value) {
+  # deprecate_soft0("4.0.0", I("`<ggplot2::element>$i <- value`"), I("`<ggplot2::element>@i <- value`"))
+  S7::props(x) <- `[[<-`(S7::props(x), i, value)
+  x
+}
+
+#' @export
+`[<-.ggplot2::element` <- function(x, i, value) {
+  # deprecate_soft0("4.0.0", I("`<ggplot2::element>[i] <- value`"), I("`S7::props(<ggplot2::element>)[i] <- value`"))
+  S7::props(x) <- `[<-`(S7::props(x), i, value)
+  x
+}
+
+#' @export
+`[[<-.ggplot2::element` <- function(x, i, value) {
+  # deprecate_soft0("4.0.0", I("`<ggplot2::element>[[i]] <- value`"), I("S7::prop(<ggplot2::element>, i) <- value"))
+  S7::props(x) <- `[[<-`(S7::props(x), i, value)
+  x
+}
+
+#' @export
 print.rel <- function(x, ...) print(noquote(paste(x, " *", sep = "")))
 
 #' Reports whether x is a rel object
