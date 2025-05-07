bd5418fe5470874108cb4be570e4424d3e8494ae
Teun van den Brand <tahvdbrand@gmail.com>
2025-05-07 10:51:45 +0200

    simplify `coord_fixed()`

--- R/coord-fixed.R
+++ R/coord-fixed.R
@@ -22,17 +22,8 @@
 #' p + coord_fixed(xlim = c(15, 30))
 #'
 #' # Resize the plot to see that the specified aspect ratio is maintained
-coord_fixed <- function(ratio = 1, xlim = NULL, ylim = NULL, expand = TRUE,
-                        clip = "on", reverse = "none") {
-  check_coord_limits(xlim)
-  check_coord_limits(ylim)
-  ggproto(NULL, CoordFixed,
-    limits = list(x = xlim, y = ylim),
-    ratio = ratio,
-    expand = expand,
-    reverse = reverse,
-    clip = clip
-  )
+coord_fixed <- function(ratio = 1, ...) {
+  coord_cartesian(ratio = ratio, ...)
 }
 
 #' @export
