ec193c0442333dee05bfcc1364a926b9e0d36179
Teun van den Brand <tahvdbrand@gmail.com>
2025-06-18 14:08:24 +0200

    stop complaints about 0-length labels, which is commonly used in label supression

--- R/labels.R
+++ R/labels.R
@@ -78,7 +78,8 @@ setup_plot_labels <- function(plot, layers, data) {
   # `geom_function()`. We can display these labels anyway, so we include them.
   plot_labels  <- plot@labels
   known_labels <- c(names(labels), fn_fmls_names(labs), "x", "y")
-  extra_labels <- setdiff(names(plot_labels), known_labels)
+  extra_labels <- names(plot_labels)[lengths(plot_labels) > 0]
+  extra_labels <- setdiff(extra_labels, known_labels)
 
   if (length(extra_labels) > 0) {
 
