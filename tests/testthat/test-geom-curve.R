11d9ecbab71165e3546bb001b3c8a5a4f268e75c
Federico Marotta <fmarotta@protonmail.com>
2025-06-22 11:31:09 +0200

    Apply suggestion to improve geom_curve() test

--- tests/testthat/test-geom-curve.R
+++ tests/testthat/test-geom-curve.R
@@ -14,22 +14,37 @@ test_that("geom_curve shape works", {
 
   df <- data.frame(x = c(1, 3), xend = c(2, 4), y = c(0, 1), yend = c(2, 1.5))
 
-  p_0 <- ggplot(df, aes(x, y, xend = xend, yend = yend)) +
-    geom_curve(arrow = arrow(), shape = 0, ncp = 1, curvature = 1)
+  p <- ggplot(df) +
+    geom_curve(
+      aes(x, y, xend = xend, yend = yend, color = "square"),
+      curvature = 1,
+      shape = 0,
+      ncp = 1
+    ) +
+    geom_curve(
+      # This layer will use `square = FALSE` in curveGrob because angle != 90
+      aes(x, y, xend = xend, yend = yend, color = "square tilted"),
+      angle = 60,
+      curvature = 1,
+      shape = 0,
+      ncp = 1
+    ) +
+    geom_curve(
+      aes(x, y, xend = xend, yend = yend, color = "spline cubic"),
+      curvature = -.5,
+      angle = 40,
+      shape = 1,
+      ncp = 1
+    ) +
+    geom_curve(
+      aes(x, y, xend = xend, yend = yend, color = "spline interpolating"),
+      curvature = -.5,
+      angle = 40,
+      shape = -1,
+      ncp = 1
+    ) +
+    NULL
 
-  # This will use `square = FALSE` in curveGrob because angle != 90
-  p_0_not_square <- ggplot(df, aes(x, y, xend = xend, yend = yend)) +
-    geom_curve(arrow = arrow(), shape = 0, ncp = 1, curvature = 1, angle = 60)
-
-  p_1 <- ggplot(df, aes(x, y, xend = xend, yend = yend)) +
-    geom_curve(arrow = arrow(), shape = 1)
-
-  p_m1 <- ggplot(df, aes(x, y, xend = xend, yend = yend)) +
-    geom_curve(arrow = arrow(), shape = -1, angle = 40)
-
-  expect_doppelganger("shape=0 geom_curve", p_0)
-  expect_doppelganger("shape=0 geom_curve", p_0_not_square)
-  expect_doppelganger("shape=1 geom_curve", p_1)
-  expect_doppelganger("shape=-1 geom_curve", p_m1)
+  expect_doppelganger("multishape geom_curve", p)
 
 })
