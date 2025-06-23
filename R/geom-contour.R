3655a59cdf68668a7e62d65320a32824e49b6b70
Teun van den Brand <tahvdbrand@gmail.com>
2025-06-23 11:12:03 +0200

    restore accent colour for `geom_contour()`

--- R/geom-contour.R
+++ R/geom-contour.R
@@ -5,7 +5,13 @@
 #' @include geom-path.R
 GeomContour <- ggproto(
   "GeomContour", GeomPath,
-  default_aes = aes(weight = 1, !!!GeomPath$default_aes)
+  default_aes = aes(
+    weight = 1,
+    colour = from_theme(accent),
+    linewidth = from_theme(linewidth),
+    linetype = from_theme(linetype),
+    alpha = NA
+  )
 )
 
 #' @rdname Geom
