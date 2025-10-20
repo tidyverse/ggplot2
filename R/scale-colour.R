bb64eddfcfcb1ac3b8f1ece918e7e669fb63de04
Teun van den Brand <49372158+teunbrand@users.noreply.github.com>
2025-10-20 11:45:10 +0200

    dedocument `type` options (#6658)

--- R/scale-colour.R
+++ R/scale-colour.R
@@ -28,10 +28,9 @@
 #'     between 0 and 1 returns the corresponding output values.
 #' @inheritDotParams continuous_scale -scale_name -trans -minor_breaks -expand
 #' @inheritDotParams binned_scale -scale_name -trans -expand
-#' @param type `r lifecycle::badge("superseded")` One of the following:
-#'   * "gradient" (the default)
-#'   * "viridis"
-#'   * A function that returns a continuous colour scale.
+#' @param type `r lifecycle::badge("superseded")` The preferred mechanism for
+#'   setting the default palette is by using the theme. For example:
+#'   `theme(palette.colour.discrete = "viridis")`.
 #' @seealso  [continuous_scale()] and [binned_scale()]
 #'
 #'   The documentation on [colour aesthetics][aes_colour_fill_alpha].
@@ -180,18 +179,7 @@ scale_fill_binned <- function(..., palette = NULL, aesthetics = "fill", guide = 
 #' @inheritParams discrete_scale
 #' @param type `r lifecycle::badge("superseded")` The preferred mechanism for
 #'   setting the default palette is by using the theme. For example:
-#'   `theme(palette.colour.discrete = "Okabe-Ito")`. One of the following:
-#'   * A character vector of color codes. The codes are used for a 'manual' color
-#'   scale as long as the number of codes exceeds the number of data levels
-#'   (if there are more levels than codes, [scale_colour_hue()]/[scale_fill_hue()]
-#'   are used to construct the default scale). If this is a named vector, then the color values
-#'   will be matched to levels based on the names of the vectors. Data values that
-#'   don't match will be set as `na.value`.
-#'   * A list of character vectors of color codes. The minimum length vector that exceeds the
-#'   number of data levels is chosen for the color scaling. This is useful if you
-#'   want to change the color palette based on the number of levels.
-#'   * A function that returns a discrete colour/fill scale (e.g., [scale_fill_hue()],
-#'   [scale_fill_brewer()], etc).
+#'   `theme(palette.colour.discrete = "Okabe-Ito")`.
 #' @export
 #' @seealso [discrete_scale()]
 #' @family colour scales
