e75707cd363e0d3e97a52f544b3540b42b8d6371
Teun van den Brand <tahvdbrand@gmail.com>
2025-11-03 10:20:06 +0100

    Fix for formula/call labels

--- R/layer.R
+++ R/layer.R
@@ -974,7 +974,10 @@ normalise_label <- function(label) {
   if (obj_is_list(label)) {
     # Ensure that each element in the list has length 1
     label[lengths(label) == 0] <- ""
-    label <- lapply(label, `[`, 1)
+    # Don't mess with call/formula
+    if (!is.call(label[[1]])) {
+      label <- lapply(label, `[`, 1)
+    }
   }
   if (is.expression(label)) {
     # Classed expressions, when converted to lists, retain their class.
