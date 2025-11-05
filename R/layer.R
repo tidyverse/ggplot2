a4e7497958082e0bedf663e668dfbaf30d991184
Teun van den Brand <tahvdbrand@gmail.com>
2025-11-05 13:44:42 +0100

    collect universal arguments in separate Rd

--- R/layer.R
+++ R/layer.R
@@ -1,3 +1,110 @@
+#' @name shared_layer_parameters
+#' @title Shared layer parameters
+#' @description
+#' This is a central place for describing typical layer parameters.
+#' It prevents cluttered definitions all over the place.
+#'
+#' @param mapping
+#' Set of aesthetic mappings created by [aes()]. If specified and `inherit.aes =
+#' TRUE` (the default), it is combined with the default mapping at the top level
+#' of the plot. You must supply `mapping` if there is no plot mapping.
+#'
+#' @param data
+#' The data to be displayed in this layer. There are three options:
+#' * `NULL` (default): the data is inherited from the plot data as specified
+#'   in the call to [ggplot()].
+#' * A `data.frame`, or other object, will override the plot data. All objects
+#'   will be fortified to produce a data frame. See [fortify()] for which
+#'   variables will be created.
+#' * A `function` will be called with a single argument, the plot data. The return
+#'   value must be a `data.frame`, and will be used as the layer data. A
+#'   `function` can be created from a `formula` (e.g. `~ head(.x, 10)`).
+#'
+#' @param geom
+#' The geometric object to use to display the data for this layer. When using a
+#' `stat_*()` function to construct a layer, the `geom` argument can be used to
+#' override the default coupling between stats and geoms. The `geom` argument
+#' accepts the following:
+#' * A `Geom` ggproto subclass, for example `GeomPoint`.
+#' * A string naming the geom. To give the geom as a string, strip the
+#'   function name of the `geom_` prefix. For example, to use `geom_point()`,
+#'   give the geom as `"point"`.
+#' * For more information and other ways to specify the geom, see the
+#'   [layer geom][layer_geoms] documentation.
+#'
+#' @param stat
+#' The statistical transformation to use on the data for this layer. When using
+#' a `geom_*()` function to construct a layer, the `stat` argument can be used
+#' to override the default coupling between geoms and stats. The `stat` argument
+#' accepts the following:
+#' * A `Stat` ggproto subclass, for example `StatCount`.
+#' * A string naming the stat. To give the stat as a string, strip the
+#'   function name of the `stat_` prefix. For example, to use `stat_count()`,
+#'   give the stat as `"count"`.
+#' * For more information and other ways to specify the stat, see the
+#'   [layer stat][layer_stats] documentation.
+#'
+#' @param position
+#' A position adjustment to use on the data for this layer. This can be used in
+#' various ways, including to prevent overplotting and improving the display.
+#' The `position` argument accepts the following:
+#' * The result of calling a position function, such as `position_jitter()`.
+#'   This method allows for passing extra arguments to the position.
+#' * A string naming the position adjustment. To give the position as a
+#'   string, strip the function name of the `position_` prefix. For example, to
+#'   use `position_jitter()`, give the position as `"jitter"`.
+#' * For more information and other ways to specify the position, see the
+#'   [layer position][layer_positions] documentation.
+#'
+#' @param na.rm
+#' If `FALSE`, the default, missing values are removed with a warning. If
+#' `TRUE`, missing values are silently removed.
+#'
+#' @param show.legend
+#' Logical. Should this layer be included in the legends? `NA`, the default,
+#' includes if any aesthetics are mapped. `FALSE` never includes, and `TRUE`
+#' always includes. It can also be a named logical vector to finely select the
+#' aesthetics to display. To include legend keys for all levels, even when no
+#' data exists, use `TRUE`.  If `NA`, all levels are shown in legend, but
+#' unobserved levels are omitted.
+#'
+#' @param inherit.aes
+#' If `FALSE`, overrides the default aesthetics, rather than combining with
+#' them. This is most useful for helper functions that define both data and
+#' aesthetics and shouldn't inherit behaviour from the default plot
+#' specification, e.g. [annotation_borders()].
+#'
+#' @param ...
+#' Other arguments passed on to [layer()]'s `params` argument. These arguments
+#' broadly fall into one of 4 categories below. Notably, further arguments to
+#' the `position` argument, or aesthetics that are required can
+#'   *not* be passed through `...`. Unknown arguments that are not part of the 4
+#' categories below are ignored.
+#' * Static aesthetics that are not mapped to a scale, but are at a fixed
+#'   value and apply to the layer as a whole. For example, `colour = "red"` or
+#'   `linewidth = 3`. The geom's documentation has an **Aesthetics** section
+#'   that lists the available options. The 'required' aesthetics cannot be
+#'   passed on to the `params`. Please note that while passing unmapped
+#'   aesthetics as vectors is technically possible, the order and required
+#'   length is not guaranteed to be parallel to the input data.
+#' * When constructing a layer using
+#'   a `stat_*()` function, the `...` argument can be used to pass on parameters
+#'   to the `geom` part of the layer. An example of this is
+#'   `stat_density(geom = "area", outline.type = "both")`. The geom's
+#'   documentation lists which parameters it can accept.
+#' * Inversely, when constructing a layer using a
+#'   `geom_*()` function, the `...` argument can be used to pass on parameters
+#'   to the `stat` part of the layer. An example of this is
+#'   `geom_area(stat = "density", adjust = 0.5)`. The stat's documentation lists
+#'   which parameters it can accept.
+#' * The `key_glyph` argument of [`layer()`] may also be passed on through
+#'   `...`. This can be one of the functions described as
+#'   [key glyphs][draw_key], to change the display of the layer in the legend.
+#'
+#' @keywords internal
+#' @aliases NULL
+NULL
+
 #' Create a new layer
 #'
 #' A layer is a combination of data, stat and geom with a potential position
@@ -5,66 +112,7 @@
 #' calls but it can also be created directly using this function.
 #'
 #' @export
-#' @param mapping Set of aesthetic mappings created by [aes()]. If specified and
-#'   `inherit.aes = TRUE` (the default), it is combined with the default mapping
-#'   at the top level of the plot. You must supply `mapping` if there is no plot
-#'   mapping.
-#' @param data The data to be displayed in this layer. There are three
-#'    options:
-#'
-#'    If `NULL`, the default, the data is inherited from the plot
-#'    data as specified in the call to [ggplot()].
-#'
-#'    A `data.frame`, or other object, will override the plot
-#'    data. All objects will be fortified to produce a data frame. See
-#'    [fortify()] for which variables will be created.
-#'
-#'    A `function` will be called with a single argument,
-#'    the plot data. The return value must be a `data.frame`, and
-#'    will be used as the layer data. A `function` can be created
-#'    from a `formula` (e.g. `~ head(.x, 10)`).
-#'
-#' @param geom The geometric object to use to display the data for this layer.
-#'   When using a `stat_*()` function to construct a layer, the `geom` argument
-#'   can be used to override the default coupling between stats and geoms. The
-#'   `geom` argument accepts the following:
-#'   * A `Geom` ggproto subclass, for example `GeomPoint`.
-#'   * A string naming the geom. To give the geom as a string, strip the
-#'     function name of the `geom_` prefix. For example, to use `geom_point()`,
-#'     give the geom as `"point"`.
-#'   * For more information and other ways to specify the geom, see the
-#'     [layer geom][layer_geoms] documentation.
-#' @param stat The statistical transformation to use on the data for this layer.
-#'   When using a `geom_*()` function to construct a layer, the `stat`
-#'   argument can be used to override the default coupling between geoms and
-#'   stats. The `stat` argument accepts the following:
-#'   * A `Stat` ggproto subclass, for example `StatCount`.
-#'   * A string naming the stat. To give the stat as a string, strip the
-#'     function name of the `stat_` prefix. For example, to use `stat_count()`,
-#'     give the stat as `"count"`.
-#'   * For more information and other ways to specify the stat, see the
-#'     [layer stat][layer_stats] documentation.
-#' @param position A position adjustment to use on the data for this layer. This
-#'   can be used in various ways, including to prevent overplotting and
-#'   improving the display. The `position` argument accepts the following:
-#'   * The result of calling a position function, such as `position_jitter()`.
-#'     This method allows for passing extra arguments to the position.
-#'   * A string naming the position adjustment. To give the position as a
-#'     string, strip the function name of the `position_` prefix. For example,
-#'     to use `position_jitter()`, give the position as `"jitter"`.
-#'   * For more information and other ways to specify the position, see the
-#'     [layer position][layer_positions] documentation.
-#' @param show.legend logical. Should this layer be included in the legends?
-#'   `NA`, the default, includes if any aesthetics are mapped.
-#'   `FALSE` never includes, and `TRUE` always includes.
-#'   It can also be a named logical vector to finely select the aesthetics to
-#'   display. To include legend keys for all levels, even
-#'   when no data exists, use `TRUE`.  If `NA`, all levels are shown in legend,
-#'   but unobserved levels are omitted.
-#' @param inherit.aes If `FALSE`, overrides the default aesthetics,
-#'   rather than combining with them. This is most useful for helper functions
-#'   that define both data and aesthetics and shouldn't inherit behaviour from
-#'   the default plot specification, e.g. [annotation_borders()].
+#' @inheritParams shared_layer_parameters
 #' @param check.aes,check.param If `TRUE`, the default, will check that
 #'   supplied parameters and aesthetics are understood by the `geom` or
 #'   `stat`. Use `FALSE` to suppress the checks.
