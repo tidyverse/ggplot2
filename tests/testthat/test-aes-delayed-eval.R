ffc60735f43c8b3476f3e247d7f9742302813f0b
Teun van den Brand <tahvdbrand@gmail.com>
2025-11-07 12:18:19 +0100

    reorganise scale tests

--- tests/testthat/test-aes-delayed-eval.R
+++ tests/testthat/test-aes-delayed-eval.R
@@ -177,3 +177,14 @@ test_that("aes evaluation fails with unknown input", {
   expect_snapshot_error(is_calculated(environment()))
   expect_snapshot_error(strip_dots(environment()))
 })
+
+
+test_that("staged aesthetics are backtransformed properly (#4155)", {
+  p <- ggplot(data.frame(value = 16)) +
+    geom_point(aes(stage(value, after_stat = x / 2), 0)) +
+    scale_x_sqrt(limits = c(0, 16), breaks = c(2, 4, 8))
+
+  # x / 2 should be 16 / 2 = 8, thus the result should be sqrt(8) on scale_x_sqrt()
+  expect_equal(get_layer_data(p)$x, sqrt(8))
+})
+
