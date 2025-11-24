1cf5af1368024149b19a6e33fdf690e663226137
Teun van den Brand <tahvdbrand@gmail.com>
2025-11-24 15:27:59 +0100

    try without messing with environment

--- tests/testthat/test-guide-old.R
+++ tests/testthat/test-guide-old.R
@@ -1,8 +1,8 @@
 skip_on_cran() # This test suite is long-running (on cran) and is skipped
 
 test_that("old S3 guides can be implemented", {
-  my_env <- env()
-  my_env$guide_circle <- function() {
+
+  guide_circle <- function() {
     structure(
       list(available_aes = c("x", "y"), position = "bottom"),
       class = c("guide", "circle")
@@ -12,26 +12,22 @@ test_that("old S3 guides can be implemented", {
   registerS3method(
     "guide_train",
     "circle",
-    function(guide, ...) guide,
-    envir = my_env
+    function(guide, ...) guide
   )
   registerS3method(
     "guide_transform",
     "circle",
-    function(guide, ...) guide,
-    envir = my_env
+    function(guide, ...) guide
   )
   registerS3method(
     "guide_merge",
     "circle",
-    function(guide, ...) guide,
-    envir = my_env
+    function(guide, ...) guide
   )
   registerS3method(
     "guide_geom",
     "circle",
-    function(guide, ...) guide,
-    envir = my_env
+    function(guide, ...) guide
   )
   registerS3method(
     "guide_gengrob",
@@ -42,11 +38,9 @@ test_that("old S3 guides can be implemented", {
         height = unit(1, "cm"),
         width = unit(1, "cm")
       )
-    },
-    envir = my_env
+    }
   )
 
-  withr::local_environment(my_env)
   withr::local_options(lifecycle_verbosity = "quiet")
 
   my_guides <- guides(x = guide_circle())
