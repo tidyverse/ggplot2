90adcd04506c49e6743edbfe0b18b08d3716ca40
Teun van den Brand <tahvdbrand@gmail.com>
2025-11-25 14:57:08 +0100

    add tests

---
+++ tests/testthat/_snaps/utilities-lifecycle.md
@@ -0,0 +1,31 @@
+# editions can be set and unset
+
+    Code
+      set_edition("nonsense")
+    Condition
+      Error in `set_edition()`:
+      ! `edition` must be one of "2025" or "2026", not "nonsense".
+
+# edition deprecation works
+
+    `foo()` was deprecated in ggplot2 4.0.0.
+    i Please use `bar()` instead.
+
+---
+
+    Code
+      foo()
+    Condition
+      Error:
+      ! `foo()` was deprecated in ggplot2 4.0.0 and is now defunct.
+      i Please use `bar()` instead.
+
+# edition supersession works
+
+    Code
+      foo()
+    Condition
+      Error:
+      ! `foo()` was deprecated in <NA> edition 2025 and is now defunct.
+      i Please use `bar()` instead.
+
---
+++ tests/testthat/test-utilities-lifecycle.R
@@ -0,0 +1,41 @@
+test_that("editions can be set and unset", {
+
+  x <- set_edition(2026)
+  expect_null(x) # Set edition returns old value
+  expect_equal(get_edition(), "2026")
+
+  x <- set_edition(NULL)
+  expect_equal(x, "2026")
+  expect_equal(get_edition(), NULL)
+
+  # Test invalid values
+  expect_snapshot(
+    set_edition("nonsense"),
+    error = TRUE
+  )
+})
+
+test_that("edition deprecation works", {
+  foo <- function() {
+    deprecate("4.0.0", what = "foo()", with = "bar()")
+  }
+  expect_snapshot_warning(foo())
+
+  set_edition(2025)
+  withr::defer(set_edition())
+
+  expect_snapshot(foo(), error = TRUE)
+})
+
+test_that("edition supersession works", {
+  foo <- function() {
+    supersede("2025", what = "foo()", with = "bar()")
+    NULL
+  }
+  expect_silent(foo())
+
+  set_edition(2025)
+  withr::defer(set_edition())
+
+  expect_snapshot(foo(), error = TRUE)
+})
